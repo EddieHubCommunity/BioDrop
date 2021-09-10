6fd9ba89a2ae06b184147ef5bea65662f2f19bc2
Eddie Jaoude <eddie@jaoudestudios.com>
2021-09-10 18:03:47 +0100

    feat: improved social buttons

--- src/App.js
+++ src/App.js
@@ -13,12 +13,12 @@ function App() {
       <Profile />
 
       <div className="p-d-flex p-jc-center">
-        <div className="p-d-flex p-flex-column">
-          <Button className="p-p-3">
+        <div className="p-d-flex p-flex-column" style={{ width: 70 + "%" }}>
+          <Button className="p-p-3 p-m-2">
             <i className="pi pi-youtube p-px-2"></i>
             <span className="p-px-3">Youtube</span>
           </Button>
-          <Button className="p-p-3">
+          <Button className="p-p-3 p-m-2">
             <i className="pi pi-twitter p-px-2"></i>
             <span className="p-px-3">Twitter</span>
           </Button>
