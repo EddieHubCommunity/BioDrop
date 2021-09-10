fa1ac2cc6b9d967776e70346710b82d3e0143d7e
Eddie Jaoude <eddie@jaoudestudios.com>
2021-09-10 17:21:56 +0100

    feat: removed unused code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import "primereact/resources/themes/saga-blue/theme.css";
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+
+import { Avatar } from "primereact/avatar";
+import { Badge } from "primereact/badge";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Avatar image="eddiejaoude.jpg" size="xlarge">
+        <Badge value="4" severity="danger" />
+      </Avatar>
     </div>
   );
 }
