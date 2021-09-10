2e94fb37d31b7398868e423ba73f9c5f506d75d3
Eddie Jaoude <eddie@jaoudestudios.com>
2021-09-10 17:41:20 +0100

    feat: bio + avatar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
 import "primereact/resources/themes/saga-blue/theme.css";
 import "primereact/resources/primereact.min.css";
 import "primeicons/primeicons.css";
+import "primeflex/primeflex.css";
 
 import { Avatar } from "primereact/avatar";
 import { Badge } from "primereact/badge";
 
 function App() {
   return (
-    <div>
-      <Avatar image="eddiejaoude.jpg" size="xlarge">
-        <Badge value="4" severity="danger" />
-      </Avatar>
+    <div className="p-m-4">
+      <div className="p-d-flex p-jc-center p-ai-center">
+        <Avatar
+          image="eddiejaoude.jpg"
+          size="xlarge"
+          shape="circle"
+          className="p-overlay-badge"
+        >
+          <Badge value="4" severity="info" />
+        </Avatar>
+      </div>
+      <div className="p-d-flex p-jc-center">
+        <p>Founder of EddieHub</p>
+      </div>
     </div>
   );
 }
