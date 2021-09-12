759c3d7b8822a9357976bcbd5ef21c130eb8c85f
Eddie Jaoude <eddie@jaoudestudios.com>
2021-09-12 19:23:16 +0100

    feat: allowed links only (#84)

--- src/Components/Links.js
+++ src/Components/Links.js
@@ -17,17 +17,19 @@ function Links({ links }) {
   return (
     <div className="p-d-flex p-jc-center">
       <div className="p-d-flex p-flex-column" style={{ width: 70 + '%' }}>
-        {links.map((link, index) => (
-          <Button
-            className="p-p-3 p-m-2 p-button-outlined"
-            style={{ color: colors[link.icon] }}
-            key={`link.url_${index}`}
-            onClick={() => goToLinkHandle(link.url)}
-          >
-            <i className={`pi pi-${link.icon} p-px-2`}></i>
-            <span className="p-px-3">{link.name}</span>
-          </Button>
-        ))}
+        {links
+          .filter((link) => Object.keys(colors).includes(link.icon))
+          .map((link, index) => (
+            <Button
+              className="p-p-3 p-m-2 p-button-outlined"
+              style={{ color: colors[link.icon] }}
+              key={`link.url_${index}`}
+              onClick={() => goToLinkHandle(link.url)}
+            >
+              <i className={`pi pi-${link.icon} p-px-2`}></i>
+              <span className="p-px-3">{link.name}</span>
+            </Button>
+          ))}
       </div>
     </div>
   )
