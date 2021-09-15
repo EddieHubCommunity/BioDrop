2c28b9c4f13479cac7b5e02d59351121ffa81b07
Aayush Jain <ayush8989049912@gmail.com>
2021-09-15 15:50:29 +0530

    revert changes to resolve conflict

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -26,7 +26,7 @@ function Home() {
   return (
     <div>
       {list.map((user, key) => (
-        <Avatar className="avatars"
+        <Avatar
           image={user.avatar}
           key={key}
           shape="circle"
