f3b5d1f2985d11d15c5b50e5fd0e99b98eb3ca8c
Emma Dawson <57045550+emmalearnscode@users.noreply.github.com>
2021-09-19 23:10:18 +0200

    fix: accessibility enhancements (#25)
    * Make jsx semantic and add aria labels and roles
    
    * Wrap avatar in anchor tab to make keyboard navigation possible. Alt text added to image.

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -2,21 +2,14 @@ import React, { useState, useEffect } from 'react'
 
 import { Avatar } from 'primereact/avatar'
 
-import { useHistory } from 'react-router-dom'
-
 function Home() {
-  const history = useHistory()
   const [list, setList] = useState([
     {
-      name: '404',
+      username: '404',
       avatar:
         'https://user-images.githubusercontent.com/624760/114314271-ea156a80-9af1-11eb-97ca-977be7565aa6.png',
     },
   ])
-
-  const goToLinkHandle = (url) => {
-    history.push(url)
-  }
 
   useEffect(() => {
     fetch('/list.json')
@@ -25,18 +18,19 @@ function Home() {
   }, [])
 
   return (
-    <div className="align-center">
+    <main className="align-center">
       {list.map((user, key) => (
-        <Avatar
-          image={user.avatar}
-          key={`avatar-${key}`}
-          shape="circle"
-          size="xlarge"
-          className="p-m-2"
-          onClick={() => goToLinkHandle(user.username)}
-        />
+        <a href={`${user.username}`} key={`avatar-${key}`}>
+          <Avatar
+            image={user.avatar}
+            shape="circle"
+            size="xlarge"
+            className="p-m-2"
+            imageAlt={user.username}
+          />
+        </a>
       ))}
-    </div>
+    </main>
   )
 }
 
