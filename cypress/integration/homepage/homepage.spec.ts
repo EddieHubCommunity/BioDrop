6bb27354276b00fe365ae4e6674af49c1e487db8
wendall.hershberger <wendall.hershberger@gmail.com>
2021-09-23 12:28:38 -0400

    Add Test for user page

--- cypress/integration/homepage/homepage.spec.ts
+++ cypress/integration/homepage/homepage.spec.ts
@@ -1,13 +1,14 @@
 /// <reference types="cypress" />
 
+const baseUrl = 'http://linkfree.eddiehub.org/';
+
 context('Homepage', () => {
     beforeEach(() => {
-      cy.visit('http://linkfree.eddiehub.org/')
+      cy.visit(baseUrl);
     })
     
     it('lists multiple users', () => {
-        cy.get('main a').should('have.length.at.least',2)
+        cy.get('main a').should('have.length.at.least',2);
     })
-  
   })
   
