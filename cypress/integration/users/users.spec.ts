306e2ee734e36b0749349dacd27c7f7d393c7cd4
wendall.hershberger <wendall.hershberger@gmail.com>
2021-09-24 11:31:19 -0400

    User Tests Navigate from HomePage. Update File Endings

--- cypress/integration/users/users.spec.ts
+++ cypress/integration/users/users.spec.ts
@@ -5,13 +5,13 @@ const baseUrl = 'http://linkfree.eddiehub.org';
 
 context('Users', () => {
     beforeEach(() => {
-      cy.visit(`${baseUrl}/eddiejaoude`,)
+      cy.visit(`${baseUrl}`,)
     })
     
     it('page shows content from user provided file', () => {
-        cy.url().should('include', '/eddiejaoude')
-        cy.get('h1').should('contain.text',userData.name)
-        cy.get('main > section > div').should('contain.text',userData.bio)
+      cy.get('[alt="eddiejaoude"]').click()
+      cy.url().should('include', '/eddiejaoude')
+      cy.get('h1').should('contain.text',userData.name)
+      cy.get('main > section > div').should('contain.text',userData.bio)
       })
   })
-  
