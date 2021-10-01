b0e1c32bab9d31a940a40d067a1451ce0279bb7e
Eddie Jaoude <eddie@jaoudestudios.com>
2021-10-01 13:43:38 +0100

    feat: cucumber testing (#226)

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -16,7 +16,11 @@
  * @type {Cypress.PluginConfig}
  */
 // eslint-disable-next-line no-unused-vars
+
+const cucumber = require('cypress-cucumber-preprocessor').default
+
 module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
+  on('file:preprocessor', cucumber())
 }
