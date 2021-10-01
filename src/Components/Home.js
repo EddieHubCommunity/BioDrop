718f497c09875c30b9cb9cc18392ca8d2e653f38
sejal <sejalzambare7@gmail.com>
2021-10-01 10:14:27 +0530

    Revert "closes #96"
    This reverts commit febfd1710e54c532770aa1f0a7a8f32ffd811541.

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,10 +1,8 @@
 import './Home.css'
 
 import React, { useState, useEffect } from 'react'
-
 import { ProgressBar } from 'primereact/progressbar'
 import { Avatar } from 'primereact/avatar'
-import ReactTooltip from 'react-tooltip'
 
 function Home() {
   const [showProgress, setShowProgress] = useState(true)
@@ -25,17 +23,14 @@ function Home() {
     <main>
       {showProgress && <ProgressBar mode="indeterminate" />}
       {list.map((user, key) => (
-        <a href={`${user.username}`} key={`avatar-${key}`} >
-          <div className = "avatar-photo" data-tip={user.username} data-for="toolTip1" data-place = "bottom">
-            <Avatar
-              image={user.avatar}
-              shape="circle"
-              size="xlarge"
-              className="p-m-2 p-avatar"
-              imageAlt={user.username}
-            />
-          </div>
-          <ReactTooltip className= "reactTooltip" id="toolTip1" />
+        <a href={`${user.username}`} key={`avatar-${key}`}>
+          <Avatar
+            image={user.avatar}
+            shape="circle"
+            size="xlarge"
+            className="p-m-2"
+            imageAlt={user.username}
+          />
         </a>
       ))}
     </main>
