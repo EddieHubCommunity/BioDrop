18e2310d868bb94a349936fd8f5374a86e8085de
Eddie Jaoude <eddie@jaoudestudios.com>
2021-10-02 08:45:20 +0100

    fix: removed tooltip

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -3,7 +3,6 @@ import './Home.css'
 import React, { useState, useEffect } from 'react'
 import { ProgressBar } from 'primereact/progressbar'
 import { Avatar } from 'primereact/avatar'
-import { Tooltip } from 'primereact/tooltip'
 
 function Home() {
   const [showProgress, setShowProgress] = useState(true)
@@ -25,16 +24,13 @@ function Home() {
       {showProgress && <ProgressBar mode="indeterminate" />}
       {list.map((user, key) => (
         <a href={`${user.username}`} key={`avatar-${key}`}>
-          <div className = "avatar-div " data-pr-tooltip={user.username} data-pr-position="bottom">
-            <Avatar
-              image={user.avatar}
-              shape="circle"
-              size="xlarge"
-              className="p-m-2"
-              imageAlt={user.username}
-            />
-          </div>
-          <Tooltip target=".avatar-div" className="p-tooltip usernameTooltip" />
+          <Avatar
+            image={user.avatar}
+            shape="circle"
+            size="xlarge"
+            className="p-m-2"
+            imageAlt={user.username}
+          />
         </a>
       ))}
     </main>
