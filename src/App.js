5ed2548691848fa67c4192631c15ab47ce23d902
Soham Sattigeri <79072777+Soham041201@users.noreply.github.com>
2021-10-03 10:37:56 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,10 @@ import 'primereact/resources/themes/saga-blue/theme.css'
 import 'primereact/resources/primereact.min.css'
 import 'primeicons/primeicons.css'
 import 'primeflex/primeflex.css'
-import Navbar from './Components/Navbar'
+
 import React from 'react'
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-
+import Navbar from './Components/Navbar'
 import Footer from './Components/Footer'
 import Socials from './Components/Socials'
 import Home from './Components/Home'
