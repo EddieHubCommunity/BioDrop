48a219a0ecf187dc4e14e3cabf7e424e9ed84fe3
Stephen Mount <sm@ste.london>
2021-10-04 19:26:17 +0000

    some suggestions

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,17 +1,12 @@
 import './Home.css'
+
 import React, { useState, useEffect } from 'react'
 import { ProgressBar } from 'primereact/progressbar'
 import { Avatar } from 'primereact/avatar'
-import { Skeleton } from 'primereact/skeleton'
 
 function Home() {
   const [showProgress, setShowProgress] = useState(true)
   const [list, setList] = useState([])
-  const [profile, setprofile] = useState({
-    user: [],
-  })
-
-  const data = []
 
   useEffect(() => {
     fetch('/list.json')
@@ -24,51 +19,22 @@ function Home() {
       .finally(() => setShowProgress(false))
   }, [])
 
-  //  Get Avatar
-  useEffect(() => {
-    list.forEach((user) => {
-      const splitId = user.avatar.split('/')
-      const userID = splitId[3].split('.')
-      const githubID = userID[0]
-      const url = 'https://avatars.githubusercontent.com/' + githubID
-      fetch(url)
-        .then(response => response.blob())
-        .then(imageBlob => {
-          const imageObjectURL = URL.createObjectURL(imageBlob)
-          const obj = {
-            avatar: imageObjectURL,
-            username: githubID,
-          }
-          data.push(obj)
-          setprofile({
-            user: data,
-          })
-        })
-    })
-  }, [list])
-
   return (
     <main>
       {showProgress && <ProgressBar mode="indeterminate" />}
-      {profile.user.length !== list.length
-        ? <div className="p-d-flex p-flex-wrap">
-          {list.map((user, index) => { return <Skeleton shape="circle" size="65px" className="p-mr-2 p-m-2" key={index} /> })}
-        </div>
-        : <>
-        {profile.user.map((user, key) => (
+      <div className="p-d-flex p-flex-wrap">
+        {list.map((user, key) => (
           <a href={`${user.username}`} key={`avatar-${key}`}>
             <Avatar
-              image={user.avatar}
+              image={`${user.avatar}?size=65`}
               shape="circle"
               size="xlarge"
               className="p-m-2"
               imageAlt={user.username}
             />
           </a>
-        ))
-        }
-        </>
-      }
+        ))}
+      </div>
     </main>
   )
 }
