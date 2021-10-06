960e3aa6abf2f0a48a766ba2278e91bfc2b37357
Suhail Khan <suhailkna@gmail.com>
2021-10-06 23:47:36 +0530

    Adding Skeleton to the User Profile when Loading

--- src/Components/UserProfile/Socials.js
+++ src/Components/UserProfile/Socials.js
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from 'react'
-import { useParams, Link } from 'react-router-dom'
+import { useParams } from 'react-router-dom'
 import { ProgressBar } from 'primereact/progressbar'
 
-import Profile from './Profile'
-import Links from './Links'
-import Milestones from './Milestones'
+import ProfilePage from './ProfilePage'
+import Placeholder from './Placeholder'
 
 function Socials() {
   const [showProgress, setShowProgress] = useState(true)
   const { username } = useParams()
   const [profile, setProfile] = useState({})
+  const [skeleton, setskeleton] = useState(true)
 
   useEffect(() => {
     fetch(`/data/${username}.json`)
@@ -20,24 +20,15 @@ function Socials() {
         alert('An error occurred please try again later.')
       })
       .finally(() => setShowProgress(false))
+    setTimeout(() => {
+      setskeleton(false)
+    }, 500)
   }, [username])
 
   return (
     <main>
       {showProgress && <ProgressBar mode="indeterminate" />}
-      {!showProgress && (
-        <>
-          <Link to="/"><i className="pi pi-arrow-left"></i></Link>
-          <Profile
-            bio={profile.bio}
-            avatar={profile.avatar}
-            name={profile.name}
-            total={profile.links.length}
-          />
-          <Links links={profile.links} />
-        </>
-      )}
-      {profile.milestones && <Milestones milestones={profile.milestones} />}
+      {skeleton ? <Placeholder list={profile}/> : <ProfilePage list={profile} />}
     </main>
   )
 }
