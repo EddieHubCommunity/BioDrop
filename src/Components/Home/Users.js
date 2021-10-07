86ad064b9fe53e05f0bc322ba0280c0a36da0351
Shivansh Yadav <yadavshivansh@gmail.com>
2021-10-07 16:25:09 +0530

    added a search box

--- src/Components/Home/Users.js
+++ src/Components/Home/Users.js
@@ -1,11 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { Chip } from 'primereact/chip'
 
 function User({ list }) {
+  const [searchTerm, setsearchTerm] = useState('')
   return (
     <>
-      {list.map((user, key) => (
+    <input className="searchBar" type="text" placeholder="Search..." onChange={event => { setsearchTerm(event.target.value) }} />
+      {list.filter(User => User.name.toLowerCase().includes(searchTerm.toLowerCase())).map((user, key) => (
         <a href={`${user.username}`} key={`avatar-${key}`}>
           <Chip image={user.avatar} className="p-m-2" label={user.name} />
         </a>
