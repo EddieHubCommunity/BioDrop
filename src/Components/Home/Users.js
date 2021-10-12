909fee710a6636973ccdd27ba207ad666c82f22c
Riddhi Damani <riddhidamani90@gmail.com>
2021-10-11 23:11:28 -0500

    Added LinkFree Logo

--- src/Components/Home/Users.js
+++ src/Components/Home/Users.js
@@ -2,19 +2,32 @@ import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { Chip } from 'primereact/chip'
 import { InputText } from 'primereact/inputtext'
+import linkfree from '../res/images/linkfree.png'
+import './Users.css'
 
 function User({ list }) {
   const [searchTerm, setSearchTerm] = useState('')
   return (
     <>
-      <div className="search-section">
-        <InputText value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} placeholder="Search..." />
+      <div className="logo-img">
+        <img src={linkfree} alt="Link Free" width="10%" height="8%"></img>
       </div>
-      {list.filter(User => User.name.toLowerCase().includes(searchTerm.toLowerCase())).map((user, key) => (
-        <a href={`${user.username}`} key={`avatar-${key}`}>
-          <Chip image={user.avatar} className="p-m-2" label={user.name} />
-        </a>
-      ))}
+      <div className="search-section">
+        <InputText
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search..."
+        />
+      </div>
+      {list
+        .filter((User) =>
+          User.name.toLowerCase().includes(searchTerm.toLowerCase()),
+        )
+        .map((user, key) => (
+          <a href={`${user.username}`} key={`avatar-${key}`}>
+            <Chip image={user.avatar} className="p-m-2" label={user.name} />
+          </a>
+        ))}
     </>
   )
 }
