0c640aacd30d729160fefa76a9837fd5b0b3e456
Nivendha <knivendha@gmail.com>
2021-10-13 11:05:25 +0530

    Update src/Components/ImageLoader.js
    Co-authored-by: Krish Gupta <krishguptadev@outlook.com>

--- src/Components/ImageLoader.js
+++ src/Components/ImageLoader.js
@@ -1,5 +1,5 @@
 
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import PropTypes from 'prop-types'
 import { Skeleton } from 'primereact/skeleton'
 
