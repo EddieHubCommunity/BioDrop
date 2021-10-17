87bf85e0396db07a55279c82e83c6f2312b5838f
Amit Wani <mtwn105@gmail.com>
2021-10-17 21:11:22 +0530

    Add user not found message on search

--- src/Components/Home/Users.js
+++ src/Components/Home/Users.js
@@ -2,19 +2,54 @@ import React, { useState } from 'react'
 import PropTypes from 'prop-types'
 import { Chip } from 'primereact/chip'
 import { InputText } from 'primereact/inputtext'
+import { Message } from 'primereact/message'
 
 function User({ list }) {
   const [searchTerm, setSearchTerm] = useState('')
   return (
     <>
       <div className="search-section">
-        <InputText value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)} placeholder="Search..." />
+        <InputText
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          placeholder="Search..."
+        />
       </div>
-      {list.filter(User => User.name.toLowerCase().includes(searchTerm.toLowerCase())).map((user, key) => (
-        <a href={`${user.username}`} key={`avatar-${key}`}>
-          <Chip image={user.avatar} className="p-m-2" label={user.name} />
-        </a>
-      ))}
+      {(() => {
+        if (
+          list.filter((User) =>
+            User.name.toLowerCase().includes(searchTerm.toLowerCase()),
+          ).length > 0
+        ) {
+          return list
+            .filter((User) =>
+              User.name.toLowerCase().includes(searchTerm.toLowerCase()),
+            )
+            .map((user, key) => (
+              <a href={`${user.username}`} key={`avatar-${key}`}>
+                <Chip image={user.avatar} className="p-m-2" label={user.name} />
+              </a>
+            ))
+        } else {
+          return (
+            <div className="p-d-flex p-jc-center p-ai-center">
+              <Message
+                severity="error"
+                text="No user found."
+              />
+            </div>
+          )
+        }
+      })()}
+      {/* {list
+        .filter((User) =>
+          User.name.toLowerCase().includes(searchTerm.toLowerCase()),
+        )
+        .map((user, key) => (
+          <a href={`${user.username}`} key={`avatar-${key}`}>
+            <Chip image={user.avatar} className="p-m-2" label={user.name} />
+          </a>
+        ))} */}
     </>
   )
 }
