04235a9aadaced244b5d3c1f74745b814b31443d
suhail34 <suhailkna@gmail.com>
2021-10-25 00:15:57 +0530

    feat: add user profile loading skeleton (#465)
    * Adding Skeleton to the User Profile when Loading
    
    * Update src/Components/UserProfile/Social.js
    
    * Resolve merge Conflicts

---
+++ src/Components/UserProfile/ProfilePage.js
@@ -0,0 +1,29 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link } from 'react-router-dom'
+
+import Links from '../Links'
+import Profile from '../Profile'
+import Milestones from '../Milestones'
+
+function User({ list }) {
+  return (<main>
+        {<>
+            <Link to="/"><i className="pi pi-arrow-left"></i></Link>
+            <Profile
+                bio={list.bio}
+                avatar={list.avatar}
+                name={list.name}
+                total={list.links.length}
+            />
+            <Links links={list.links} />
+        </>}
+      {list.milestones && <Milestones milestones={list.milestones} />}
+    </main>)
+}
+
+User.propTypes = {
+  list: PropTypes.array.isRequired,
+}
+
+export default User
---
+++ src/Components/UserProfile/Socials.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react'
+import { useParams } from 'react-router-dom'
+import { ProgressBar } from 'primereact/progressbar'
+
+import ProfilePage from './ProfilePage'
+import Placeholder from './Placeholder'
+
+function Socials() {
+  const [showProgress, setShowProgress] = useState(true)
+  const { username } = useParams()
+
+  const [profile, setProfile] = useState({})
+  const [skeleton, setskeleton] = useState(true)
+
+  useEffect(() => {
+    fetch(`/data/${username}.json`)
+      .then((response) => response.json())
+      .then((data) => setProfile(data))
+
+      .catch((error) => {
+        console.log('Socials useEffect', error)
+        alert('An error occurred please try again later.')
+      })
+      .finally(() => {
+        setShowProgress(false)
+        setTimeout(() => {
+          setskeleton(false)
+        }, 500)
+      })
+  }, [username])
+
+  if (!profile) {
+    return <div className="p-text-center">
+        <div className="flex-column">
+        <img src='/eddiehub_community_logo.webp' alt="image" style={{ width: '150px' }}/>
+        <h1>Profile not found.</h1>
+        <h1>If you are a new user, please consider registering at LinkFree.</h1>
+        <h2>Read the documendation <a href="https://github.com/EddieHubCommunity/LinkFree#readme" target="_blank" rel="noreferrer">here</a>.</h2>
+      </div>
+    </div>
+  } else {
+    return (
+    <main>
+      {showProgress && <ProgressBar mode="indeterminate" />}
+
+      {skeleton ? <Placeholder list={profile}/> : <ProfilePage list={profile} />}
+
+    </main>
+    )
+  }
+}
+
+export default Socials
