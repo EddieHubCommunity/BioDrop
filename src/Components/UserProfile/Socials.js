e93cdf5d7dd0f0e2474208ff654492ce986356eb
Suhail Khan <suhailkna@gmail.com>
2021-10-24 15:56:39 +0530

    Resolve merge Conflicts

--- src/Components/UserProfile/Socials.js
+++ src/Components/UserProfile/Socials.js
@@ -12,7 +12,6 @@ function Socials() {
   const [profile, setProfile] = useState({})
   const [skeleton, setskeleton] = useState(true)
 
-
   useEffect(() => {
     fetch(`/data/${username}.json`)
       .then((response) => response.json())
