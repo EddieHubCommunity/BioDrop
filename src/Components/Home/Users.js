fe414300cfe0624c4ac03764e080c31f3d7b4a9c
Vyom Jain <vyom.j@protonmail.com>
2021-10-29 13:19:00 +0530

    fix: no users found message (#520)
    * Add user not found message on search
    
    * Update user not found message
    
    Co-authored-by: David Leal <halfpacho@gmail.com>
    
    * Removed commented code
    
    * Refactoring the code, removed if else in render
    
    * Updating the error message
    
    Co-authored-by: Tom Schmelzer <30869493+schmelto@users.noreply.github.com>
    
    * fix: tests for no results found
    
    Co-authored-by: Amit Wani <mtwn105@gmail.com>
    Co-authored-by: David Leal <halfpacho@gmail.com>
    Co-authored-by: Tom Schmelzer <30869493+schmelto@users.noreply.github.com>
    Co-authored-by: Eddie Jaoude <eddie@jaoudestudios.com>

--- src/Components/Home/Users.js
+++ src/Components/Home/Users.js
@@ -3,12 +3,24 @@ import { Link } from 'react-router-dom'
 import PropTypes from 'prop-types'
 import { Chip } from 'primereact/chip'
 
+import { Message } from 'primereact/message'
+
 import Navbar from '../Navbar'
 import Searchbar from './Searchbar'
 
 function Users({ list }) {
   const [searchTerm, setSearchTerm] = useState('')
-  const searchHandler = (value) => setSearchTerm(value || '')
+  const [filteredList, setFilteredList] = useState(list)
+
+  const searchHandler = (value) => {
+    setSearchTerm(value || '')
+    setFilteredList(
+      list.filter((User) =>
+        User.name.toLowerCase().includes(value.toLowerCase()),
+      ),
+    )
+  }
+
   return (
     <>
       <Navbar
@@ -17,15 +29,22 @@ function Users({ list }) {
         }
       />
       <div className="user-list">
-        {list
-          .filter((User) =>
-            User.name.toLowerCase().includes(searchTerm.toLowerCase()),
-          )
-          .map((user, key) => (
-            <Link to={`${user.username}`} key={`avatar-${key}`}>
-              <Chip image={user.avatar} className="p-m-2" label={user.name} />
-            </Link>
-          ))}
+        {!!filteredList &&
+          filteredList.length > 0 &&
+          filteredList
+            .filter((User) =>
+              User.name.toLowerCase().includes(searchTerm.toLowerCase()),
+            )
+            .map((user, key) => (
+              <Link to={user.username} key={`avatar-${key}`}>
+                <Chip image={user.avatar} className="p-m-2" label={user.name} />
+              </Link>
+            ))}
+        {!!filteredList && filteredList.length === 0 && (
+          <div className="p-d-flex p-jc-center p-ai-center">
+            <Message severity="error" text="No users found please try again" />
+          </div>
+        )}
       </div>
     </>
   )
