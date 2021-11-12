98508eac8ee9e88bf3be19a06b29527eb742f771
Fidal <fidal15perfect@gmail.com>
2021-11-12 12:48:50 +0530

    add contribute at end

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -8,22 +8,27 @@ import { Menubar } from 'primereact/menubar'
 function Navbar({ items, start, end }) {
   if (!end) {
     end = (
-      <a href="https://github.com/EddieHubCommunity/LinkFree">
-        <i className="pi pi-github" style={{ fontSize: '2em' }}></i>
-      </a>
+      <div className="p-d-flex p-jc-center p-ai-center p-pr-2">
+        <div className="p-p-1"> Contribute on </div>
+        <a
+          href="https://github.com/EddieHubCommunity/LinkFree"
+          className="p-p-1"
+        >
+          <i className="pi pi-github" style={{ fontSize: '1em' }}></i>
+        </a>
+        <div>v.0.0.0</div>
+      </div>
     )
   }
 
-  if (!items) {
-    items = [
-      {
-        label: 'Contribute on Github',
-        icon: 'pi pi-fw pi-github',
-      },
-    ]
-  }
-
-  return <Menubar model={items} start={start} end={end} className="p-mb-4" />
+  return (
+    <Menubar
+      model={items}
+      start={start}
+      end={end}
+      className="p-mb-4 p-flex-wrap p-jc-center"
+    />
+  )
 }
 
 Navbar.propTypes = {
