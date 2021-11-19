03bff72fa43cfa2f85b912bb81c14287b7530825
Mark Smellie <marksmellie892@gmail.com>
2021-11-19 08:59:42 +0000

    Update src/Components/Profile.js
    Co-authored-by: Krish Gupta <krishguptadev@outlook.com>

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -25,7 +25,7 @@ function Profile({ profile, username }) {
             className="p-mr-2 p-mt-2"
           />
         </Avatar>
-        <h1 className="p-m-2">{name}</h1>
+        <h1 className="p-m-2">{name} <span class="p-m-2">({username})</span></h1>
         <h2 className="p-avatar-username">({username})</h2>
       </div>
       <div className="p-d-flex p-jc-center w-50">
