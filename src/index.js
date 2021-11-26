373039c8d7f8e1f11244a95e3fde82d97f984334
Goodness Ezeh <88127727+GoodnessEzeh@users.noreply.github.com>
2021-11-26 19:14:22 +0900

    Update the comments in index.js
    Update the comments in index.js

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ ReactDOM.render(
   document.getElementById('root'),
 )
 
-// If you want to start measuring performance in your app, pass a function
+// If you want to start measuring the performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// or send results to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
