c0b29d72eb72cd4e9af575c881944c0383242ea2
Harsh Rai <harshraitth2@gmail.com>
2021-11-26 05:55:44 +0000

    removed async from setDefaultSVG function

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
 export default {
-  setDefaultSVG: async function(name, error) {
+  setDefaultSVG: function(name, error) {
     const url = `https://avatars.dicebear.com/api/initials/${name}.svg`
     error.target.onerror = null
     error.target.src = url
