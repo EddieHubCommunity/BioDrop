6301df7587331dd7804069666ad7badffb7b66fe
Krish Gupta <krishguptadev@outlook.com>
2021-11-29 03:27:16 +0000

    style: format code

--- src/Components/Home/Placeholders.js
+++ src/Components/Home/Placeholders.js
@@ -6,24 +6,20 @@ import { Skeleton } from 'primereact/skeleton'
 function Placeholder({ list }) {
   return (
     <>
-    <Skeleton
-      shape="rectangle"
-      height="3.2rem"
-      className="p-mb-4"
-    />
-    <div className="p-d-flex p-flex-wrap p-jc-center">
-      {list.map((user, key) => {
-        return (
-          <Skeleton
-            width="16rem"
-            height="2.6rem"
-            borderRadius="2rem"
-            className="p-m-2 p-mr-2"
-            key={`skeleton-${key}`}
-          />
-        )
-      })}
-    </div>
+      <Skeleton shape="rectangle" height="3.2rem" className="p-mb-4" />
+      <div className="p-d-flex p-flex-wrap p-jc-center">
+        {list.map((user, key) => {
+          return (
+            <Skeleton
+              width="16rem"
+              height="2.6rem"
+              borderRadius="2rem"
+              className="p-m-2 p-mr-2"
+              key={`skeleton-${key}`}
+            />
+          )
+        })}
+      </div>
     </>
   )
 }
