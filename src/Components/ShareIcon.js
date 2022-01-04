bf92cb14af31322ca8132b12b8f23ad47b82af6a
avneesh0612 <avneeshagarwal0612@gmail.com>
2022-01-04 17:43:21 +0530

    feat: componentize icons

---
+++ src/Components/ShareIcon.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import GetIcons from './Icons/GetIcons'
+import PropTypes from 'prop-types'
+import './ShareIcon.css'
+
+function ShareIcon({ link, label, iconName }) {
+  return (
+    <a
+      className="shareIcon__link"
+      href={link}
+      target="_blank"
+      rel="noopener noreferrer"
+      aria-label={label}
+    >
+      <GetIcons className="shareIcon__icon" iconName={iconName} size={20} />
+    </a>
+  )
+}
+
+export default ShareIcon
+
+ShareIcon.propTypes = {
+  link: PropTypes.string.isRequired,
+  label: PropTypes.string.isRequired,
+  iconName: PropTypes.string.isRequired,
+}
--- src/Components/ShareProfile.js
+++ src/Components/ShareProfile.js
@@ -2,6 +2,7 @@ import React from 'react'
 import GetIcons from './Icons/GetIcons'
 import PropTypes from 'prop-types'
 import './ShareProfile.css'
+import ShareIcon from './ShareIcon'
 
 export default function ShareProfile({ username }) {
   const CopyLink = () => {
@@ -11,33 +12,22 @@ export default function ShareProfile({ username }) {
 
   return (
     <div className="profile__container">
-      <a
-        className="profile__iconLinks"
-        href={`https://www.facebook.com/sharer/sharer.php?u=https://linkfree.eddiehub.org/${username}`}
-        target="_blank"
-        rel="noopener noreferrer"
-        aria-label="Share on Facebook"
-      >
-        <GetIcons className="profile__icon" iconName="facebook" size={20} />
-      </a>
-      <a
-        className="profile__iconLinks"
-        href={`https://twitter.com/intent/tweet?text=Check out my LinkFree profile! https://linkfree.eddiehub.org/${username}`}
-        target="_blank"
-        rel="noopener noreferrer"
-        aria-label="Share on Twitter"
-      >
-        <GetIcons className="profile__icon" iconName="twitter" size={20} />
-      </a>
-      <a
-        className="profile__iconLinks"
-        href={`https://www.linkedin.com/sharing/share-offsite?url=https%3A%2F%2Flinkfree.eddiehub.org%2F${username}`}
-        target="_blank"
-        rel="noopener noreferrer"
-        aria-label="Share on LinkedIn"
-      >
-        <GetIcons className="profile__icon" iconName="linkedin" size={20} />
-      </a>
+      <ShareIcon
+        link={`https://www.facebook.com/sharer/sharer.php?u=https://linkfree.eddiehub.org/${username}`}
+        label="Share on Facebook"
+        iconName="facebook"
+      />
+      <ShareIcon
+        link={`https://twitter.com/intent/tweet?text=Check out my LinkFree profile! https://linkfree.eddiehub.org/${username}`}
+        label="Share on Twitter"
+        iconName="twitter"
+      />
+
+      <ShareIcon
+        link={`https://www.linkedin.com/sharing/share-offsite?url=https%3A%2F%2Flinkfree.eddiehub.org%2F${username}`}
+        label="Share on LinkedIn"
+        iconName="linkedin"
+      />
 
       <a role="button" onClick={CopyLink} aria-label="Copy link to profile">
         <GetIcons className="profile__icon" iconName="link" size={20} />
