9238f5d62cc4a6d97973a92a6450c5402e1f2095
Digambar Tupurwadi <digz@kali>
2022-01-05 16:09:09 +0530

    Added Scroll-to-top button

---
+++ src/Components/ScrollToTopBtn.js
@@ -0,0 +1,41 @@
+import React, { useState, useEffect } from 'react'
+import { BsFillArrowUpCircleFill } from 'react-icons/bs'
+import './ScrollToTopBtn.css'
+
+function ScrollToTopBtn() {
+  const [scrollBtnVisibility, setScrollBtnVisibility] = useState(false)
+
+  const toggleVisibility = () => {
+    if (window.scrollY > 300) {
+      setScrollBtnVisibility(true)
+    } else {
+      setScrollBtnVisibility(false)
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('scroll', toggleVisibility)
+    return () => {
+      window.removeEventListener('scroll', toggleVisibility)
+    }
+  }, [])
+
+  const scrollToTop = () => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    })
+    setScrollBtnVisibility(scrollBtnVisibility)
+  }
+
+  return (
+    <BsFillArrowUpCircleFill
+      style={scrollBtnVisibility ? {} : { opacity: '0' }}
+      className="scrollToTop-btn"
+      onClick={scrollToTop}
+      size={70}
+    />
+  )
+}
+
+export default ScrollToTopBtn
