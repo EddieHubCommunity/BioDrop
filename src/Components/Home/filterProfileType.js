6b471bcaa60158c6cd88aa24f0e3ead8b081f090
nathgoutam93 <nathgoutam93@gmail.com>
2022-01-08 23:21:31 +0530

    fix: removed extra div on filterProfileType component

--- src/Components/Home/filterProfileType.js
+++ src/Components/Home/filterProfileType.js
@@ -3,18 +3,11 @@ import PropTypes from 'prop-types'
 
 export default function ProfileTypeFilter({ profileType, typeHandler }) {
   return (
-    <div className="flex flex-column justify-content-center">
-      <div className="m-3 flex">
-        <select
-          value={profileType}
-          onChange={(e) => typeHandler(e.target.value)}
-        >
-          <option value="personal">Personal</option>
-          <option value="misc">Misc</option>
-          <option value="community">Community</option>
-        </select>
-      </div>
-    </div>
+    <select value={profileType} onChange={(e) => typeHandler(e.target.value)}>
+      <option value="personal">Personal</option>
+      <option value="misc">Misc</option>
+      <option value="community">Community</option>
+    </select>
   )
 }
 
