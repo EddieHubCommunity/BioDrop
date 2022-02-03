c134e584019958b266b266f9af90290aa6794ae9
Kendall Pinto <kendalldoescoding@gmail.com>
2022-02-03 13:05:13 +0530

    FORMAT JS

--- src/utils.js
+++ src/utils.js
@@ -1,7 +1,7 @@
 export default {
-  setDefaultSVG: function(name, error) {
-    const url = `https://avatars.dicebear.com/api/initials/${name}.svg`
-    error.target.onerror = null
-    error.target.src = url
-  },
+    setDefaultSVG: function(name, error) {
+        const url = `https://avatars.dicebear.com/api/initials/${name}.svg`
+        error.target.onerror = null
+        error.target.src = url
+    },
 }
