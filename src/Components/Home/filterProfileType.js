14678f798278c126640fdc260294d516a5d02c38
Kendall Pinto <kendalldoescoding@gmail.com>
2022-02-03 13:13:42 +0530

    format js

--- src/Components/Home/filterProfileType.js
+++ src/Components/Home/filterProfileType.js
@@ -1,24 +1,42 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Dropdown } from 'primereact/dropdown'
+import {
+    Dropdown
+} from 'primereact/dropdown'
 
-export default function ProfileTypeFilter({ profileType, typeHandler }) {
-  const profileTypes = [
-    { label: 'Personal', value: 'personal' },
-    { label: 'All', value: 'all' },
-    { label: 'Community', value: 'community' },
-  ]
+export default function ProfileTypeFilter({
+    profileType,
+    typeHandler
+}) {
+    const profileTypes = [{
+            label: 'Personal',
+            value: 'personal'
+        },
+        {
+            label: 'All',
+            value: 'all'
+        },
+        {
+            label: 'Community',
+            value: 'community'
+        },
+    ]
 
-  return (
-    <Dropdown
-      value={profileType}
-      options={profileTypes}
-      onChange={(e) => typeHandler(e.value)}
-    />
-  )
+    return ( <
+        Dropdown value = {
+            profileType
+        }
+        options = {
+            profileTypes
+        }
+        onChange = {
+            (e) => typeHandler(e.value)
+        }
+        />
+    )
 }
 
 ProfileTypeFilter.propTypes = {
-  profileType: PropTypes.string.isRequired,
-  typeHandler: PropTypes.func.isRequired,
+    profileType: PropTypes.string.isRequired,
+    typeHandler: PropTypes.func.isRequired,
 }
