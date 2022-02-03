4229d52bb10f14400b9d387a73c2cd6fe37a4554
Kendall Pinto <kendalldoescoding@gmail.com>
2022-02-03 13:07:35 +0530

    format js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,13 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root'),
+ReactDOM.render( <
+    React.StrictMode >
+    <
+    App / >
+    <
+    /React.StrictMode>,
+    document.getElementById('root'),
 )
 
 // If you want to start measuring performance in your app, pass a function
