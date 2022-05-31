a13173c1957b26e077dbb76f6f8e6dd04b05ee5b
Eddie Jaoude <eddie@jaoudestudios.com>
2022-05-31 11:40:18 +0100

    fix: improved html

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -6,7 +6,7 @@ import GetIcons from './Icons/GetIcons'
 
 function Home() {
   return (
-    <>
+    <main>
       <Navbar
         start={
           <Link to="/search" aria-label="Search">
@@ -14,15 +14,15 @@ function Home() {
           </Link>
         }
       />
-      <h2 className="text-4xl text-center">
+      <h1 className="text-4xl text-center">
         LinkFree connects audiences to all of your content with just one link
-      </h2>
+      </h1>
       <p className="text-2xl text-center">
         It is an open-source alternative to Linktree implemented in JavaScript
       </p>
       <p className="text-1xl text-center">
-        See <Link to="/eddiejaoude">Eddie Jaoude&apos;s</Link> profile for an example.{' '}
-        Want to add your profile? Read the{' '}
+        See <Link to="/eddiejaoude">Eddie Jaoude&apos;s</Link> profile for an
+        example. Want to add your profile? Read the{' '}
         <a href="https://github.com/EddieHubCommunity/LinkFree#-to-add-your-profile">
           instructions
         </a>
@@ -35,7 +35,7 @@ function Home() {
           alt="Exapmle of the LinkFree app on Apple devices"
         />
       </p>
-    </>
+    </main>
   )
 }
 
