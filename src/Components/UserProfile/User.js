d9d3ce47a7d0980293736db218409deafccdec51
Andrew Koo <47183818+andrewkykoo@users.noreply.github.com>
2022-07-12 15:17:18 -0400

    fix: Update back arrow to navigate to correct page (#1482)
    * updated Link component in Navbar component to point to search page
    
    * reverted package-lock.json to its original ver
    
    * changed the label of the link component
    
    Co-authored-by: Andrew Koo <andrewkoo@Andrews-MBP.lan>

--- src/Components/UserProfile/User.js
+++ src/Components/UserProfile/User.js
@@ -38,7 +38,7 @@ function User({ singleUser }) {
       {!singleUser && (
         <Navbar
           start={
-            <Link to="/" aria-label="Go back to Home">
+            <Link to="/search" aria-label="Go back to Search">
               <GetIcons iconName="arrowLeft" size={20} />
             </Link>
           }
