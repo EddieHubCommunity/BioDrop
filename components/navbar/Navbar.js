7942c611c49103fca2da0b78f5b1bfaf558203ae
Eddie Jaoude <eddie@jaoudestudios.com>
2022-07-30 16:59:38 +0100

    fix: removed user profile img #1470

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -28,8 +28,8 @@ export default function Navbar() {
               <div className="flex-shrink-0">
                 <img
                   className="h-8 w-8"
-                  src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg"
-                  alt="Workflow"
+                  src="/logo192.png"
+                  alt="EddieHub logo"
                 />
               </div>
               <div className="hidden md:block">
@@ -105,13 +105,6 @@ export default function Navbar() {
           </div>
           <div className="pt-4 pb-3 border-t border-gray-700">
             <div className="flex items-center px-5">
-              <div className="flex-shrink-0">
-                <img
-                  className="h-10 w-10 rounded-full"
-                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                  alt=""
-                />
-              </div>
               <div className="ml-3">
                 <div className="text-base font-medium leading-none text-white">
                   GitHub
