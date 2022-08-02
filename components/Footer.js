53fb984e8cfd3721b3e44e5b1300cbc62b443584
JealousGx <khiljimateenn@gmail.com>
2022-08-02 15:20:50 +0500

    Home page and bug fixes

--- components/Footer.js
+++ components/Footer.js
@@ -1,8 +1,9 @@
+import Link from "next/link";
+
 export default function Footer() {
   return (
     <footer className="flex justify-center mt-8 italic">
-      Powered by{" "}
-      <a href="https://github.com/EddieHubCommunity/LinkFree">EddieHub</a>
+      Powered by <Link href="https://github.com/EddieHubCommunity/LinkFree"><span>EddieHub</span></Link>
     </footer>
   );
 }
