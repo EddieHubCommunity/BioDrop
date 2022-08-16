5402a02c77c55e2705919e4a3ee4bf99cf6eb236
jasham.qureshi <jasham.qureshi@covalensedigital.com>
2022-08-16 11:35:26 +0530

    fixed commented issue

--- config/mongo.js
+++ config/mongo.js
@@ -2,7 +2,8 @@ import mongoose from "mongoose";
 
 const connectMongo = async () => {
     try{
-        await mongoose.connect(process.env.MONGODB_URI).then(() => console.error("DB connected")).catch((err) => console.error("DB not connected",err));
+        await mongoose.connect(process.env.MONGODB_URI)
+        console.error("DB connected")
     }catch(err){
         console.error("DB Not connected",err)
     }
