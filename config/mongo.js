e365e405b6b088c8790d30a1e0dbb78ca63c49ff
Jasham <jasham009@gmail.com>
2022-08-17 16:08:34 +0530

    fix: missing db connection (#1615)
    * fixed missing db initiation code
    
    * fixed commented issues
    
    * fixed commented issue
    
    * fixed commented issues
    
    Co-authored-by: jasham.qureshi <jasham.qureshi@covalensedigital.com>

--- config/mongo.js
+++ config/mongo.js
@@ -1,5 +1,12 @@
 import mongoose from "mongoose";
 
-const connectMongo = async () => mongoose.connect(process.env.MONGODB_URI);
+const connectMongo = async () => {
+    try{
+        await mongoose.connect(process.env.MONGODB_URI)
+        console.log("DB connected")
+    }catch(err){
+        console.error("DB Not connected",err)
+    }
+}
 
 export default connectMongo;
