f068e85616f1a2fc8fa68873cc0180fde57a72d2
Tom Schmelzer <30869493+schmelto@users.noreply.github.com>
2022-08-24 12:28:23 +0000

    style: format files

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,10 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="flex justify-center mt-8 italic">
-      Powered by <Link href="https://github.com/EddieHubCommunity/LinkFree"><span>EddieHub</span></Link>
+      Powered by{" "}
+      <Link href="https://github.com/EddieHubCommunity/LinkFree">
+        <span>EddieHub</span>
+      </Link>
     </footer>
   );
 }
