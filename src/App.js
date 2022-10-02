e60115355504883a53e2c2e8aa3111f762ae4d17
Fais Edathil <102040088+E-fais@users.noreply.github.com>
2022-10-02 21:51:10 +0300

    Replace Switch with Routes
    In  react-router-dom version 6,  'Switch' key word is replaced as 'Routes'

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 import 'primereact/resources/themes/saga-blue/theme.css'
 import 'primereact/resources/primereact.min.css'
@@ -19,7 +19,7 @@ function App() {
       <div className="p-2 md:p-4 max-h-screen">
         {user.username && <User singleUser={user} />}
         {!user.username && (
-          <Switch>
+          <Routes>
             <Route path="/search">
               <Search />
             </Route>
@@ -29,7 +29,7 @@ function App() {
             <Route path="/">
               <Home />
             </Route>
-          </Switch>
+          </Routes>
         )}
         <Footer />
       </div>
