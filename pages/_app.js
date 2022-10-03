6771765a04a6db2e2fc7b985805578e4f9684dbe
Ketan Saraf <saraf.ketan1108@gmail.com>
2022-10-03 17:53:06 +0530

    Add dark theme config and install
    Installed next-themes package & wrapped the application with ThemesProvider

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,15 @@
 import "../styles/globals.css";
 import MultiLayout from "../components/layouts/MultiLayout";
+import { ThemeProvider } from "next-themes";
 
 export default function MyApp({ Component, pageProps }) {
   // Use the layout defined at the page level, if available
   const getLayout =
     Component.getLayout || ((page) => <MultiLayout>{page}</MultiLayout>);
 
-  return getLayout(<Component {...pageProps} />);
+  return (
+    <ThemeProvider attribute="class">
+      {getLayout(<Component {...pageProps} />)}
+    </ThemeProvider>
+  );
 }
