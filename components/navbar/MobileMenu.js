69ad4b8ccec0eb17d9f543b00e3f9db12eb03fa4
Yazdun <yazduntube@gmail.com>
2022-10-07 09:31:09 +0330

    removed commented codes

--- components/navbar/MobileMenu.js
+++ components/navbar/MobileMenu.js
@@ -1,7 +1,6 @@
 import { useRouter } from "next/router";
 import { appRoutes } from "./data";
 import NavLink from "./NavLink";
-import app from "../../config/app.json";
 import { Version } from "./Version";
 
 export const MobileMenu = () => {
@@ -15,16 +14,6 @@ export const MobileMenu = () => {
         ))}
       </div>
       <div className="pt-4 pb-3 border-t border-gray-700">
-        {/* <div className="flex items-center px-5">
-          <ul className="flex flex-col gap-2 md:flex-row">
-            <li className="text-base font-medium leading-none text-white">
-              GitHub
-            </li>
-            <li className="text-sm font-medium leading-none text-gray-400">
-              v{app.version}
-            </li>
-          </ul>
-        </div> */}
         <Version />
       </div>
     </div>
