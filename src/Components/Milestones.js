e9e87e0847f4f1cede76791e648420df18845c20
vasu <vasu@vasus-MacBook-Air.local>
2022-10-12 01:39:06 +0530

    resolving conflict

--- src/Components/Milestones.js
+++ src/Components/Milestones.js
@@ -8,6 +8,7 @@ import {
   VerticalTimeline,
   VerticalTimelineElement,
 } from 'react-vertical-timeline-component'
+
 import 'react-vertical-timeline-component/style.min.css'
 
 function Milestones({ milestones }) {
