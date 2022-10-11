456099f77277cd9086124594351066bebe8c66d9
vasu <vasu@vasus-MacBook-Air.local>
2022-10-12 01:54:22 +0530

    resolving conflict

--- src/Components/Milestones.js
+++ src/Components/Milestones.js
@@ -39,9 +39,6 @@ function Milestones({ milestones }) {
               <h3 className="vertical-timeline-element-title">
                 {milestone.title}
               </h3>
-              <h3 className="vertical-timeline-element-title">
-                {milestone.title}
-              </h3>
               {milestone.image && (
                 <img className="timeline-img" src={milestone.image} />
               )}
