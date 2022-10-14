0d707a5fc19fd81e84b26a23689de30beff6a0f5
Eddie Jaoude <eddie@jaoudestudios.com>
2022-10-14 16:44:05 +0100

    fix: search test skeleton

---
+++ tests/search.spec.js
@@ -0,0 +1,19 @@
+// @ts-check
+const { test, expect } = require("@playwright/test");
+
+test("Search has title", async ({ page }) => {
+  await page.goto("/search");
+  await expect(page).toHaveTitle(/Search/);
+});
+
+test.fixme("Navigate to the Search page", async ({ page }) => {
+  // 1. nagivate to homepage
+  // 2. click on search on the navbar
+  // 3. check for the text "search" on the page
+});
+
+test.fixme("Search works correctly", async ({ page }) => {
+  // 1. nagivate to search page
+  // 2. show user is listed with all users (no search term)
+  // 3. type in search and check that user with the name exist and check a name doesn't exist from step 2
+});
