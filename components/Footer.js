92d80980c14b677b13875b55a9289c7ffb06f63d
Eddie Jaoude <eddie@jaoudestudios.com>
2022-10-14 16:28:32 +0100

    fix: footer test (#1967)

--- components/Footer.js
+++ components/Footer.js
@@ -3,9 +3,8 @@ import Link from "next/link";
 export default function Footer() {
   return (
     <footer className="flex justify-center mt-8 italic">
-      Powered by{" "}
       <Link href="https://github.com/EddieHubCommunity/LinkFree">
-        <span>EddieHub</span>
+        Powered by EddieHub
       </Link>
     </footer>
   );
