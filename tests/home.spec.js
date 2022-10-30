46acd8cb892d67967ba088ec67e9e49e9a53803c
Eddie Jaoude <eddie@jaoudestudios.com>
2022-10-30 09:11:47 +0000

    chore: removed unrequired comments

--- tests/home.spec.js
+++ tests/home.spec.js
@@ -8,20 +8,15 @@ test("homepage has title", async ({ page }) => {
 
 test("homepage has example link", async ({ page }) => {
   await page.goto("/");
-
-  // create a locator
   const getStarted = page.getByText("Eddie Jaoude's");
 
-  // Click the get started link.
   await getStarted.click();
 
-  // Expects the URL to contain intro.
   await expect(page).toHaveURL(/eddiejaoude/);
 });
 
 test("Footer link goes to GitHub", async ({ page }) => {
   await page.goto("/");
-
   const getFooter = page.getByText("Powered by EddieHub");
 
   await getFooter.click();
