aeb7f0f96b196da02875dfa7c61b867cc6e3d3c9
Eddie Jaoude <eddie@jaoudestudios.com>
2022-10-31 12:50:42 +0000

    fix: NavLink Story to have args

--- stories/components/navbar/Navlink.stories.js
+++ stories/components/navbar/Navlink.stories.js
@@ -1,8 +1,15 @@
 import NavLink from "../../../components/navbar/NavLink";
 
-
-export default{
-  component: NavLink
+export default {
+  component: NavLink,
 };
 
-export const Basic = <NavLink />;
+export const Basic = <NavLink {...args} />;
+
+Basic.args = {
+  path: "",
+  item: {
+    url: "",
+  },
+  mode: "",
+};
