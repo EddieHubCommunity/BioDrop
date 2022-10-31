fcf51d84dffd5240503902dab38e10cab8481da3
Eddie Jaoude <eddie@jaoudestudios.com>
2022-10-31 21:30:13 +0000

    fix: popular test failing

--- tests/popular.spec.js
+++ tests/popular.spec.js
@@ -1,9 +1,13 @@
 // @ts-check
 import { test, expect } from "@playwright/test";
 
-test.fixme("Click on popular profile in navbar navigates to popular page", async ({ page }) => {
-  // 1. click on popular nav link
-  // 2. check if it takes us to the popular page
+test.fixme(
+  "Click on popular profile in navbar navigates to popular page",
+  async ({ page }) => {
+    // 1. click on popular nav link
+    // 2. check if it takes us to the popular page
+  }
+);
 
 test.fixme("Popular profiles listed", async ({ page }) => {
   // 1. navigate to popular profile page
