72027e931a646e68416482f35c88163094bcb6e3
Jaynarayan vaishnav <jaynarayan540@gmail.com>
2022-11-01 01:40:20 +0530

    add test to check the navigation of popular profile
    Added skeleton to navigate to profile page while clicking on the popular profile.

--- tests/popular.spec.js
+++ tests/popular.spec.js
@@ -1,1 +1,5 @@
-
+test.fixme("click on popular profile navigates to profile page", async ({ page }) => {
+  // 1. navigate to popular profile
+  // 2. click on search on the popular profile
+  // 3. check if it takes us to the profile page
+});
