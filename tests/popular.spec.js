ab894589a1ec46fb40e4bf6a9628baed59dc10da
Jaynarayan vaishnav <jaynarayan540@gmail.com>
2022-11-01 02:51:16 +0530

    tests: popular profile search (#2088)
    * add test for popular profile search
    
    * add test for popular profile view
    
     Addedtest to check wheather the most popular profile is at thee top of the profile list
    
    * update search.spec.js
    
    moved the test for popular profile view to popular.spec.js

---
+++ tests/popular.spec.js
@@ -0,0 +1,4 @@
+test.fixme("Popular profile view", async ({ page }) => {
+  // 1. click on the popular profile
+  // 2. check wheather the most popular profile is at thee top of the profile list
+});
