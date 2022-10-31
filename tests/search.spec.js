aeec075d47a2f85c1b27cc514a3245242d3afaf8
jaynarayan20 <jaynarayan540@gmail.com>
2022-10-31 22:03:23 +0530

    test for navigation of popular profile.

--- tests/search.spec.js
+++ tests/search.spec.js
@@ -40,3 +40,9 @@ test.fixme("After search click profile", async ({ page }) => {
   // 2. click on searched profile
   // 3. check profile is displayed
 });
+
+test.fixme("click on popular profile navigates to profile page", async ({ page }) => {
+  // 1. navigate to popular profile
+  // 2. click on search on the popular profile
+  // 3. check if it takes us to the profile page
+});
