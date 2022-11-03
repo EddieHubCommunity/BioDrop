96de8c9fc29818d6db12731a4801a3b5beabcf74
Noibisjunior <noibisjunior22@gmail.com>
2022-11-03 14:54:35 +0100

    Added a boostrap accordion

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
             href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
             rel="stylesheet"
             integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-            cross-Origin="anonymous"
+            crossOrigin="anonymous"
             async
           />
         </head>
