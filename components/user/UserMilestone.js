baf3495f7fef7ea423a2c2b68289a8d9d10a38d8
Eddie Jaoude <eddie@jaoudestudios.com>
2022-11-04 19:00:30 +0000

    fix: milestone icon

--- components/user/UserMilestone.js
+++ components/user/UserMilestone.js
@@ -1,16 +1,21 @@
+import Icon from "../Icon";
+
 export default function UserMilestone({ milestone }) {
   return (
     <div
-      className="border-2 border-gray-200 p-4 my-2 rounded-tr-lg grow"
+      className="border-2 border-gray-200 p-4 my-2 rounded-tr-lg grow flex gap-4 items-center"
       key={milestone.title}
       style={{
         "border-color": milestone.color,
       }}
     >
-      <h2 className="text-xl">
-        {milestone.date} {milestone.title}
-      </h2>
-      <p>{milestone.description}</p>
+      <Icon name={milestone.icon} />
+      <div>
+        <h2 className="text-xl">
+          {milestone.date} {milestone.title}
+        </h2>
+        <p>{milestone.description}</p>
+      </div>
     </div>
   );
 }
