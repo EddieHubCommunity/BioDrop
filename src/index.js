dcb612132a3693e38c4f21a3bb6cd200c811cccc
Arnab Dey <info@arnabcs.ml>
2022-11-05 18:35:44 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ ReactDOM.render(
   document.getElementById('root'),
 )
 
-// If you want to start measuring performance in your app, pass a function
+// If you want to start measuring performance in your app, then pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals()
