80fd19dc65610070c95bd7c2f86251b1468491e2
Vasu Singh <vasucp1207@gmail.com>
2022-11-05 21:24:24 +0530

    fix: dark mode toggle reachable with keyboard (#1931)
    * Fixing Dark mode toggle is not reachable with keyboard navigation
    
    * Replacing a tag with the button Issue #1927
    
    * Replacing a tag with the button Issue #1927
    
    * Fix margins due to a tag changing to button
    
    Co-authored-by: vasu <vasu@vasus-MacBook-Air.local>

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -43,7 +43,7 @@ function Navbar({ items, start, end }) {
 
         <div style={theme}>v{version}</div>
 
-        <div className="theme--button">
+        <button className="theme--button">
           {darkTheme
             ? (
             <GetIcons
@@ -56,7 +56,7 @@ function Navbar({ items, start, end }) {
             : (
             <GetIcons iconName="sun" onClick={toggleTheme} size={20} />
               )}
-        </div>
+        </button>
       </div>
     )
   }
