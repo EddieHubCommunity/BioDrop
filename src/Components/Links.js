04fc02bf94df1589711c0521c1dee9169c57aa4a
Alex Rock <pierstoval@gmail.com>
2022-11-05 17:18:31 +0100

    feat: use the "rel" attribute for specific link types (#2103)
    One example is if you want to use LinkFree as your "home page", and want to add a link to your Mastodon profile, adding `rel="me"` in the link allows to make your mastodon account "verified" (similar to what twitter blue does but with less certainty and safety).
    
    Since "rel" is a standard attribute, instead of just a flag for `rel="me"`, this PR allows supporting **any** value for `rel`, up to the user 👍

--- src/Components/Links.js
+++ src/Components/Links.js
@@ -36,6 +36,7 @@ function Links({ links }) {
                     ? 'white'
                     : colors[link.icon],
               }}
+              rel={link.rel ? link.rel : null}
               href={link.url}
               target="_blank"
               rel="noopener noreferrer"
@@ -59,6 +60,7 @@ function Links({ links }) {
               onMouseOut={MouseOut}
               className={`p-3 my-2 p-button-outlined ${link.icon}`}
               style={{ color: colors.globe }}
+              rel={link.rel ? link.rel : null}
               href={link.url}
               target="_blank"
               rel="noopener noreferrer"
