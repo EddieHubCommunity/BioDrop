cd7fbb763dd745061e22dd27d8d75c33f8c48bbe
kartik <97971066+kartikgajjar7@users.noreply.github.com>
2022-11-05 21:32:29 +0530

    fix: visible links (#1992)
    Co-authored-by: Eddie Jaoude <eddie@jaoudestudios.com>

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -60,18 +60,9 @@ function Home() {
           It is an open-source alternative to Linktree implemented in JavaScript
         </p>
         <p className="text-1xl text-center">
-          See{' '}
-          <Link
-            to="/eddiejaoude"
-            className={`${darkTheme ? 'text-blue-200' : ''}`}
-          >
-            Eddie Jaoude&apos;s
-          </Link>{' '}
-          profile for an example. Want to add your profile? Read the{' '}
-          <a
-            href="https://github.com/EddieHubCommunity/LinkFree#-to-add-your-profile"
-            className={`${darkTheme ? 'text-blue-200' : ''}`}
-          >
+          See <Link style={{ color: "#5917ff" }} to="/eddiejaoude">Eddie Jaoude&apos;s</Link> profile for an
+          example. Want to add your profile? Read the{' '}
+          <a style={{ color: "#5917ff" }} href="https://github.com/EddieHubCommunity/LinkFree#-to-add-your-profile">
             instructions
           </a>
           .
