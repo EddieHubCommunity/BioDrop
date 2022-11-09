33aef3928e5406061494268a626d74d10679cd4e
Maxime TFT <mkubik@tftlabs.com>
2022-11-09 17:20:53 +0100

    [fix]: add .env error handler for the mongo connection string

--- config/mongo.js
+++ config/mongo.js
@@ -1,5 +1,11 @@
-import mongoose from "mongoose";
-import * as fs from "fs";
+import mongoose from 'mongoose';
+import * as fs from 'fs';
+
+if (!process.env.LINKFREE_MONGO_CONNECTION_STRING) {
+  throw new Error(
+    'Please define LINKFREE_MONGO_CONNECTION_STRING to .env.local'
+  );
+}
 
 let hasConnection = false;
 const connectMongo = async () => {
@@ -10,15 +16,15 @@ const connectMongo = async () => {
     // DigitalOcean Apps has cert as environment variable but Mongo needs a file path
     // Write Mongo cert file to disk
     if (process.env.CA_CERT) {
-      fs.writeFileSync("cert.pem", process.env.CA_CERT);
+      fs.writeFileSync('cert.pem', process.env.CA_CERT);
     }
 
     mongoose.connect(process.env.LINKFREE_MONGO_CONNECTION_STRING);
     hasConnection = true;
-    console.log("DB connected");
+    console.log('DB connected');
   } catch (err) {
     hasConnection = false;
-    console.error("DB Not connected", err);
+    console.error('DB Not connected', err);
   }
 };
 
