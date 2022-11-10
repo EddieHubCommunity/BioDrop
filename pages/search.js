e0dc420c575f5343c72b3b6a45fabfbb30411055
Tom Schmelzer <tom.schmelzer@web.de>
2022-11-10 14:46:05 +0000

    feat: add warning remove results if under 3 characters

--- pages/search.js
+++ pages/search.js
@@ -24,20 +24,27 @@ export default function Search({ users }) {
   const { username } = router.query;
   const [filteredUsers, setFilteredUsers] = useState([]);
   const [notFound, setNotFound] = useState();
+  const [threeOrMore, setThreeOrMore] = useState();
+
+  let results = [];
 
   useEffect(() => {
     if (username) {
       setNotFound(username);
+      setThreeOrMore(false);
     }
   }, []);
 
   const filterData = (value) => {
     if (value.length <= 3) {
+      setThreeOrMore(false);
+      setFilteredUsers(results);
       setNotFound();
     }
 
     if (value.length >= 3) {
-      const results = users.filter((user) =>
+      setThreeOrMore(true);
+      results = users.filter((user) =>
         user.name.toLowerCase().includes(value.toLowerCase())
       );
 
@@ -63,7 +70,7 @@ export default function Search({ users }) {
       <div className="flex flex-col px-6 align-center">
         <h1 className="text-4xl mb-4  font-bold">Search</h1>
         <input
-          placeholder="Search users (minimum 3 characters)"
+          placeholder="Search users"
           className="border-2 hover:border-orange-600 transition-all duration-250 ease-linear rounded px-6 py-2"
           name="keyword"
           onChange={(e) => filterData(e.target.value)}
@@ -71,6 +78,11 @@ export default function Search({ users }) {
         {notFound && (
           <h2 className="bg-red-200 text-red-600 border-2 border-red-600 p-5 my-5 text-xl">
             {notFound} not found
+          </h2>
+        )}
+        {!threeOrMore && (
+          <h2 className="bg-yellow-200 text-yellow-600 border-2 border-yellow-600 p-5 my-5 text-xl">
+            You have to enter at least 3 characters to search for a user.
           </h2>
         )}
         <ul>
