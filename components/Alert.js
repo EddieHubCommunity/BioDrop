8675cefe71bfa90a9e026bc272b4eb65b6438ad8
Eddie Jaoude <eddie@jaoudestudios.com>
2022-11-13 10:33:17 +0000

    fix: failing tests

--- components/Alert.js
+++ components/Alert.js
@@ -7,7 +7,7 @@ export default function alert({ type, message }) {
     case "success":
       return (
         <div
-          className="bg-green-100 rounded-lg py-5 px-6 mb-3 text-base text-green-700 inline-flex items-center w-full animate-pulse"
+          className={`alert-${type} bg-green-100 rounded-lg py-5 px-6 mb-3 text-base text-green-700 inline-flex items-center w-full animate-pulse`}
           role="alert"
         >
           <BsCheckCircleFill className="mr-2" />
@@ -18,7 +18,7 @@ export default function alert({ type, message }) {
     case "error":
       return (
         <div
-          className="bg-red-100 rounded-lg py-5 px-6 mb-3 text-base text-red-700 inline-flex items-center w-full animate-pulse"
+          className={`alert-${type} bg-red-100 rounded-lg py-5 px-6 mb-3 text-base text-red-700 inline-flex items-center w-full animate-pulse`}
           role="alert"
         >
           <MdError className="mr-2" />
@@ -29,7 +29,7 @@ export default function alert({ type, message }) {
     case "warning":
       return (
         <div
-          className="bg-yellow-100 rounded-lg py-5 px-6 mb-3 text-base text-yellow-700 inline-flex items-center w-full animate-pulse"
+          className={`alert-${type} bg-yellow-100 rounded-lg py-5 px-6 mb-3 text-base text-yellow-700 inline-flex items-center w-full animate-pulse`}
           role="alert"
         >
           <MdOutlineWarningAmber className="mr-2" />
@@ -40,7 +40,7 @@ export default function alert({ type, message }) {
     case "info":
       return (
         <div
-          className="bg-blue-100 rounded-lg py-5 px-6 mb-3 text-base text-blue-700 inline-flex items-center w-full animate-pulse"
+          className={`alert-${type} bg-blue-100 rounded-lg py-5 px-6 mb-3 text-base text-blue-700 inline-flex items-center w-full animate-pulse`}
           role="alert"
         >
           <FaInfoCircle className="mr-2" />
--- config/mongo.js
+++ config/mongo.js
@@ -1,14 +1,14 @@
 import mongoose from "mongoose";
 import * as fs from "fs";
 
-if (!process.env.LINKFREE_MONGO_CONNECTION_STRING) {
-  throw new Error(
-    "Please define the LINKFREE_MONGO_CONNECTION_STRING environment variable (if local add to .env file)"
-  );
-}
-
 let hasConnection = false;
 const connectMongo = async () => {
+  if (!process.env.LINKFREE_MONGO_CONNECTION_STRING) {
+    throw new Error(
+      "Please define the LINKFREE_MONGO_CONNECTION_STRING environment variable (if local add to .env file)"
+    );
+  }
+
   if (hasConnection) {
     return;
   }
