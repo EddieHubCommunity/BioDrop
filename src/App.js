dbe992456aa0919b3b82cb0c673f3bcf120a8461
Noibisjunior <noibisjunior22@gmail.com>
2022-11-22 17:36:33 +0100

    removed the link

--- src/App.js
+++ src/App.js
@@ -25,16 +25,6 @@ function App() {
 
   return (
     <Router>
-      <head>
-        <link
-          href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css"
-          rel="stylesheet"
-          integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC"
-          crossOrigin="anonymous"
-          async
-        />
-      </head>
-
       <div className="p-2 md:p-4 max-h-screen" style={theme}>
         {user.username && <User singleUser={user} />}
         {!user.username && (
