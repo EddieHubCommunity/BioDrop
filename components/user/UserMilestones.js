ba6f864dbf143a0ba14feddbf0cfea027d502ba1
Eddie Jaoude <eddie@jaoudestudios.com>
2022-11-28 08:51:53 +0000

    fix: no data for milestones + testimonials

--- components/user/UserMilestones.js
+++ components/user/UserMilestones.js
@@ -5,15 +5,16 @@ export default function UserMilestones({ data }) {
   return (
     <>
       {!data.milestones && <Alert type="info" message="No milestones found" />}
-      {data.milestones.map((milestone, index) => (
-        <div className="flex" key={index}>
-          <div className="w-14 border-l-4 flex flex-col">
-            <div className="border-dashed border-b-2 grow"></div>
-            <div className="grow"></div>
+      {data.milestones &&
+        data.milestones.map((milestone, index) => (
+          <div className="flex" key={index}>
+            <div className="w-14 border-l-4 flex flex-col">
+              <div className="border-dashed border-b-2 grow"></div>
+              <div className="grow"></div>
+            </div>
+            <UserMilestone milestone={milestone} />
           </div>
-          <UserMilestone milestone={milestone} />
-        </div>
-      ))}
+        ))}
     </>
   );
 }
