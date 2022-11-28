9841621803e3598c769cff295495b66d3623513a
Eddie Jaoude <eddie@jaoudestudios.com>
2022-11-28 22:36:34 +0000

    fix: search has basic test

--- tests/search.spec.js
+++ tests/search.spec.js
@@ -6,34 +6,51 @@ test("Search has title", async ({ page }) => {
   await expect(page).toHaveTitle(/Search/);
 });
 
-test.fixme("Navigate to the Search page", async ({ page }) => {
+test("Navigate to the Search page", async ({ page }) => {
   // 1. nagivate to homepage
   // 2. click on search on the navbar
   // 3. check for the text "search" on the page
 });
 
-test.fixme("Search works correctly", async ({ page }) => {
+test("Search works correctly", async ({ page }) => {
   // 1. nagivate to search page
-  // 2. show user is listed with all users (no search term)
-  // 3. type in search and check that user with the name exist and check a name doesn't exist from step 2
+  await page.goto("/search");
+
+  // 2. show no users are listed
+  await expect(page.locator("li")).toHaveCount(0);
+
+  // 3. type in search and check that user with the name exist and check a name doesn't exist
+  const input = page.locator("[name='keyword']");
+  await input.type("eddie");
+
+  await expect(page.locator("li")).toHaveCount(3);
 });
 
-test.fixme("Search page has no results when no search term used", async ({ page }) => {
-  // 1. Navigate to search page
-  // 2. Check no results are displayed
-});
+test.fixme(
+  "Search page has no results when no search term used",
+  async ({ page }) => {
+    // 1. Navigate to search page
+    // 2. Check no results are displayed
+  }
+);
 
-test.fixme("Search page shows no results after typing 2 characters", async ({ page }) => {
-  // 1. Navigate to search page
-  // 2. Type 2 characters
-  // 3. Check no results are displayed
-});
+test.fixme(
+  "Search page shows no results after typing 2 characters",
+  async ({ page }) => {
+    // 1. Navigate to search page
+    // 2. Type 2 characters
+    // 3. Check no results are displayed
+  }
+);
 
-test.fixme("Search page shows results after typing 3 characters", async ({ page }) => {
-  // 1. Navigate to search page
-  // 2. Type 3 characters
-  // 3. Check if the results show up if the it get matched.
-});
+test.fixme(
+  "Search page shows results after typing 3 characters",
+  async ({ page }) => {
+    // 1. Navigate to search page
+    // 2. Type 3 characters
+    // 3. Check if the results show up if the it get matched.
+  }
+);
 
 test.fixme("After search click profile", async ({ page }) => {
   // 1. perform search
@@ -41,8 +58,11 @@ test.fixme("After search click profile", async ({ page }) => {
   // 3. check profile is displayed
 });
 
-test.fixme("find the profile after providing concise name", async ({ page }) => {
-  // 1. click on search profile
-  // 2. type the whole name
-  // 3. Display the profile if the name is correct
-});
+test.fixme(
+  "find the profile after providing concise name",
+  async ({ page }) => {
+    // 1. click on search profile
+    // 2. type the whole name
+    // 3. Display the profile if the name is correct
+  }
+);
