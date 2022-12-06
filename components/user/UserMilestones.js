6aa0db37c22d02452a0af6771fbc144245c7efe7
Eddie Jaoude <eddie@jaoudestudios.com>
2022-12-06 14:14:18 +0000

    feat: milestones refactor

--- components/user/UserMilestones.js
+++ components/user/UserMilestones.js
@@ -5,16 +5,12 @@ export default function UserMilestones({ data }) {
   return (
     <>
       {!data.milestones && <Alert type="info" message="No milestones found" />}
-      {data.milestones &&
-        data.milestones.map((milestone, index) => (
-          <div className="flex" key={index}>
-            <div className="w-14 border-l-4 flex flex-col">
-              <div className="border-dashed border-b-2 grow"></div>
-              <div className="grow"></div>
-            </div>
-            <UserMilestone milestone={milestone} />
-          </div>
-        ))}
+      <ul role="list" className="divide-y divide-gray-200">
+        {data.milestones &&
+          data.milestones.map((milestone, key) => (
+            <UserMilestone milestone={milestone} key={key} />
+          ))}
+      </ul>
     </>
   );
 }
