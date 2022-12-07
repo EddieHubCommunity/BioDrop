a136532113d96f4309f974ed02a09d95bbc6565c
Eddie Jaoude <eddie@jaoudestudios.com>
2022-12-07 07:27:09 +0000

    feat: milestones refactor (#2331)
    * feat: milestones refactor
    
    * fix: milestone styling

--- components/user/UserMilestone.js
+++ components/user/UserMilestone.js
@@ -6,19 +6,30 @@ export default function UserMilestone({ milestone }) {
   return (
     <a
       href={milestone.url}
-      className="border-2 border-gray-200 p-4 my-2 rounded-tr-lg grow flex gap-4 items-center"
-      key={milestone.title}
+      key={milestone.url}
+      target="_blank"
+      rel="noreferrer"
       style={{
         borderColor: milestone.color,
       }}
     >
-      <DisplayIcon />
-      <div>
-        <h2 className="text-xl">
-          {milestone.date} {milestone.title}
-        </h2>
-        <p>{milestone.description}</p>
-      </div>
+      <li
+        className="py-4 border-l-2 mb-4 pl-2 hover:border-l-4 pr-2 shadow-md"
+        style={{
+          borderColor: milestone.color,
+        }}
+      >
+        <div className="flex space-x-3">
+          <DisplayIcon />
+          <div className="flex-1 space-y-1">
+            <div className="flex items-center justify-between">
+              <h3 className="text-sm font-medium">{milestone.title}</h3>
+              <p className="text-sm text-gray-500">{milestone.date}</p>
+            </div>
+            <p className="text-sm text-gray-500">{milestone.description}</p>
+          </div>
+        </div>
+      </li>
     </a>
   );
 }
