00490a90d391bfe10ffb296ee0463ec9791e11aa
Dan <35927536+dan-mba@users.noreply.github.com>
2022-12-10 00:35:20 -0500

    fix: navbar improvements (#2308)

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -78,7 +78,7 @@ export default function Navbar() {
                 type="button"
                 className="bg-gray-800 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
                 aria-controls="mobile-menu"
-                aria-expanded="false"
+                aria-expanded={isOpen}
               >
                 <span className="sr-only">Open main menu</span>
                 <svg
