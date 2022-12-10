2ba8146d4931030008e1c5d0d425c7eb792b9194
Eddie Jaoude <eddie@jaoudestudios.com>
2022-12-10 05:14:15 +0000

    feat: social icons increment stats (#2357)

--- components/user/UserSocials.js
+++ components/user/UserSocials.js
@@ -1,10 +1,27 @@
 import getIcon from "../Icon";
 
-function UserSocial({ social }) {
+function UserSocial({ BASE_URL, username, social }) {
   const Icon = getIcon(social.platform);
+  const clickLink = async () => {
+    try {
+      const res = await fetch(
+        `${BASE_URL}/api/statistics/${username}/${encodeURIComponent(
+          social.url
+        )}`,
+        { method: "PUT" }
+      );
+      const data = await res.json();
+      setClicks(data.clicks);
+    } catch (e) {
+      // TODO: link not found
+      console.log("ERROR link not found ", e);
+    }
+  };
+
   return (
     <a
       href={social.url}
+      onClick={clickLink}
       target="_blank"
       rel="noopener noreferrer"
       className="hover:scale-125 transition ease-in-out delay-100"
