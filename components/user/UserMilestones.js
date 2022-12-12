afd1cfeebe2dfaf5dae530fa833c82f5fd52507d
manish <manishbisht9711@gmail.com>
2022-12-12 20:15:40 +0530

    feature : future goals

--- components/user/UserMilestones.js
+++ components/user/UserMilestones.js
@@ -1,5 +1,6 @@
 import UserMilestone from "./UserMilestone";
 import Alert from "../Alert";
+import UserGoal from "./UserGoal";
 
 export default function UserMilestones({ data }) {
   return (
@@ -7,8 +8,24 @@ export default function UserMilestones({ data }) {
       {!data.milestones && <Alert type="info" message="No milestones found" />}
       <ul role="list" className="divide-y divide-gray-200">
         {data.milestones &&
-          data.milestones.map((milestone, key) => (
-            <UserMilestone milestone={milestone} key={key} />
+          data.milestones
+            .filter((milestone) => !milestone.isGoal)
+            .map((milestone, key) => (
+              <UserMilestone milestone={milestone} key={key} />
+            ))}
+      </ul>
+
+      <div className="flex justify-center items-center gap-3 text-gray-500 my-4  text-xl p-4">
+        <div className="border-t-8 border-dotted border-orange-600 w-[3rem]"></div>
+        Future Goals
+        <div className="border-t-8 border-dotted border-orange-600 w-[3rem]"></div>
+      </div>
+
+      <ul role="list" className="divide-y divide-gray-200">
+        {data.milestones
+          .filter((milestone) => milestone.isGoal)
+          .map((goal, key) => (
+            <UserGoal goal={goal} key={key} />
           ))}
       </ul>
     </>
