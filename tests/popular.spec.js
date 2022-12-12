71bf73c1da091201b830c553d0d9b127d48d6869
Dan <35927536+dan-mba@users.noreply.github.com>
2022-12-12 09:29:48 -0500

    add tests to popular page

--- tests/popular.spec.js
+++ tests/popular.spec.js
@@ -1,5 +1,6 @@
 // @ts-check
 import { test, expect } from "@playwright/test";
+const AxeBuilder = require('@axe-core/playwright').default;
 
 test.fixme(
   "Click on popular profile in navbar navigates to popular page",
@@ -12,4 +13,12 @@ test.fixme(
 test.fixme("Popular profiles listed", async ({ page }) => {
   // 1. navigate to popular profile page
   // 2. check the most popular profile is at the top of the profile list
+});
+
+test('should pass axe wcag accessibility tests', async ({ page }) => {
+  await page.goto('/popular');
+  const accessibilityScanResults = await new AxeBuilder({ page })
+    .withTags(['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa'])
+    .analyze();
+  expect(accessibilityScanResults.violations).toEqual([]);
 });
