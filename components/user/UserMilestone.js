3b984f2e68b12cf1178a65b1d119a679e8aa5e35
manish bisht <manishbisth9711@gmail.com>
2022-12-19 16:30:27 +0530

    Chores : combine userGoal and userMilestone component

--- components/user/UserMilestone.js
+++ components/user/UserMilestone.js
@@ -1,8 +1,38 @@
 import Icon from "../Icon";
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 
-export default function UserMilestone({ milestone }) {
-  return (
+export default function UserMilestone({ milestone, isGoal }) {
+  return isGoal ? (
+    <a
+      href={milestone.url ? milestone.url : "/"}
+      key={milestone.url ? milestone.url : milestone.title}
+      className={!milestone.url ? "pointer-events-none" : ""}
+      target="_blank"
+      rel="noreferrer"
+    >
+      <li
+        className="py-4 border-l-2 mb-4 pl-2 hover:border-l-4 pr-2 shadow-md opacity-50"
+        style={{
+          borderColor: milestone.color,
+        }}
+      >
+        <div className="flex space-x-3">
+          <Icon name={milestone.icon} />
+          <div className="flex-1 space-y-1">
+            <div className="flex items-center justify-between">
+              <h3 className="text-lg font-medium">{milestone.title}</h3>
+              <p className="text-sm text-gray-500">
+                {milestone.date ? milestone.date : ""}
+              </p>
+            </div>
+            <p className="text-sm text-gray-500">
+              <ReactMarkdown>{milestone.description}</ReactMarkdown>
+            </p>
+          </div>
+        </div>
+      </li>
+    </a>
+  ) : (
     <a
       href={milestone.url}
       key={milestone.url}
@@ -24,7 +54,7 @@ export default function UserMilestone({ milestone }) {
             </div>
             <p className="text-sm text-gray-500">
               <ReactMarkdown>{milestone.description}</ReactMarkdown>
-              </p>
+            </p>
           </div>
         </div>
       </li>
--- components/user/UserMilestones.js
+++ components/user/UserMilestones.js
@@ -1,6 +1,5 @@
 import UserMilestone from "./UserMilestone";
 import Alert from "../Alert";
-import UserGoal from "./UserGoal";
 
 export default function UserMilestones({ data }) {
   return (
@@ -11,21 +10,21 @@ export default function UserMilestones({ data }) {
           data.milestones
             .filter((milestone) => !milestone.isGoal)
             .map((milestone, key) => (
-              <UserMilestone milestone={milestone} key={key} />
+              <UserMilestone milestone={milestone} isGoal={false} key={key} />
             ))}
       </ul>
 
       <div className="flex justify-center items-center gap-3 text-gray-500 my-4  text-xl p-4">
-        <div className="border-t-8 border-dotted border-orange-600 w-[3rem]"></div>
+        <div className="border-t-8 border-dotted border-orange-600 w-[2rem] lg:w-[3rem]"></div>
         Future Goals
-        <div className="border-t-8 border-dotted border-orange-600 w-[3rem]"></div>
+        <div className="border-t-8 border-dotted border-orange-600 w-[2rem] lg:2-[3rem]"></div>
       </div>
 
       <ul role="list" className="divide-y divide-gray-200">
         {data.milestones
           .filter((milestone) => milestone.isGoal)
           .map((goal, key) => (
-            <UserGoal goal={goal} key={key} />
+            <UserMilestone milestone={goal} isGoal={true} key={key} />
           ))}
       </ul>
     </>
