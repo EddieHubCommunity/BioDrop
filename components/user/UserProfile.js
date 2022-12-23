48e620615555c185b1548476ccbfcddd9e5fff70
Muthukumar-Muthu <m.m.muthu1388@gmail.com>
2022-12-23 18:54:49 +0530

    Fix QR code margin for mobile

--- components/user/UserProfile.js
+++ components/user/UserProfile.js
@@ -72,7 +72,7 @@ export default function UserProfile({ BASE_URL, data }) {
         </div>
       )}
 
-      <div className="flex justify-center">
+      <div className="flex justify-center sm:my-4">
         {qrShow && (
           <QRCodeSVG
             value={`${BASE_URL}/${data.username}`}
