d5593cb7e038b7e395b49e1d3ba1d947bab57ad8
Eddie Jaoude <eddie@jaoudestudios.com>
2022-12-23 15:59:57 +0000

    fix: docs layout width

--- components/layouts/DocsLayout.js
+++ components/layouts/DocsLayout.js
@@ -3,7 +3,9 @@ export default function DocsLayout({ children }) {
     <>
       <div className="float-none mx-auto my-0 w-[90%] max-w-[1440px] prose">
         <div className="flex flex-grow flex-row">
-          <main className="mx-4">{children}</main>
+          <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+            {children}
+          </main>
         </div>
       </div>
     </>
