1443886d31300bd563650dea2efd660d75ee4297
Eddie Jaoude <eddie@jaoudestudios.com>
2022-12-26 15:04:59 +0000

    fix: quickstart test

--- tests/docs/quickstart.spec.js
+++ tests/docs/quickstart.spec.js
@@ -3,5 +3,5 @@ import { test, expect } from "@playwright/test";
 
 test("quickstart has title", async ({ page }) => {
   await page.goto("/docs/quickstart");
-  await expect(page).toHaveTitle(/Quickstart/);
+  await expect(page).toHaveTitle(/QuickStart/);
 });
