54d9d88c38601f679a76e58ae4c20a157b94aa83
Eddie Jaoude <eddie@jaoudestudios.com>
2022-12-26 10:36:11 +0000

    fix: docs pages to be consistent #2495

--- components/layouts/DocsLayout.js
+++ components/layouts/DocsLayout.js
@@ -13,11 +13,22 @@ export default function DocsLayout({ children, title }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Page>
-        <div className="float-none mx-auto my-0 w-[90%] max-w-[1440px] prose">
+        <h1 className="text-4xl mb-4 font-bold">Documentation</h1>
+        <p>
+          Here you should find everything you need from getting started with
+          creating your Profile to more advanced topics. You can contribute to
+          our documentation and find the files here{" "}
+          <a
+            href="https://github.com/EddieHubCommunity/LinkFree/tree/main/pages/docs"
+            target="_blank"
+            rel="noreferrer"
+          >
+            https://github.com/EddieHubCommunity/LinkFree/tree/main/pages/docs
+          </a>
+        </p>
+        <div className="float-none my-0 max-w-[1440px] prose">
           <div className="flex flex-grow flex-row">
-            <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
-              {children}
-            </main>
+            <main className="max-w-7xl px-4 sm:px-6 lg:px-8">{children}</main>
           </div>
         </div>
       </Page>
