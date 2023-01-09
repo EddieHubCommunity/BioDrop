0639a9f167c3b2856bf698157e46fd07db657905
Eddie Jaoude <eddie@jaoudestudios.com>
2023-01-09 22:06:38 +0000

    feat: trending profiles (#2927)
    * feat: storing daily view on user profiles (#2691)
    
    * Add Schema and code for storing daily view on user profiles
    
    * simplified schema for storing daily views on profiles
    
    * Add segment for showing most popular profiles of last 28 days
    
    * feat: trending profiles
    
    * feat: trending profiles
    
    * fix: trending section heading
    
    * feat: refresh random profiles (#2888)
    
    * feat: Random users on click
    
    * Chores: remove extra variable
    
    * feat: Refresh Icon
    
    Co-authored-by: manish bisht <manishbisth9711@gmail.com>
    
    * feat: refresh random profiles
    
    * fix: removed unused file
    
    Co-authored-by: Shyam Patel <72148710+shyam0705@users.noreply.github.com>
    Co-authored-by: Manish Bisht <89926834+ManishBisht777@users.noreply.github.com>
    Co-authored-by: manish bisht <manishbisth9711@gmail.com>

--- pages/discover.js
+++ pages/discover.js
@@ -1,16 +1,40 @@
 import Head from "next/head";
 import UserCard from "../components/user/UserCard";
 import Page from "../components/Page";
+import { useState } from "react";
+import { TbRefresh } from "react-icons/tb";
 
 export async function getServerSideProps(context) {
-  let data = [];
+  let data = {
+    popular: [],
+    random: [],
+    trending: [],
+  };
   try {
     const res = await fetch(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/api/users/discover`
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/discover/popular`
     );
-    data = await res.json();
+    data.popular = await res.json();
   } catch (e) {
     console.log("ERROR loading popular profiles", e);
+  }
+
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/discover/random`
+    );
+    data.random = await res.json();
+  } catch (e) {
+    console.log("ERROR loading random profiles", e);
+  }
+
+  try {
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL}/api/discover/trending`
+    );
+    data.trending = await res.json();
+  } catch (e) {
+    console.log("ERROR loading trending profiles", e);
   }
 
   return {
@@ -19,6 +43,19 @@ export async function getServerSideProps(context) {
 }
 
 export default function Popular({ data }) {
+  const [randomProfiles, setRandomProfiles] = useState(data.random);
+
+  const fetchRandom = async () => {
+    try {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/discover/random`
+      );
+      setRandomProfiles(await res.json());
+    } catch (e) {
+      console.log("ERROR refreshing profiles", e);
+    }
+  };
+
   return (
     <>
       <Head>
@@ -30,12 +67,38 @@ export default function Popular({ data }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Page>
-        <h1 className="text-4xl mb-4 font-bold">Discover LinkFree Profiles</h1>
+        <h1 className="text-2xl md:text-4xl mb-4 font-bold">
+          Discover LinkFree Profiles
+        </h1>
 
         <div className="mb-12">
-          <h2 className="text-xl font-bold mb-4">Random LinkFree Profiles</h2>
+          <div className="flex flex-col md:flex-row gap-3 items-center justify-between mb-4">
+            <h2 className="text-md md:text-xl font-bold">
+              Random LinkFree Profiles
+            </h2>
+            <button
+              className="inline-flex gap-3 items-center justify-center rounded-md border border-transparent bg-white px-5 py-3 text-base font-medium text-indigo-600 border-indigo-600 hover:bg-indigo-50"
+              onClick={() => fetchRandom()}
+            >
+              <TbRefresh />
+              Refresh
+            </button>
+          </div>
           <ul className="flex flex-wrap gap-3 justify-center">
-            {data.random.map((profile) => (
+            {randomProfiles.map((profile) => (
+              <li key={profile.username}>
+                <UserCard profile={profile} />
+              </li>
+            ))}
+          </ul>
+        </div>
+
+        <div className="mb-12">
+          <h2 className="text-xl font-bold mb-4">
+            Trending LinkFree Profiles (last 24 hours)
+          </h2>
+          <ul className="flex flex-wrap gap-3 justify-center">
+            {data.trending.map((profile) => (
               <li key={profile.username}>
                 <UserCard profile={profile} />
               </li>
