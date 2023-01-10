df88410157dd52172ede4c6b20c46d276264448f
aadarsh805 <aadarshthakur80501@gmail.com>
2023-01-10 23:22:23 +0530

    added cursor-pointer to the tab list

--- components/event/EventTabs.js
+++ components/event/EventTabs.js
@@ -115,7 +115,7 @@ export function EventTabs({ tabs, setTabs, sortEvents, setSortEvents }) {
                   tab.current
                     ? "border-indigo-500 text-indigo-600"
                     : "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300",
-                  "w-1/4 py-4 px-1 text-center border-b-2 font-medium text-sm flex justify-center items-center gap-2"
+                  "w-1/4 py-4 px-1 text-center border-b-2 font-medium text-sm flex justify-center items-center gap-2 cursor-pointer"
                 )}
                 aria-current={tab.current ? "page" : undefined}
               >
