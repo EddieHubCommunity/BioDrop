d0c665c0bc689228bc1ef8fe85266d47b46b9c89
Eddie Jaoude <eddie@jaoudestudios.com>
2023-01-15 23:58:54 +0000

    fix: boundary condition checks

--- components/user/UserSocials.js
+++ components/user/UserSocials.js
@@ -2,31 +2,17 @@ import getIcon from "../Icon";
 
 function UserSocial({ BASE_URL, username, social }) {
   const DisplayIcon = getIcon(social.icon);
-  const clickLink = async () => {
-    try {
-      const res = await fetch(
-        `${BASE_URL}/api/statistics/${username}/${encodeURIComponent(
-          social.url
-        )}`,
-        { method: "PUT" }
-      );
-      const data = await res.json();
-      setClicks(data.clicks);
-    } catch (e) {
-      // TODO: link not found
-      console.log("ERROR link not found ", e);
-    }
-  };
 
   return (
     <a
-      href={social.url}
-      onClick={clickLink}
+      href={`${BASE_URL}/api/users/${username}/links/${encodeURIComponent(
+        social.url
+      )}`}
       target="_blank"
       rel="noopener noreferrer"
       className="hover:scale-125 transition ease-in-out delay-100"
     >
-      <DisplayIcon aria-label={social.icon.slice(2)}/>
+      <DisplayIcon aria-label={social.icon.slice(2)} />
     </a>
   );
 }
--- pages/api/users/[username]/links/[url].js
+++ pages/api/users/[username]/links/[url].js
@@ -1,26 +1,41 @@
-import Link from "../../../models/Link";
-import Profile from "../../../models/Profile";
-import Stats from "../../../models/Stats";
-import connectMongo from "../../../config/mongo";
+import fs from "fs";
+import path from "path";
+
+import Link from "../../../../../models/Link";
+import Profile from "../../../../../models/Profile";
+import Stats from "../../../../../models/Stats";
+import connectMongo from "../../../../../config/mongo";
 
 export default async function handler(req, res) {
   await connectMongo();
-  const { data } = req.query;
-  const username = data[0];
-  const url = data[1];
 
-  if (req.method != "PUT" || !username || !url) {
+  const { username, url } = req.query;
+
+  if (req.method != "GET") {
     return res
       .status(400)
-      .json({ error: "Invalid request: 'username' and url 'required'" });
+      .json({ error: "Invalid request: GET request required" });
   }
 
-  const getProfile = await Profile.findOne({ username });
+  // load profile json file and check link
+  const filePath = path.join(process.cwd(), "data", `${username}.json`);
+  let data = {};
+  try {
+    data = JSON.parse(fs.readFileSync(filePath, "utf8"));
+  } catch (e) {
+    console.log(`ERROR loading username ${username}`, e);
+    return res.status(404).json({ error: `ERROR ${username} not found` });
+  }
+
+  if (!data.links.find((link) => link.url === url)) {
+    console.log(`ERROR link ${url} not found for username ${username}`);
+    return res.status(404).json({ error: `ERROR ${url} not found` });
+  }
+
+  let getProfile = await Profile.findOne({ username });
 
   if (!getProfile) {
-    return res
-      .status(404)
-      .json({ error: "Invalid request: 'username' not found" });
+    return res.status(404).json({ error: `ERROR ${username} not found` });
   }
 
   const getLink = await Link.findOne({ username, url });
@@ -36,7 +51,10 @@ export default async function handler(req, res) {
         }
       );
     } catch (e) {
-      console.log("ERROR incrementing link", e);
+      console.log(
+        `ERROR incrementing link: ${url} for username ${username}`,
+        e
+      );
     }
   }
 
@@ -48,6 +66,7 @@ export default async function handler(req, res) {
         url,
         clicks: 1,
       });
+
       await Profile.updateOne(
         {
           username,
@@ -58,13 +77,17 @@ export default async function handler(req, res) {
         { new: true, useFindAndModify: false }
       );
     } catch (e) {
-      console.log("ERROR creating link", e);
+      console.log(
+        `ERROR creating link stats: ${url} for username ${username}`,
+        e
+      );
     }
   }
 
   const date = new Date();
   date.setHours(1, 0, 0, 0);
   const getPlatformStats = await Stats.findOne({ date });
+
   if (getPlatformStats) {
     try {
       await Stats.updateOne(
@@ -93,6 +116,5 @@ export default async function handler(req, res) {
     }
   }
 
-  const latestLink = await Link.findOne({ username, url });
-  res.status(201).json(latestLink);
+  return res.status(201).redirect(decodeURIComponent(url));
 }
