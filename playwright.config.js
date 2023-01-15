f7ff992c88385606b99c27d2f523e8b1a90125a3
Dan <35927536+dan-mba@users.noreply.github.com>
2023-01-15 10:31:02 -0500

    improve CI test performance

--- playwright.config.js
+++ playwright.config.js
@@ -38,7 +38,7 @@ const config = {
   //workers: process.env.CI || process.env.GITPOD_MEMORY ? 1 : undefined,
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: "html",
+  reporter: process.env.CI ? [ ['junit', { outputFile: 'results.xml' }], ['list'] ] : 'html',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
