00b9bb4c08f4db95d51875e2e0b02b287712e5f6
Harsh Deep <hdeep61034@gmail.com>
2023-01-15 19:09:19 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -412,16 +412,17 @@ export default function Home({ total, today }) {
                     <div className="flow-root rounded-lg bg-gray-50 px-6 pb-8">
                       <div className="-mt-6">
                         <div>
+                        <Link 
+                          href={feature.path} 
+                             >
                           <span className="inline-flex items-center justify-center rounded-xl bg-indigo-500 p-3 shadow-lg">
-                            <Link
-                            href={feature.path}>
-                          
+                            
                             <feature.icon
                               className="h-8 w-8 text-white"
                               aria-hidden="true"
-                                 />
-                                </Link>
+                                 />    
                           </span>
+                          </Link>
                         </div>
                         <h3 className="mt-8 text-lg font-semibold leading-8 tracking-tight">
                           <Link
