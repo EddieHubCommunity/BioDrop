77b088bd32a3f453378440914ef599b322e1ad80
aadarsh805 <aadarshthakur80501@gmail.com>
2023-01-17 19:47:27 +0530

    changed key to id instead of name

--- components/user/UserLinks.js
+++ components/user/UserLinks.js
@@ -2,6 +2,8 @@ import UserLink from "./UserLink";
 import Alert from "../Alert";
 
 export default function UserLinks({ BASE_URL, data }) {
+  data.links = data.links.map((link, i) => ({ id: i, ...link }));
+
   return (
     <>
       {!data.links && <Alert type="info" message="No links found" />}
@@ -11,7 +13,7 @@ export default function UserLinks({ BASE_URL, data }) {
             data.links.map((link) => (
               <UserLink
                 BASE_URL={BASE_URL}
-                key={link.name}
+                key={link.id}
                 link={link}
                 username={data.username}
                 displayStatsPublic={data.displayStatsPublic}
