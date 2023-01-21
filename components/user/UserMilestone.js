faa043193ef165350abd14255a2fbd6ec26fe967
Manish Bisht <89926834+ManishBisht777@users.noreply.github.com>
2023-01-22 02:48:07 +0530

    feat: future goals (#2383)
    * feature : future goals
    
    * Chores : combine userGoal and userMilestone component
    
    * chores : Delete manishbisht.json
    
    * Fix: merge conflicts
    
    * Fix : using the same code
    
    Co-authored-by: manish bisht <manishbisth9711@gmail.com>
    Co-authored-by: Eddie Jaoude <eddie@jaoudestudios.com>

--- components/user/UserMilestone.js
+++ components/user/UserMilestone.js
@@ -1,18 +1,20 @@
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
 import getIcon from "../Icon";
 
-export default function UserMilestone({ milestone }) {
+export default function UserMilestone({ milestone, isGoal }) {
   const DisplayIcon = getIcon(milestone.icon);
-
   return (
     <a
-      href={milestone.url}
-      key={milestone.url}
+      href={milestone.url ? milestone.url : "/"}
+      key={milestone.url ? milestone.url : milestone.title}
+      className={!milestone.url ? "pointer-events-none" : ""}
       target="_blank"
       rel="noreferrer"
     >
       <li
-        className="py-4 border-l-2 mb-4 pl-2 hover:border-l-4 pr-2 shadow-md"
+        className={`py-4 border-l-2 mb-4 pl-2 hover:border-l-4 pr-2 shadow-md ${
+          isGoal ? "opacity-50" : ""
+        }`}
         style={{
           borderColor: milestone.color,
         }}
--- components/user/UserMilestones.js
+++ components/user/UserMilestones.js
@@ -7,8 +7,24 @@ export default function UserMilestones({ data }) {
       {!data.milestones && <Alert type="info" message="No milestones found" />}
       <ul role="list" className="divide-y divide-gray-200">
         {data.milestones &&
-          data.milestones.map((milestone, key) => (
-            <UserMilestone milestone={milestone} key={key} />
+          data.milestones
+            .filter((milestone) => !milestone.isGoal)
+            .map((milestone, key) => (
+              <UserMilestone milestone={milestone} isGoal={false} key={key} />
+            ))}
+      </ul>
+
+      <div className="flex justify-center items-center gap-3 text-gray-500 my-4  text-xl p-4">
+        <div className="border-t-8 border-dotted border-orange-600 w-[2rem] lg:w-[3rem]"></div>
+        Future Goals
+        <div className="border-t-8 border-dotted border-orange-600 w-[2rem] lg:2-[3rem]"></div>
+      </div>
+
+      <ul role="list" className="divide-y divide-gray-200">
+        {data.milestones
+          .filter((milestone) => milestone.isGoal)
+          .map((goal, key) => (
+            <UserMilestone milestone={goal} isGoal={true} key={key} />
           ))}
       </ul>
     </>
