ad2025fb810ffa359e0a67a07f5b3980528526e9
Eddie Jaoude <eddie@jaoudestudios.com>
2023-01-21 10:47:46 +0000

    feat: abbreviate numbers (#3744)
    * feat: abbreviate numbers (#3484)
    
    * feat: Intl API Performance
    
    * Update Tag.js
    
    * Restore package-lock.json
    
    * fix: refactored abbreviation
    
    Co-authored-by: Krish Gupta <krshg@outlook.com>

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from "next/head";
 import Link from "next/link";
 import Image from "next/image";
-import { abbreviateNumber } from "js-abbreviation-number";
 import { IconContext } from "react-icons";
 import {
   MdOutlinePlayArrow,
@@ -15,6 +14,7 @@ import {
 import { FaMedal } from "react-icons/fa";
 
 import singleUser from "../config/user.json";
+import { abbreviateNumber } from "../services/utils/abbreviateNumbers";
 
 export async function getServerSideProps(context) {
   if (singleUser.username) {
