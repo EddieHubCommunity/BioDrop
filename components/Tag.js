ad181dbfe3ec511e7bd37525a35b277ddbe43631
Eddie Jaoude <eddie@jaoudestudios.com>
2023-01-21 10:16:38 +0000

    fix: refactored abbreviation

--- components/Tag.js
+++ components/Tag.js
@@ -1,5 +1,5 @@
 import Link from "next/link";
-import { abbreviateNumber } from "../util/number";
+import { abbreviateNumber } from "../services/utils/abbreviateNumbers";
 
 export default function Tag({ name, total }) {
   return (
