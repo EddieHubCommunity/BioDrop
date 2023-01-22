85563340f03d07bf29910af5ba40be9728cb6d54
FrozenHearth <vishudbz@gmail.com>
2023-01-22 17:08:11 +0530

    Feat/3636 - Add ellipsis to profile cards

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,5 +7,5 @@ module.exports = {
   theme: {
     extend: {},
   },
-  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms")],
+  plugins: [require("@tailwindcss/typography"), require("@tailwindcss/forms"), require('@tailwindcss/line-clamp')],
 };
