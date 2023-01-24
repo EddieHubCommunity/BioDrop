13a5f7e270a7133c4158e449ac31a878f03ed300
Daniel-jp <danny198672@hotmail.com>
2023-01-24 15:06:31 +0100

    sending project

--- pages/events.js
+++ pages/events.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-no-undef */
 export default function Events({ events }) {
     let categorisedEvents = {
         all: events,
@@ -39,4 +40,36 @@ export default function Events({ events }) {
 
     const [tabs, setTabs] = useState(filters);
     const [eventType, setEventType] = useState("all");
+
+    return (
+
+        <>
+      <Head>
+        <title>Events the community members are going to</title>
+        <meta name="description" content="Search LinkFree user directory" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Page>
+        <h1 className="mb-4 text-4xl font-bold">Community events</h1>
+        <EventTabs
+          tabs={tabs}
+          eventType={eventType}
+          setEventType={setEventType}
+        />
+        <ul role="list" className="mt-6 divide-y divide-gray-200">
+          <h2 className="mb-3 text-lg font-bold text-gray-600 text-md md:text-2xl md:mb-6">
+            {filters.find((filter) => filter.key === eventType).description}
+          </h2>
+
+          {categorisedEvents[eventType]?.map((event) => (
+            <EventCard
+              event={event}
+              username={event.username}
+              key={`${event.name} ${event.username}`}
+            />
+          ))}
+        </ul>
+      </Page>
+    </>
+    );
 }
