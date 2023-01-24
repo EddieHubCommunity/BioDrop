54aa08e5873241e7ac9fa364cb1caeadba5de034
Aadarsh Thakur <95094057+Aadarsh805@users.noreply.github.com>
2023-01-24 13:56:32 +0530

    fixe: links view count sorting (#3496)
    * fixes the links view count not being sorted
    
    * changed key to id instead of name

--- components/user/UserLinks.js
+++ components/user/UserLinks.js
@@ -2,16 +2,18 @@ import UserLink from "./UserLink";
 import Alert from "../Alert";
 
 export default function UserLinks({ BASE_URL, data }) {
+  data.links = data.links.map((link, i) => ({ id: i, ...link }));
+
   return (
     <>
       {!data.links && <Alert type="info" message="No links found" />}
       {data.links && (
         <div className="flex flex-col items-center w-full">
           {data.links &&
-            data.links.map((link, index) => (
+            data.links.map((link) => (
               <UserLink
                 BASE_URL={BASE_URL}
-                key={index}
+                key={link.id}
                 link={link}
                 username={data.username}
                 displayStatsPublic={data.displayStatsPublic}
