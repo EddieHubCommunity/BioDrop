bb836a7c065ae5be23d74626d3eedb379e97ff05
Eddie Jaoude <eddie@jaoudestudios.com>
2023-01-25 20:00:41 +0000

    feat: refactored milestones (#3967)

--- components/user/UserMilestone.js
+++ components/user/UserMilestone.js
@@ -1,4 +1,6 @@
 import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+import { FaExternalLinkAlt } from "react-icons/fa";
+
 import getIcon from "../Icon";
 import Link from "../Link";
 
@@ -6,49 +8,39 @@ export default function UserMilestone({ milestone, isGoal }) {
   const DisplayIcon = getIcon(milestone.icon);
   return (
     <li
-      className={`py-4 border-l-2 mb-4 pl-2 hover:border-l-4 pr-2 shadow-md ${
-        isGoal ? "opacity-50" : ""
-      }`}
+      className={`py-4 ${isGoal ? "opacity-50" : ""}`}
       style={{
         borderColor: milestone.color,
       }}
     >
-      {milestone.url && (
-        <Link
-          href={milestone.url}
-          key={milestone.url}
-          target="_blank"
-          rel="noreferrer"
-        >
-          <div className="flex space-x-3">
-            <DisplayIcon />
-            <div className="flex-1 space-y-1">
-              <div className="flex items-center justify-between">
-                <h3 className="text-lg font-medium">{milestone.title}</h3>
-                <p className="text-sm text-gray-500">{milestone.date}</p>
-              </div>
-              <ReactMarkdown className="text-sm text-gray-500">
-                {milestone.description}
-              </ReactMarkdown>
-            </div>
+      <div className="flex space-x-3">
+        {!milestone.image && milestone.icon && (
+          <DisplayIcon className="h-8 w-8 rounded-full" />
+        )}
+        {milestone.image && (
+          <img
+            className="h-8 w-8 rounded-full"
+            src={milestone.image}
+            alt={milestone.title}
+          />
+        )}
+        <div className="flex-1 space-y-1">
+          <div className="flex items-center justify-between">
+            <h3 className="text-sm font-medium flex gap-2 items-center">
+              <span>{milestone.title}</span>
+              {milestone.url && (
+                <Link href={milestone.url} target="_blank">
+                  <FaExternalLinkAlt />
+                </Link>
+              )}
+            </h3>
+            <p className="text-sm text-gray-500">{milestone.date}</p>
           </div>
-        </Link>
-      )}
-
-      {!milestone.url && (
-        <div className="flex space-x-3">
-          <DisplayIcon />
-          <div className="flex-1 space-y-1">
-            <div className="flex items-center justify-between">
-              <h3 className="text-lg font-medium">{milestone.title}</h3>
-              <p className="text-sm text-gray-500">{milestone.date}</p>
-            </div>
-            <ReactMarkdown className="text-sm text-gray-500">
-              {milestone.description}
-            </ReactMarkdown>
-          </div>
+          <ReactMarkdown className="text-sm text-gray-500">
+            {milestone.description}
+          </ReactMarkdown>
         </div>
-      )}
+      </div>
     </li>
   );
 }
