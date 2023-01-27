600c99b93f804e27a1ffd138dd9b832089b0d955
Ananya Nayak <55504616+Ananya2001-an@users.noreply.github.com>
2023-01-27 14:09:48 +0530

    added more href conditions
    needed to keep user's input in search bar and url search params in sync

--- components/Tag.js
+++ components/Tag.js
@@ -2,17 +2,31 @@ import Link from "../components/Link";
 import { abbreviateNumber } from "../services/utils/abbreviateNumbers";
 import { useRouter } from "next/router";
 
-export default function Tag({ name, total, path, selected }) {
+export default function Tag({ name, total, path,currentInput, selected, tags }) {
   const router = useRouter();
   const { search } = router.query;
+  const tagNames = []
+  tags && tags.slice(0,10).map(tag=> tagNames.push(tag.name))
+
   return (
     <div className="relative">
       <Link
         href={path && path ==='/search'?
         (!selected?
-        (search === ''?
-        `/search?search=${name}`: `/search?search=${search}and${name}`)
-        :`/search?search=${search.split('and').filter(query => query !== name).join('and')}`
+        (currentInput !== '' && search === '' ||currentInput !== '' && search === undefined?
+        `/search?search=${currentInput}and${name}`:currentInput === ''?
+        search === ''||search === undefined?
+        `/search?search=${name}`: search.split('and').every(s => tagNames.indexOf(s)>=0)?
+        `/search?search=${search}and${name}`: search.split('and').filter(s => tagNames.indexOf(s) < 0).reduce((result, word) => result.replace(word, ''), search).split('and').filter(s=> s!=='').join('and') === ''?
+        `/search?search=${name}`:`/search?search=${search.split('and').filter(s => tagNames.indexOf(s) < 0).reduce((result, word) => result.replace(word, ''), search).split('and').filter(s=> s!=='').join('and')}and${name}`
+        
+        :search.split('and').filter(s => tagNames.indexOf(s) < 0).reduce((result, word) => result.replace(word, ''), search).split('and').filter(s=> s!=='').join('and') === ''?
+        `/search?search=${currentInput.split(', ').join('and')}and${name}`:currentInput.split(', ').filter(input => tagNames.indexOf(input) < 0).join('and') === ''?
+        `/search?search=${search.split('and').filter(s => tagNames.indexOf(s) < 0).reduce((result, word) => result.replace(word, ''), search).split('and').filter(s=> s!=='').join('and')}and${name}`:`/search?search=${currentInput.split(', ').filter(input => tagNames.indexOf(input) < 0).join('and')}and${search.split('and').filter(s => tagNames.indexOf(s) < 0).reduce((result, word) => result.replace(word, ''), search).split('and').filter(s=> s!=='').join('and')}and${name}`)
+        : currentInput === ''?
+        `/search?search=${search.split('and').filter(query => query !== name).filter(query => tagNames.indexOf(query) >= 0).join('and')}`:currentInput.split(', ').filter(input => tagNames.indexOf(input) < 0).join('and') === ''?
+        `/search?search=${search.split('and').filter(query => query !== name).filter(query => tagNames.indexOf(query) >= 0).join('and')}`:search.split('and').filter(query => query !== name).filter(query => tagNames.indexOf(query) >= 0).join('and') === ''?
+        `/search?search=${currentInput.split(', ').filter(input => tagNames.indexOf(input) < 0).join('and')}`:`/search?search=${currentInput.split(', ').filter(input => tagNames.indexOf(input) < 0).join('and')}and${search.split('and').filter(query => query !== name).filter(query => tagNames.indexOf(query) >= 0).join('and')}`
         ):`/search?search=${name}`}
         
         className={`flex flex-row p-1 m-2 rounded-lg text-sm font-mono border-2 cursor-pointer shadow-none ${selected? 'hover:border-white bg-orange-600 text-white' : 'hover:border-orange-600'}`}
