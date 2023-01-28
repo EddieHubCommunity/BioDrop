9ed537cc057e4a74471949d4211029c6770c4aaf
Ertan Taner <ertantaner2000@gmail.com>
2023-01-28 04:12:42 +0300

    fix: quickstart video position

--- components/QuickstartVideo.js
+++ components/QuickstartVideo.js
@@ -2,7 +2,7 @@ export default function QuickstartVideo() {
     return (
         <iframe width="410" 
         height="220"
-        className="ml-auto mt-2"
+        className="mt-2"
         src="https://www.youtube.com/embed/05HEeCQSKRE" 
         title="Linkfree Quickstart Video" 
         allow="accelerometer; autoplay;" 
