f8557e5292e8c149cd7392daca6c97f29b81146c
Eddie Jaoude <eddie@jaoudestudios.com>
2023-01-28 15:08:28 +0000

    fix: removed auth persistence

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,12 +1,7 @@
 import NextAuth from "next-auth";
 import GithubProvider from "next-auth/providers/github";
-import { MongoDBAdapter } from "@next-auth/mongodb-adapter";
-import clientPromise from "../../../config/mongoAuth";
 
 export const authOptions = {
-  adapter: MongoDBAdapter(clientPromise, {
-    databaseName: "linkfree",
-  }),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
