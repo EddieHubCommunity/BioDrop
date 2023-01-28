ecf242aa16187d5b9ee3864546f38e2369e7a363
Pravendra Kumar <kumar.pravendra@thinksys.com>
2023-01-28 14:09:11 +0530

    show spinner when route change

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,34 @@
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 import "../styles/globals.css";
 import MultiLayout from "../components/layouts/MultiLayout";
-
+import Progress from "../components/Spinner/RouteSpinner";
 export default function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    const handleStart = (url) => url !== router.asPath && setLoading(true);
+    const handleComplete = (url) => url === router.asPath && setLoading(false);
+
+    router.events.on("routeChangeStart", handleStart);
+    router.events.on("routeChangeComplete", handleComplete);
+    router.events.on("routeChangeError", handleComplete);
+
+    return () => {
+      router.events.off("routeChangeStart", handleStart);
+      router.events.off("routeChangeComplete", handleComplete);
+      router.events.off("routeChangeError", handleComplete);
+    };
+  });
   // Use the layout defined at the page level, if available
   const getLayout =
     Component.getLayout || ((page) => <MultiLayout>{page}</MultiLayout>);
 
-  return getLayout(<Component {...pageProps} />);
+  return (
+    <>
+      {loading && <Progress />}
+      {getLayout(<Component {...pageProps} />)}
+    </>
+  );
 }
