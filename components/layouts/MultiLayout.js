8c5ae9aa2dff0af6814a662935daf913dce84537
Swagata Roy <swagataroy534@gmail.com>
2023-01-31 18:51:47 +0530

    Fixed: Layout issue for viewing docs in mobile view

--- components/layouts/MultiLayout.js
+++ components/layouts/MultiLayout.js
@@ -5,7 +5,7 @@ import Footer from "../Footer";
 
 export default function MultiLayout({ children }) {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col min-h-screen min-w-fit">
       <Navbar />
       <main className="flex-1">{children}</main>
       <Footer />
