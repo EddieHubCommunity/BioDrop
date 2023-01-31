45ce803e06d5506196a8b4fe57a959e9d3cb0330
Dan <35927536+dan-mba@users.noreply.github.com>
2023-01-31 09:40:10 -0500

    Add a11y plugin for storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,4 @@
-import "../styles/globals.css";
+import '../styles/globals.css';
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -12,4 +12,12 @@ export const parameters = {
       basePath: '.'
     },
   },
+  a11y: {
+    options: {
+      runOnly: {
+        type: 'tag',
+        values: ['wcag2a', 'wcag2aa', 'wcag21a', 'wcag21aa']
+      }
+    }
+  }
 };
