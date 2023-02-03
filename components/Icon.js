90300dffaeec4e1912284863631442b545f892e2
Eddie Jaoude <eddie@jaoudestudios.com>
2023-02-03 19:45:09 +0700

    fix: reformatted the code

--- components/Icon.js
+++ components/Icon.js
@@ -1,39 +1,35 @@
-import dynamic from 'next/dynamic';
+import dynamic from "next/dynamic";
 
-// Function to return default icon
-function returnDefault() {
-  return dynamic(() =>
-    import('react-icons/fa').then((mod) => mod.FaGlobe)
-  )
+function defaultIcon() {
+  return dynamic(() => import("react-icons/fa").then((mod) => mod.FaGlobe));
 }
+
 export default function getIcon(name = "FaGlobe") {
   let icon;
 
   switch (name.slice(0, 2)) {
     case "Fa":
       icon = dynamic(() =>
-        import('react-icons/fa')
-          .then((mod) => {
-            let node = mod[name];
-            if(!node) node=returnDefault();
-            return node;
-          })
-      )
+        import("react-icons/fa").then((mod) => {
+          let node = mod[name];
+          if (!node) node = defaultIcon();
+          return node;
+        })
+      );
       break;
     case "Si":
       icon = dynamic(() =>
-        import('react-icons/si')
-          .then((mod) => {
-            let node = mod[name];
-            if(!node) node=returnDefault();
-            return node;
-          })
-      )
+        import("react-icons/si").then((mod) => {
+          let node = mod[name];
+          if (!node) node = defaultIcon();
+          return node;
+        })
+      );
       break;
   }
 
   if (!icon) {
-    return returnDefault();
+    return defaultIcon();
   }
 
   return icon;
