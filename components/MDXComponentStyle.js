501cad6fd0cd2d785546750334085d298994a1e1
syed imam <syedimam1998@gmail.com>
2023-02-04 00:13:01 +0530

    table width fixed

--- components/MDXComponentStyle.js
+++ components/MDXComponentStyle.js
@@ -1,5 +1,6 @@
 export  const MDXComponentStyle= {
  
   h2: (props) => <h2 {...props} className="text-red-500" />,
+  table:(props)=><div className="w-full overflow-auto" ><table {...props}></table></div>
   
 }
