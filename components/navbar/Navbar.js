15df919fedec071b988cf90a306feb19878eadff
alok verma <alokverma749@gmail.com>
2023-02-05 12:07:32 +0530

    fix navbar width

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -88,7 +88,7 @@ export default function Navbar() {
   return (
     <div className="min-h-full" ref={navConRef}>
       <nav className="bg-gray-800">
-        <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="w-full mx-auto px-4 sm:px-6 lg:px-8">
           <div className="flex items-center justify-between h-16">
             <div className="flex items-center">
               <div className="flex-shrink-0">
