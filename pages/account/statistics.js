7ee80e6566cbcf2b9eb5d784dfe7fa47347eef80
Saadat Badgujar <saadatbadgujar@gmail.com>
2023-02-11 10:29:38 +0530

    fix: added md:whitespace-nowrap class

--- pages/account/statistics.js
+++ pages/account/statistics.js
@@ -146,7 +146,7 @@ export default function Search({ data, profile }) {
             {data.links &&
               data.links.individual.map((link) => (
                 <tr key={link.url}>
-                  <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
+                  <td className="md:whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-6">
                     {link.url}
                   </td>
                   <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
