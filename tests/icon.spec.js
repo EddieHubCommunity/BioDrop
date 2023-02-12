0291841c26508bccd0152e5659c646c6b0b00d7e
Maneesh Cheema <ayechico21@gmail.com>
2023-02-12 15:28:09 +0530

    fix: number of popular icons

--- tests/icon.spec.js
+++ tests/icon.spec.js
@@ -28,7 +28,7 @@ test("Icon search page has no results when no search term used", async ({
   const input = page.locator("[name='keyword']");
   await input.type("");
 
-  await expect(page.locator("li")).toHaveCount(0);
+  await expect(page.locator("li")).toHaveCount(4);
 });
 
 test("Icon search page shows no results after typing 2 characters", async ({
