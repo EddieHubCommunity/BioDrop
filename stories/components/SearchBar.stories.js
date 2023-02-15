290716015c06c7ad8179f7f9704abce32edb7d37
Piyush Aggarwal <piyushaggarwal801@gmail.com>
2023-02-15 23:37:45 +0530

    added Search Bar basic story

---
+++ stories/components/SearchBar.stories.js
@@ -0,0 +1,10 @@
+import SearchBar from "../../components/SearchBar";
+
+export default {
+  component: SearchBar,
+  title: "components/Search Bar",
+};
+
+const Template = (args) => <SearchBar {...args} />;
+
+export const Basic = Template.bind({});
