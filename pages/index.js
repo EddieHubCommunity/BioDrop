c6f1f8529bfc140bd45c81cbeb51d62220ecd511
Chinmay Mhatre <chinmaymhatre12@gmail.com>
2023-02-19 18:55:38 +0530

    user testimonial card

--- pages/index.js
+++ pages/index.js
@@ -244,7 +244,7 @@ export default function Home({ total, today }) {
 
       <div className="bg-gray-50 dark:bg-gray-800">
         <div className="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:flex lg:items-center lg:justify-between lg:py-16 lg:px-8">
-          <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
+          <h2 className="text-3xl font-bold tracking-tight text-gray-900 dark:text-gray-300 sm:text-4xl">
             <span className="block">Ready to dive in?</span>
             <span className="block text-indigo-600">
               Add your free Profile today!
