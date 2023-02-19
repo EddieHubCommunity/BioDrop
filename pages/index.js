7e57ab7a5bc567de6b7f0255beee6676889c17ea
Chinmay Mhatre <chinmaymhatre12@gmail.com>
2023-02-19 17:28:13 +0530

    landing page complete

--- pages/index.js
+++ pages/index.js
@@ -212,7 +212,7 @@ export default function Home({ total, today }) {
         />
       </div>
 
-      <div className="bg-white">
+      <div className="bg-white dark:bg-gray-800">
         <div className="mx-auto max-w-7xl py-16 px-4 sm:px-6 lg:px-8">
           <div className="overflow-hidden rounded-lg bg-indigo-700 shadow-xl lg:grid lg:grid-cols-2 lg:gap-4">
             <div className="px-6 pt-10 pb-12 sm:px-16 sm:pt-16 lg:py-16 lg:pr-0 xl:py-20 xl:px-20">
@@ -242,7 +242,7 @@ export default function Home({ total, today }) {
         </div>
       </div>
 
-      <div className="bg-gray-50">
+      <div className="bg-gray-50 dark:bg-gray-800">
         <div className="mx-auto max-w-7xl py-12 px-4 sm:px-6 lg:flex lg:items-center lg:justify-between lg:py-16 lg:px-8">
           <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
             <span className="block">Ready to dive in?</span>
