dcf35fc6d668a562e1a561310ac105614daa27d9
Chinmay Mhatre <chinmaymhatre12@gmail.com>
2023-02-19 22:36:28 +0530

    event card colour add a new colour

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,11 @@ module.exports = {
   ],
   darkMode:"class",
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        dark:"#233042"
+      }
+    },
   },
   plugins: [
     require("@tailwindcss/typography"),
