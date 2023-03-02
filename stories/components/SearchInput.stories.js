d09f75c13628612870a588b7d1091369418b7194
Piyush Aggarwal <piyushaggarwal801@gmail.com>
2023-03-02 19:23:02 +0530

    fix: console errors for no prop passed

--- stories/components/SearchInput.stories.js
+++ stories/components/SearchInput.stories.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import SearchInput from "../../components/SearchInput";
 
 export default {
@@ -5,6 +6,9 @@ export default {
   title: "components/Search Input",
 };
 
-const Template = (args) => <SearchInput {...args} />;
+const Template = (args) => {
+  const [value, setValue] = useState("");
+  return <SearchInput {...args} value={value} onChange={setValue} />;
+};
 
 export const Basic = Template.bind({});
