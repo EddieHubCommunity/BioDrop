b6ee05d2c5ea6953e20f337fccc109a8d8ed9541
Yatharth Verma <32243289+yatharth1706@users.noreply.github.com>
2023-03-06 09:40:45 +0530

    feat: playground and preview feature (#4211)
    * feat: Added playground and preview feature
    
    * feat: Add preview modal and some changes in playground
    
    * fix: reset function
    
    * fix: review changes suggested by eddie
    
    ---------
    
    Co-authored-by: Eddie Jaoude <eddie@jaoudestudios.com>

---
+++ pages/playground.js
@@ -0,0 +1,127 @@
+import PageHead from "../components/PageHead";
+import Page from "../components/Page";
+import { useState } from "react";
+import Alert from "./../components/Alert";
+import { useRouter } from "next/router";
+import PreviewModal from "../components/PreviewModal";
+
+export default function Playground() {
+  const [profileJson, setProfileJson] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+  const [successMsg, setSuccessMsg] = useState("");
+  const router = useRouter();
+  const [gitUsername, setGitUsername] = useState("");
+  const [previewModalState, setPreviewModalState] = useState(false);
+  const [previewModalData, setPreviewModalData] = useState();
+
+  const handleValidateJson = () => {
+    try {
+      if (profileJson) {
+        let parsedJson = JSON.parse(profileJson);
+        setSuccessMsg("Valid Json");
+        setErrMsg("");
+      }
+      return true;
+    } catch (err) {
+      setErrMsg(err.toString());
+      setSuccessMsg("");
+      return false;
+    }
+  };
+
+  const handleFormatJson = () => {
+    try {
+      if (profileJson) {
+        let parsedJson = JSON.parse(profileJson);
+        let formattedJson = JSON.stringify(parsedJson, undefined, 4);
+        setProfileJson(formattedJson);
+        setErrMsg("");
+      }
+    } catch (err) {
+      setErrMsg(err.toString());
+      setSuccessMsg("");
+    }
+  };
+
+  const handleReset = () => {
+    setPreviewModalData();
+    setGitUsername("");
+    setProfileJson("");
+    setSuccessMsg("");
+    setErrMsg("");
+  };
+
+  const handlePreview = () => {
+    try {
+      if (!profileJson) {
+        setErrMsg("Profile json required");
+        setSuccessMsg("");
+      }
+      if (!gitUsername) {
+        setErrMsg("Github username required");
+        setSuccessMsg("");
+      }
+
+      if (gitUsername && profileJson && handleValidateJson()) {
+        setErrMsg("");
+        let actualJson = { username: gitUsername, ...JSON.parse(profileJson) };
+        actualJson = JSON.stringify(actualJson);
+        setPreviewModalData(actualJson);
+        setPreviewModalState(true);
+      }
+    } catch (err) {
+      setErrMsg(err.toString());
+      setSuccessMsg("");
+    }
+  };
+
+  return (
+    <>
+      <PageHead
+        title="Playground"
+        description="Playground for verifying and preview linkfree profile json"
+      />
+
+      <Page>
+        <h1 className="text-2xl md:text-4xl mb-4 font-bold">Playground</h1>
+        <p>Enter github username, profile json below and preview how it will actually look</p>
+        <input
+          type="text"
+          placeholder="Enter github username"
+          className="mt-4 border-2 hover:border-orange-600 transition-all duration-250 ease-linear rounded px-6 py-2 mb-4 block w-full"
+          name="githubUsername"
+          value={gitUsername}
+          onChange={(e) => setGitUsername(e.target.value)}
+        />
+        <textarea
+          placeholder="Enter profile json"
+          className="mt-4 h-80 border-2 hover:border-orange-600 transition-all duration-250 ease-linear rounded px-6 py-2 mb-4 block w-full"
+          name="profileJson"
+          value={profileJson}
+          onChange={(e) => setProfileJson(e.target.value)}
+        />
+        <div className="flex flex-row justify-end mb-3">
+          <span className="mr-3 cursor-pointer" onClick={handleReset}>
+            Reset
+          </span>
+          <span className="mr-3 cursor-pointer" onClick={handleValidateJson}>
+            Validate
+          </span>
+          <span className="mr-3 cursor-pointer" onClick={handleFormatJson}>
+            Format
+          </span>
+          <span className="cursor-pointer" onClick={handlePreview}>
+            Preview
+          </span>
+        </div>
+        {errMsg && <Alert type="error" message={errMsg} />}
+        {successMsg && <Alert type="success" message={successMsg} />}
+        <PreviewModal
+          isOpen={previewModalState}
+          toggle={() => setPreviewModalState(!previewModalState)}
+          data={previewModalData}
+        />
+      </Page>
+    </>
+  );
+}
