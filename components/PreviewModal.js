ebcc0f7cf735ff3bbea8698b9a7e0ece6e32349f
Eddie Jaoude <eddie@jaoudestudios.com>
2023-03-06 11:27:26 +0700

    fix: refactored playground

--- components/PreviewModal.js
+++ components/PreviewModal.js
@@ -1,172 +1,48 @@
-import { useState, useEffect } from "react";
-import Link from "../components/Link";
-import { IconContext } from "react-icons";
-import { FaRegComments } from "react-icons/fa";
-import UserProfile from "../components/user/UserProfile";
-import UserTabs from "../components/user/UserTabs";
-import UserLinks from "../components/user/UserLinks";
-import UserMilestones from "../components/user/UserMilestones";
-import UserTestimonials from "../components/user/UserTestimonials";
-import UserEvents from "../components/user/UserEvents";
+import UserPage from "./user/UserPage";
 
-export default function Preview({ isOpen, toggle, data }) {
-  const [userData, setUserData] = useState();
+export default function Preview({ toggle, data }) {
   const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 
-  useEffect(() => {
-    if (isOpen) {
-      handlePreviewData();
-    }
-  }, [isOpen]);
-
-  const handlePreviewData = () => {
-    let userPreviewData = data;
-    let parsedData = JSON.parse(userPreviewData);
-    setUserData(parsedData);
-    handleTabs(parsedData);
-  };
-
-  const handleTabs = (data) => {
-    let displayTabs = defaultTabs.flatMap((tab) => {
-      if (tab.name === "My Links") {
-        if (data?.links && data?.links.length) {
-          return { ...tab, total: data?.links.length };
-        }
-        return [];
-      }
-      if (tab.name === "Milestones") {
-        if (data?.milestones && data?.milestones.length) {
-          return { ...tab, total: data?.milestones.length };
-        }
-        return [];
-      }
-      if (tab.name === "Testimonials") {
-        if (data?.testimonials && data?.testimonials.length) {
-          return { ...tab, total: data?.testimonials.length };
-        }
-        return [];
-      }
-      if (tab.name === "Events") {
-        if (data?.events && data?.events.length) {
-          return { ...tab, total: data?.events.length };
-        }
-        return [];
-      }
-    });
-
-    setTabs(displayTabs);
-  };
-
-  const defaultTabs = [
-    { name: "My Links", href: "#", current: true, order: "ASC" },
-    { name: "Milestones", href: "#", current: false, order: "ASC" },
-    { name: "Testimonials", href: "#", current: false, order: "ASC" },
-    { name: "Events", href: "#", current: false, order: "ASC" },
-  ];
-
-  const [tabs, setTabs] = useState([]);
-
   return (
-    <>
-      {isOpen && (
-        <div
-          id="defaultModal"
-          tabindex="-1"
-          aria-hidden="true"
-          class="fixed top-0 left-0 right-0 z-50  w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full bg-gray-200/50"
-        >
-          <div class="relative w-full h-full max-w-5xl md:h-auto mx-auto shadow-2xl">
-            {/* <!-- Modal content --> */}
-            <div class="relative bg-white text-gray-900 rounded-lg">
-              {/* <!-- Modal header --> */}
-              <div class="flex items-start justify-between p-4  rounded-t border-gray-600">
-                <h3 class="text-xl font-semibold text-gray-900 ">Preview</h3>
-                <button
-                  type="button"
-                  class="text-gray-400 bg-transparent rounded-lg text-sm p-1.5 ml-auto inline-flex items-center hover:bg-gray-600 hover:text-white"
-                  data-modal-hide="defaultModal"
-                  onClick={toggle}
-                >
-                  <svg
-                    aria-hidden="true"
-                    class="w-5 h-5"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                      clip-rule="evenodd"
-                    ></path>
-                  </svg>
-                  <span class="sr-only">Close modal</span>
-                </button>
-              </div>
-              {/* <!-- Modal body --> */}
-              <div class="p-6 space-y-6">
-                {userData && tabs && (
-                  <>
-                    <UserProfile data={userData} BASE_URL={BASE_URL} />
-
-                    <UserTabs
-                      tabs={tabs}
-                      setTabs={setTabs}
-                      userData={userData}
-                      setUserData={setUserData}
-                    />
-
-                    {tabs.find((tab) => tab.name === "My Links") &&
-                      tabs.find((tab) => tab.name === "My Links").current && (
-                        <UserLinks data={userData} BASE_URL={BASE_URL} />
-                      )}
-
-                    {tabs.find((tab) => tab.name === "Milestones") &&
-                      tabs.find((tab) => tab.name === "Milestones").current && (
-                        <UserMilestones data={userData} />
-                      )}
-
-                    {tabs.find((tab) => tab.name === "Testimonials") &&
-                      tabs.find((tab) => tab.name === "Testimonials").current && (
-                        <UserTestimonials data={userData} />
-                      )}
-
-                    {tabs.find((tab) => tab.name === "Events") &&
-                      tabs.find((tab) => tab.name === "Events").current && (
-                        <UserEvents data={userData} />
-                      )}
-
-                    <Link
-                      href={`https://github.com/EddieHubCommunity/LinkFree/issues/new?labels=testimonial&template=testimonial.yml&title=New+Testimonial+for+${userData?.name}&name=${userData?.username}`}
-                      rel="noopener noreferrer"
-                      target="_blank"
-                    >
-                      <div className="fixed bottom-5 right-5 px-4 py-2 bg-indigo-600 text-white flex items-center gap-1 rounded-full hover:bg-indigo-800">
-                        <IconContext.Provider
-                          value={{ color: "white", style: { verticalAlign: "middle" } }}
-                        >
-                          <FaRegComments />
-                        </IconContext.Provider>
-                        <p className="text-sm font-medium">Add testimonial for {userData?.name}</p>
-                      </div>
-                    </Link>
-                  </>
-                )}
-              </div>
-              <div class="flex items-center p-6 space-x-2  rounded-b">
-                <button
-                  data-modal-hide="defaultModal"
-                  type="button"
-                  class="text-white focus:ring-4 focus:outline-none  font-medium rounded-lg text-sm px-5 py-2.5 text-center bg-blue-600 hover:bg-blue-700 focus:ring-blue-800"
-                  onClick={toggle}
-                >
-                  Cancel
-                </button>
-              </div>
-            </div>
+    <div
+      id="defaultModal"
+      tabindex="-1"
+      aria-hidden="true"
+      class="fixed top-0 left-0 right-0 z-50  w-full p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-modal md:h-full bg-gray-200/50"
+    >
+      <div class="relative w-full h-full max-w-5xl md:h-auto mx-auto shadow-2xl">
+        {/* <!-- Modal content --> */}
+        <div class="relative bg-white text-gray-900 rounded-lg">
+          {/* <!-- Modal header --> */}
+          <div class="flex items-start justify-between p-4  rounded-t border-gray-600">
+            <button
+              type="button"
+              class="text-gray-400 bg-transparent rounded-lg text-sm p-1.5 ml-auto inline-flex items-center hover:bg-gray-600 hover:text-white"
+              data-modal-hide="defaultModal"
+              onClick={toggle}
+            >
+              <svg
+                aria-hidden="true"
+                class="w-5 h-5"
+                fill="currentColor"
+                viewBox="0 0 20 20"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  fill-rule="evenodd"
+                  d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                  clip-rule="evenodd"
+                ></path>
+              </svg>
+              <span class="sr-only">Close modal</span>
+            </button>
+          </div>
+          {/* <!-- Modal body --> */}
+          <div class="p-6 space-y-6">
+            <UserPage data={data} BASE_URL={BASE_URL} />
           </div>
         </div>
-      )}
-    </>
+      </div>
+    </div>
   );
 }
--- pages/playground.js
+++ pages/playground.js
@@ -2,28 +2,30 @@ import PageHead from "../components/PageHead";
 import Page from "../components/Page";
 import { useState } from "react";
 import Alert from "./../components/Alert";
-import { useRouter } from "next/router";
+import Button from "./../components/Button";
 import PreviewModal from "../components/PreviewModal";
+import Input from "../components/form/input";
 
 export default function Playground() {
   const [profileJson, setProfileJson] = useState("");
-  const [errMsg, setErrMsg] = useState("");
-  const [successMsg, setSuccessMsg] = useState("");
-  const router = useRouter();
+  const [validateComplete, setValidateComplete] = useState(false);
+  const [formatComplete, setFormatComplete] = useState(false);
+  const [errorMessage, setErrMsg] = useState("");
+  const [successMessage, setSuccessMsg] = useState("");
   const [gitUsername, setGitUsername] = useState("");
   const [previewModalState, setPreviewModalState] = useState(false);
   const [previewModalData, setPreviewModalData] = useState();
 
   const handleValidateJson = () => {
     try {
-      if (profileJson) {
-        let parsedJson = JSON.parse(profileJson);
-        setSuccessMsg("Valid Json");
-        setErrMsg("");
-      }
+      JSON.parse(profileJson);
+      setSuccessMsg("Valid Json");
+      setErrMsg("");
+      setValidateComplete(true);
       return true;
     } catch (err) {
       setErrMsg(err.toString());
+      setError(true);
       setSuccessMsg("");
       return false;
     }
@@ -31,41 +33,27 @@ export default function Playground() {
 
   const handleFormatJson = () => {
     try {
-      if (profileJson) {
-        let parsedJson = JSON.parse(profileJson);
-        let formattedJson = JSON.stringify(parsedJson, undefined, 4);
-        setProfileJson(formattedJson);
-        setErrMsg("");
-      }
+      let parsedJson = JSON.parse(profileJson);
+      let formattedJson = JSON.stringify(parsedJson, undefined, 4);
+      setProfileJson(formattedJson);
+      setErrMsg("");
+      setFormatComplete(true);
     } catch (err) {
       setErrMsg(err.toString());
       setSuccessMsg("");
     }
   };
 
-  const handleReset = () => {
-    setPreviewModalData();
-    setGitUsername("");
-    setProfileJson("");
-    setSuccessMsg("");
-    setErrMsg("");
-  };
-
   const handlePreview = () => {
-    try {
-      if (!profileJson) {
-        setErrMsg("Profile json required");
-        setSuccessMsg("");
-      }
-      if (!gitUsername) {
-        setErrMsg("Github username required");
-        setSuccessMsg("");
-      }
+    if (!gitUsername) {
+      setErrMsg("Github username required");
+      setSuccessMsg("");
+    }
 
+    try {
       if (gitUsername && profileJson && handleValidateJson()) {
         setErrMsg("");
         let actualJson = { username: gitUsername, ...JSON.parse(profileJson) };
-        actualJson = JSON.stringify(actualJson);
         setPreviewModalData(actualJson);
         setPreviewModalState(true);
       }
@@ -84,13 +72,18 @@ export default function Playground() {
 
       <Page>
         <h1 className="text-2xl md:text-4xl mb-4 font-bold">Playground</h1>
-        <p>Enter github username, profile json below and preview how it will actually look</p>
-        <input
-          type="text"
-          placeholder="Enter github username"
-          className="mt-4 border-2 hover:border-orange-600 transition-all duration-250 ease-linear rounded px-6 py-2 mb-4 block w-full"
-          name="githubUsername"
+
+        {errorMessage && <Alert type="error" message={errorMessage} />}
+        {successMessage && <Alert type="success" message={successMessage} />}
+
+        <p>
+          Enter github username, profile json below and preview how it will
+          actually look
+        </p>
+        <Input
+          name={gitUsername}
           value={gitUsername}
+          placeholder="Enter github username"
           onChange={(e) => setGitUsername(e.target.value)}
         />
         <textarea
@@ -98,29 +91,36 @@ export default function Playground() {
           className="mt-4 h-80 border-2 hover:border-orange-600 transition-all duration-250 ease-linear rounded px-6 py-2 mb-4 block w-full"
           name="profileJson"
           value={profileJson}
-          onChange={(e) => setProfileJson(e.target.value)}
+          onChange={(e) => {
+            setProfileJson(e.target.value);
+            setFormatComplete(false);
+            setValidateComplete(false);
+            setSuccessMsg("");
+            setErrMsg("");
+          }}
         />
-        <div className="flex flex-row justify-end mb-3">
-          <span className="mr-3 cursor-pointer" onClick={handleReset}>
-            Reset
-          </span>
-          <span className="mr-3 cursor-pointer" onClick={handleValidateJson}>
-            Validate
-          </span>
-          <span className="mr-3 cursor-pointer" onClick={handleFormatJson}>
-            Format
-          </span>
-          <span className="cursor-pointer" onClick={handlePreview}>
-            Preview
-          </span>
+        <div className="flex flex-row justify-end mb-3 gap-2">
+          {!formatComplete && (
+            <Button text="Format" onClick={handleFormatJson} primary={false} />
+          )}
+          {formatComplete && !validateComplete && (
+            <Button
+              text="Validate"
+              onClick={handleValidateJson}
+              primary={false}
+            />
+          )}
+          {formatComplete && validateComplete && (
+            <Button text="Preview" onClick={handlePreview} primary={true} />
+          )}
         </div>
-        {errMsg && <Alert type="error" message={errMsg} />}
-        {successMsg && <Alert type="success" message={successMsg} />}
-        <PreviewModal
-          isOpen={previewModalState}
-          toggle={() => setPreviewModalState(!previewModalState)}
-          data={previewModalData}
-        />
+
+        {previewModalData && previewModalState && (
+          <PreviewModal
+            toggle={() => setPreviewModalState(!previewModalState)}
+            data={previewModalData}
+          />
+        )}
       </Page>
     </>
   );
