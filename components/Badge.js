7032164e0e40748312096c724006617ef08ef62d
Vedant Manjrekar <93585735+Vedant-Manjrekar@users.noreply.github.com>
2023-03-28 05:40:18 +0530

    feat: disable clear button when no selection (#5588)
    * Added a feature which disables the clear button when nothing is selected.
    
    * Removed unwanted console.log .

--- components/Badge.js
+++ components/Badge.js
@@ -7,6 +7,7 @@ export default function Badge({
   position,
   className,
   badgeClassName,
+  disable,
   children,
   display = true,
   onClick,
@@ -36,7 +37,11 @@ export default function Badge({
   const badge = (
     <div
       title={title}
-      className={`absolute inline-block rotate-0 skew-x-0 skew-y-0 scale-x-100 scale-y-100 py-1 px-1.5 text-xs leading-none text-center whitespace-nowrap align-baseline font-bold bg-orange-600 text-black rounded-full z-10 ${css} ${badgeClassName}`}
+      className={`absolute inline-block rotate-0 skew-x-0 skew-y-0 scale-x-100 scale-y-100 py-1 px-1.5 text-xs leading-none text-center whitespace-nowrap align-baseline font-bold bg-orange-600 ${
+        disable ? "text-[#EEEEEE]" : "text-black"
+      } ${
+        disable ? "bg-[#C2C2C2]" : "bg-orange-600"
+      } rounded-full z-10 ${css} ${badgeClassName}`}
       onClick={() => (onClick ? onClick() : null)}
     >
       {content}
