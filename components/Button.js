c8751592649812b46ef761f2694e1a627565d350
Eddie Jaoude <eddie@jaoudestudios.com>
2023-03-29 19:23:23 +0700

    fix: map clear topics button (#5701)
    * fix: disabled button for map (#5681)
    
    * Fix for the disabled effect of Button component.
    
    * removed 'primary' style option as it seems unnecessary.
    
    * Fixed disabled effect, removed unwanted changes.
    
    * removed unwanted changes.
    
    * fix: unrequired code changes
    
    ---------
    
    Co-authored-by: Vedant Manjrekar <93585735+Vedant-Manjrekar@users.noreply.github.com>

--- components/Button.js
+++ components/Button.js
@@ -8,12 +8,13 @@ export default function Button({
 }) {
   let className =
     "inline-flex items-center justify-center rounded-md border border-transparent px-5 py-3 text-base font-medium first-letter:bg-white";
-  className += primary
-    ? " text-white bg-indigo-600 hover:bg-indigo-700"
-    : " text-indigo-600 hover:bg-indigo-50";
-  className += disable
-    ? " border-2 border-red bg-gray-50 hover:bg-gray-50 text-gray-200 cursor-not-allowed"
-    : " cursor-pointer";
+  !disable
+    ? (className += primary
+        ? " text-white bg-indigo-600 hover:bg-indigo-700"
+        : " text-indigo-600 hover:bg-indigo-50")
+    : (className += disable
+        ? " border-2 border-red bg-gray-50 hover:bg-gray-50 text-gray-200 cursor-not-allowed"
+        : " cursor-pointer");
 
   const link = (
     <Link className={className} {...restProps}>
