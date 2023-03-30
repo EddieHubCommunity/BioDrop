ae0517e988219b367b925587fa598285699737ae
Chinmay Mhatre <chinmaymhatre12@gmail.com>
2023-03-30 11:15:07 +0530

    removed dark variant on the progress bar

--- components/statistics/ProfileProgress.js
+++ components/statistics/ProfileProgress.js
@@ -6,7 +6,7 @@ const ProfileProgress = ({ progress }) => {
             <h3 className='text-lg font-medium leading-6 text-gray-900'>Profile Completion: <span className='text-indigo-600'>{progress.percentage} %</span></h3>
 
 
-            <div class="w-full bg-gray-200 rounded-full h-2.5 dark:bg-gray-600">
+            <div class="w-full bg-gray-200 rounded-full h-2.5">
                 <div class="bg-indigo-600 h-2.5 my-4 rounded-full" style={{ "width": `${progress.percentage}%` }}></div>
             </div>
         </div>
