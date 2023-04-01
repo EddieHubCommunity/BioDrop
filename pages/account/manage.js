6b2e185f246da2a59e23b41040c26106fdd5ffbf
Eddie Jaoude <eddie@jaoudestudios.com>
2023-04-01 21:08:22 +0700

    docs: manage data with forms

--- pages/account/manage.js
+++ pages/account/manage.js
@@ -2,12 +2,11 @@
 // add manage link to navbar
 
 // 2. extras
+// form links + socials
 // form tags
-// form socials
-// form links
-// form events
 // form milestones
 // form testimonials
+// form events
 
 import { authOptions } from "../api/auth/[...nextauth]";
 import { unstable_getServerSession } from "next-auth/next";
@@ -76,6 +75,19 @@ export default function Manage({ BASE_URL, profile }) {
     const data = await res.json();
   };
 
+  const tabs = [
+    { name: "Profile", href: "#", current: true },
+    { name: "Links", href: "#", current: false },
+    { name: "Tags", href: "#", current: false },
+    { name: "Milestones", href: "#", current: false },
+    { name: "Events", href: "#", current: false },
+    { name: "Testimonials", href: "#", current: false },
+  ];
+
+  function classNames(...classes) {
+    return classes.filter(Boolean).join(" ");
+  }
+
   return (
     <>
       <PageHead
@@ -108,8 +120,47 @@ export default function Manage({ BASE_URL, profile }) {
                 </p>
               </div>
 
+              <div className="border-b border-gray-200 pb-5 sm:pb-0">
+                <div className="mt-3 sm:mt-4">
+                  <div className="sm:hidden">
+                    <label htmlFor="current-tab" className="sr-only">
+                      Select a tab
+                    </label>
+                    <select
+                      id="current-tab"
+                      name="current-tab"
+                      className="block w-full rounded-md border-gray-300 py-2 pl-3 pr-10 text-base focus:border-indigo-500 focus:outline-none focus:ring-indigo-500 sm:text-sm"
+                      defaultValue={tabs.find((tab) => tab.current).name}
+                    >
+                      {tabs.map((tab) => (
+                        <option key={tab.name}>{tab.name}</option>
+                      ))}
+                    </select>
+                  </div>
+                  <div className="hidden sm:block">
+                    <nav className="-mb-px flex space-x-8">
+                      {tabs.map((tab) => (
+                        <a
+                          key={tab.name}
+                          href={tab.href}
+                          className={classNames(
+                            tab.current
+                              ? "border-indigo-500 text-indigo-600"
+                              : "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700",
+                            "whitespace-nowrap border-b-2 px-1 pb-4 text-sm font-medium"
+                          )}
+                          aria-current={tab.current ? "page" : undefined}
+                        >
+                          {tab.name}
+                        </a>
+                      ))}
+                    </nav>
+                  </div>
+                </div>
+              </div>
+
               <div className="space-y-6 sm:space-y-5">
-                <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:border-t sm:border-gray-200 sm:pt-5">
+                <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:pt-5">
                   <label
                     htmlFor="username"
                     className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
