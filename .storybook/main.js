1bc8b5dc356c80f880f851d85ffc04abb1d742f1
Dan <35927536+dan-mba@users.noreply.github.com>
2023-04-03 07:01:40 -0400

    update Storybook to v7 GA

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,22 +1,14 @@
 const config = {
-  stories: [
-    "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
-  ],
-  addons: [
-    "@storybook/addon-essentials",
-    "@storybook/addon-a11y",
-    "@tomfreudenberg/next-auth-mock/storybook",
-  ],
+  stories: ["../stories/**/*.stories.mdx", "../stories/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: ["@storybook/addon-essentials", "@storybook/addon-a11y", "@tomfreudenberg/next-auth-mock/storybook", "@storybook/addon-mdx-gfm"],
   staticDirs: ["../public"],
   framework: {
     name: "@storybook/nextjs",
-    options: {},
+    options: {}
   },
-  env: (config) => ({
+  env: config => ({
     ...config,
-    STORYBOOK_RUN: true,
-  }),
+    STORYBOOK_RUN: true
+  })
 };
-
-export default config
+export default config;
