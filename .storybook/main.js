6e87b6f4c01f291cd24b7f257aa7e78a4717389a
Dan <35927536+dan-mba@users.noreply.github.com>
2023-04-03 07:18:43 -0400

    remove more sb script changes

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,7 +1,7 @@
 const config = {
   stories: [
     "../stories/**/*.stories.mdx",
-    "../stories/**/*.stories.@(js|jsx|ts|tsx)"
+    "../stories/**/*.stories.@(js|jsx|ts|tsx)",
   ],
   addons: [
     "@storybook/addon-essentials",
@@ -19,4 +19,5 @@ const config = {
     STORYBOOK_RUN: true,
   }),
 };
+
 export default config;
