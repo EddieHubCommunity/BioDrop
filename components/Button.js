897ea0e96245b9731c2ac287cc19ec2e029f868b
Eddie Jaoude <eddie@jaoudestudios.com>
2023-04-03 12:32:44 +0700

    fix: buttons for dark mode

--- components/Button.js
+++ components/Button.js
@@ -10,8 +10,8 @@ export default function Button({
     "inline-flex items-center justify-center rounded-md border border-transparent px-5 py-3 text-base font-medium first-letter:bg-white";
   !disable
     ? (className += primary
-        ? " text-white bg-indigo-600 hover:bg-indigo-700"
-        : " text-indigo-600 hover:bg-indigo-50")
+        ? " text-white bg-secondary-medium hover:bg-secondary-high"
+        : " text-secondary-medium dark:text-secondary-high dark:hover:text-secondary-low dark:bg-secondary-low hover:bg-secondary-medium dark:hover:bg-secondary-medium")
     : (className += disable
         ? " border-2 border-red bg-gray-50 hover:bg-gray-50 text-gray-200 cursor-not-allowed"
         : " cursor-pointer");
