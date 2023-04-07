e6c1f7cd39cded71646d4b6feb46eb4b2eb1147d
Eddie Jaoude <eddie@jaoudestudios.com>
2023-04-07 17:18:10 +0700

    fix: dark mode button on map

--- components/navbar/Navbar.js
+++ components/navbar/Navbar.js
@@ -10,6 +10,8 @@ import getIcon from "@components/Icon";
 import { useTheme } from "next-themes";
 
 const FaGithub = getIcon("FaGithub");
+const FaRegMoon = getIcon("FaRegMoon");
+const FaSun = getIcon("FaSun");
 
 export default function Navbar() {
   const { systemTheme, theme, setTheme } = useTheme();
@@ -35,45 +37,19 @@ export default function Navbar() {
 
     if (currentTheme === "dark") {
       return (
-        <button className="px-2" onClick={() => setTheme("light")}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke-width="1.5"
-            stroke="white"
-            class="w-6 h-6"
-          >
-            <path
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
-            />
-          </svg>
+        <button className="p-2 text-white" onClick={() => setTheme("light")}>
+          <FaSun />
         </button>
       );
     }
 
     return (
       <button
-        className="px-2"
+        className="p-2 text-white"
         onClick={() => setTheme("dark")}
         aria-label="Toggle Theme"
       >
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          stroke-width="1.5"
-          stroke="white"
-          class="w-6 h-6"
-        >
-          <path
-            stroke-linecap="round"
-            stroke-linejoin="round"
-            d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
-          />
-        </svg>
+        <FaRegMoon />
       </button>
     );
   };
