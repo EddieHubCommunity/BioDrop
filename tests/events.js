1cd61a8c9cf72d90dc2622171b85472bfdeac77e
Dan <35927536+dan-mba@users.noreply.github.com>
2023-04-11 07:18:54 -0400

    add dark mode accessibility tests

--- tests/events.js
+++ tests/events.js
@@ -15,10 +15,26 @@ test.fixme("Events listed", async ({ page }) => {
   await expect(page.locator("li")).toBeGreaterThan(1);
 });
 
-test("should pass axe wcag accessibility tests", async ({ page }) => {
-  await page.goto("/events");
-  const accessibilityScanResults = await new AxeBuilder({ page })
-    .withTags(["wcag2a", "wcag2aa", "wcag21a", "wcag21aa"])
-    .analyze();
-  expect(accessibilityScanResults.violations).toEqual([]);
+test.describe("accessibility tests (light)", () => {
+  test.use({ colorScheme: 'light' });
+
+  test("should pass axe wcag accessibility tests", async ({ page }) => {
+    await page.goto("/events");
+    const accessibilityScanResults = await new AxeBuilder({ page })
+      .withTags(["wcag2a", "wcag2aa", "wcag21a", "wcag21aa"])
+      .analyze();
+    expect(accessibilityScanResults.violations).toEqual([]);
+  });
+});
+
+test.describe("accessibility tests (dark)", () => {
+  test.use({ colorScheme: 'dark' });
+  
+  test("should pass axe wcag accessibility tests (dark)", async ({ page }) => {
+    await page.goto("/events");
+    const accessibilityScanResults = await new AxeBuilder({ page })
+      .withTags(["wcag2a", "wcag2aa", "wcag21a", "wcag21aa"])
+      .analyze();
+    expect(accessibilityScanResults.violations).toEqual([]);
+  });
 });
