712967f0508a5608a4e871ce4330e15e6ddf34cf
pratham <darkvoid6565@gmail.com>
2023-04-12 17:45:25 +0530

    li component moved to a separate file

--- components/mdx/ComponentStyle.js
+++ components/mdx/ComponentStyle.js
@@ -1,8 +1,9 @@
+import { Li } from "./Li";
 export const ComponentStyle = {
   table: (props) => (
     <div className="w-full overflow-auto">
       <table {...props}></table>
     </div>
   ),
-  li: (props) => <li className="w-full break-all" {...props}></li>,
+  li: (props) => <Li {...props}></Li>,
 };
---
+++ components/mdx/Li.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Li = (props) => <li className="w-full break-all" {...props}></li>;
+
+export default Li;
