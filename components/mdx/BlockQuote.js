fbe85b2da6eae8846f218236c8f34dbd3c20d090
Chinmay Mhatre <chinmaymhatre12@gmail.com>
2023-04-12 07:16:06 +0530

    fix: dark mode docs, a, blockquote, strong tags (#6000)
    * added custom a and strong component to fix darkmode bugs
    
    * block quote fix

---
+++ components/mdx/BlockQuote.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BlockQuote = ({children}) => {
+    return (
+        <blockquote className='text-primary-medium dark:text-primary-low-medium'>{children}</blockquote>
+    )
+}
+
+export default BlockQuote
