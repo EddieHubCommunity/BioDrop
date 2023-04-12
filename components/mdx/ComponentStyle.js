77dbc3ff695b0c393ebc80c7c8b2ec987fb5c1bb
Eddie Jaoude <eddie@jaoudestudios.com>
2023-04-12 21:40:50 +0700

    Update components/mdx/ComponentStyle.js

--- components/mdx/ComponentStyle.js
+++ components/mdx/ComponentStyle.js
@@ -1,4 +1,4 @@
-import Li  from "./Li";
+import Li  from "@components/mdx/Li";
 import Link from "@components/Link";
 import Strong from "@components/mdx/Strong";
 import BlockQuote from "@components/mdx/BlockQuote";
