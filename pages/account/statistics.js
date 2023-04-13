9ad504122e9a11182f61f11f3a789ecfeef7be54
Landouda <landoudalinda2468.com@gmail.com>
2023-04-13 04:12:05 +0100

    feat: account hint section draft proposal

--- pages/account/statistics.js
+++ pages/account/statistics.js
@@ -1,7 +1,15 @@
 import { authOptions } from "../api/auth/[...nextauth]";
 import { unstable_getServerSession } from "next-auth/next";
 import ProgressBar from "@components/statistics/ProgressBar";
-
+import { MDXProvider } from '@mdx-js/react'
+import Bio from '../../components/hints/bio.mdx'
+import Links from '../../components/hints/links.mdx'
+import Milestones from '../../components/hints/milestones.mdx'
+import Tags from '../../components/hints/tags.mdx'
+import Socials from '../../components/hints/socials.mdx'
+import Location from '../../components/hints/location.mdx'
+import Testimonials from '../../components/hints/testimonials.mdx'
+import Link from "next/link";
 import {
   BarChart,
   Bar,
@@ -19,6 +27,8 @@ import Page from "@components/Page";
 import PageHead from "@components/PageHead";
 import { abbreviateNumber } from "@services/utils/abbreviateNumbers";
 import BasicCards from "@components/statistics/BasicCards";
+import { FiChevronLeft, FiChevronRight } from "react-icons/fi";
+import { useState } from "react";
 
 export async function getServerSideProps(context) {
   const { req, res } = context;
@@ -104,6 +114,17 @@ export default function Statistics({ data, profile, progress }) {
     };
   });
 
+  const profileSectionsSpecification = {
+    "bio": { value: "bio", link: "https://linkfree.eddiehub.io/docs/how-to-guides/bio", description: "Describe in a few words who you are.", component: Bio },
+    "links": { value: "links", link: "https://linkfree.eddiehub.io/docs/how-to-guides/links", description: "Let people discover all your great content in one place.", component: Links },
+    "milestones": { value: "milestones", link: "https://linkfree.eddiehub.io/docs/how-to-guides/milestones", description: "Demonstrate the highlights of your career.", component: Milestones },
+    "tags": { value: "tags", link: "https://linkfree.eddiehub.io/docs/how-to-guides/tags", description: "These are searchable keywords for people to discover your Profile.", component: Tags },
+    "socials": { value: "socials", link: "https://linkfree.eddiehub.io/docs/how-to-guides/socials-shortcuts", description: "Add a shortcut to your favourite social media accounts.", component: Socials },
+    "location": { value: "location", link: "https://linkfree.eddiehub.io/docs/how-to-guides/events", description: "Add events you either hosted or attended.", component: Location },
+    "testimonials": { value: "testimonials", link: "https://linkfree.eddiehub.io/docs/how-to-guides/testimonials", description: "Show off the great feedback you have received.", component: Testimonials },
+  }
+  const [hint, setHint] = useState(profileSectionsSpecification[progress.missing[0]])
+
   const cardData = [
     {
       name: "Total views",
@@ -143,6 +164,45 @@ export default function Statistics({ data, profile, progress }) {
           </span>
 
           <ProgressBar progress={progress} />
+
+          <span className="flex flex-col justify-between mt-6">
+            <span className="text-lg font-medium text-primary-medium dark:text-primary-low">
+              Here is how you can stengthen your profile 💪
+            </span>
+            <div className="flex flex-row justify-center items-center border p-4 my-6 dark:border-primary-medium">
+              <p className="mt-1 text-sm text-primary-medium dark:text-primary-low-medium w-2/12 ">
+                {hint.description}
+              </p>
+              <span className="w-8/12 mx-auto">
+                <MDXProvider>
+                  <hint.component />
+                </MDXProvider>
+              </span>
+            </div>
+            <div className="flex justify-between items-center">
+              <div className="flex items-center">
+                <button
+                  onClick={() => setHint(profileSectionsSpecification[progress.missing[progress.missing.indexOf(hint.value) == 0 ? progress.missing.length - 1 : progress.missing.indexOf(hint.value) - 1]])}
+                  type="button"
+                  className="inline-flex items-center justify-center p-2 rounded-md text-primary-black hover:text-white hover:bg-primary-medium focus:outline-offset-2 mx-auto"
+                  aria-controls="mobile-menu"
+                > <FiChevronLeft /> Prev </button>
+                <button
+                  onClick={() => setHint(profileSectionsSpecification[progress.missing[(progress.missing.indexOf(hint.value) + 1) % (progress.missing.length)]])}
+                  type="button"
+                  className="inline-flex items-center justify-center p-2 rounded-md text-primary-black hover:text-white hover:bg-primary-medium focus:outline-offset-2 mx-auto"
+                  aria-controls="mobile-menu"
+                > Next <FiChevronRight /> </button>
+              </div>
+              <Link
+                href={hint.link}
+                rel="noopener noreferrer"
+                target="_blank"
+                className="bg-secondary-high inline-flex items-center justify-center p-2 rounded-md text-white  hover:text-white hover:bg-primary-medium focus:outline-offset-2 "
+              > Learn More </Link>
+            </div>
+
+          </span>
         </div>
 
         <h1 className="text-4xl mb-4 font-bold">
