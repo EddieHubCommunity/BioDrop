7fe9c4c288b81b804433b0951491c597a05968d7
ajaxbo360 <ajakak.2016@gmail.com>
2023-04-13 12:58:26 +0000

    Fix:conflict in componentStyle

--- components/mdx/ComponentStyle.js
+++ components/mdx/ComponentStyle.js
@@ -1,20 +1,10 @@
 import Table from "./Table";
-export const ComponentStyle = {
-  table: Table,
-  li: (props) => <li className="w-full break-all" {...props}></li>,
-};
-
 import Li  from "@components/mdx/Li";
 import Link from "@components/Link";
 import Strong from "@components/mdx/Strong";
 import BlockQuote from "@components/mdx/BlockQuote";
-
 export const ComponentStyle = {
-  table: (props) => (
-    <div className="w-full overflow-auto">
-      <table {...props}></table>
-    </div>
-  ),
+  table:Table,
   li: Li,
   a: Link,
   strong: Strong,
