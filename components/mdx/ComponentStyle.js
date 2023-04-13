389286cf17db9ed1f220059c1dfc3661dfae4766
ajaxbo360 <ajakak.2016@gmail.com>
2023-04-13 13:21:07 +0000

    Fix: coding style not properly followed

--- components/mdx/ComponentStyle.js
+++ components/mdx/ComponentStyle.js
@@ -1,10 +1,10 @@
-import Table from "./Table";
+import Table from "@components/mdx/Table";
 import Li  from "@components/mdx/Li";
 import Link from "@components/Link";
 import Strong from "@components/mdx/Strong";
 import BlockQuote from "@components/mdx/BlockQuote";
 export const ComponentStyle = {
-  table:Table,
+  table: Table,
   li: Li,
   a: Link,
   strong: Strong,
