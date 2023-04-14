764ed34f6a7add9dc980f5d19fa2578c64d0ccd8
Grut <akashsah946@gmail.com>
2023-04-14 07:04:27 +0000

    added clarity to the Events Page

--- components/user/UserEvents.js
+++ components/user/UserEvents.js
@@ -5,12 +5,13 @@ import Alert from "@components/Alert";
 import EventKey from "@components/event/EventKey";
 
 export default function UserEvents({ data }) {
-  const [eventType, seteventType] = useState("future");
+  const [eventType, setEventType] = useState("all");
   const futureEvents = data.events.filter(
     (event) => new Date(event.date.start) > new Date()
   );
 
   let categorisedEvents = {
+    all: data.events,
     future: futureEvents,
     ongoing: data.events.filter(
       (event) =>
@@ -27,15 +28,35 @@ export default function UserEvents({ data }) {
       .sort((a, b) => new Date(b.date.start) - new Date(a.date.start)),
   };
 
+  const handleEventTypeChange = (event) => {
+    setEventType(event.target.value);
+  };
+
   return (
     <div className="mt-6">
-      <EventKey
-        categorisedEvents={categorisedEvents}
-        onToggleEventType={(newValue) => seteventType(newValue)}
-      />
+      <label
+        htmlFor="event-type-select"
+        className=" mr-2 font-semibold text-gray-700"
+      >
+        Select Event Type:
+      </label>
+      <select
+        id="event-type-select"
+        value={eventType}
+        onChange={handleEventTypeChange}
+        className="block w-full max-w-lg px-4 py-2 border border-orange-300 rounded-md shadow-sm focus:outline-none focus:ring-2 focus:ring-orange-500 focus:border-orange-500"
+      >
+        <option value="all">All Events</option>
+        <option value="future">Future Events</option>
+        <option value="ongoing">Ongoing Events</option>
+        <option value="virtual">Virtual Events</option>
+        <option value="inPerson">In-Person Events</option>
+        <option value="cfpOpen">Events with Open CFP</option>
+        <option value="past">Past Events</option>
+      </select>
 
       {!data.events && <Alert type="info" message="No events found" />}
-      <ul role="list" className="divide-y divide-primary-low">
+      <ul role="list" className="divide-y divide-primary-low mt-4">
         {data.events &&
           categorisedEvents[eventType].map((event, index) => (
             <EventCard event={event} key={index} />
