a868be8e7d60c306ec31a88dbf8a9720bb30de60
Eddie Jaoude <eddie@jaoudestudios.com>
2023-04-18 10:33:53 +0700

    fix: saving mock user to db

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -1,12 +1,16 @@
 import { test, expect } from "@playwright/test";
+import { login, logout } from "./setup/auth";
 
-test("User can login to dashboard", async ({ page }) => {
-    /*  if the global config worked to set a logged in state, 
-     I would expect 1 of 2 things to happen here, 
-     either the Login button shouldn't show bc the context is already set 
-     OR clicking on the github button would just work */
-    await page.goto("/");
-    await page.click('a >> text=Login');
-    await page.click('button >> text=Sign in with GitHub');
-    await page.screenshot({ path: 'screenshot.png' });
-  });
+test("Guest user cannot access dashboard", async ({ page }) => {
+  // fixture: make sure user is logged out
+  await logout();
+  await page.goto("/account/statistics");
+  await expect(page).toHaveURL(/\//);
+});
+
+test("Logged in user can access dashboard", async ({ page }) => {
+  // fixture: make sure user is logged in
+  await login();
+  await page.goto("/account/statistics");
+  await expect(page).toHaveURL(/account\/statistics/);
+});
---
+++ tests/setup/auth.js
@@ -0,0 +1,89 @@
+import { chromium } from "@playwright/test";
+import path from "node:path";
+import connectMongo from "@config/mongo";
+
+import User from "@models/User";
+import Session from "@models/Session";
+import Account from "@models/Account";
+
+const login = async () => {
+  await connectMongo();
+
+  const storagePath = path.resolve(__dirname, "storageState.json");
+  const date = new Date();
+  const sessionToken = "04456e41-ec3b-4edf-92c1-48c14e57cacd2";
+  let testUser;
+
+  try {
+    testUser = await User.findOneAndUpdate(
+      { email: "testemail@test.com" },
+      {
+        name: "Automate Test",
+        image: "https://github.com/eddiejaoude.png",
+        emailVerified: null,
+      },
+      { new: true, upsert: true }
+    );
+  } catch (e) {
+    console.log("Test user creation failed", e);
+  }
+
+  try {
+    const session = await Session.findOneAndUpdate(
+      {
+        userId: testUser._id,
+      },
+      {
+        expires: new Date(date.getFullYear(), date.getMonth() + 1, 0),
+        sessionToken: sessionToken,
+      },
+      { new: true, upsert: true }
+    );
+  } catch (e) {
+    console.log("Test session creation failed", e);
+  }
+
+  try {
+    const account = await Account.findOneAndUpdate(
+      {
+        userId: testUser._id,
+      },
+      {
+        type: "oauth",
+        provider: "github",
+        providerAccountId: "2222222",
+        access_token: "ggg_zZl1pWIvKkf3UDynZ09zLvuyZsm1yC0YoRPt",
+        token_type: "bearer",
+        scope: "read:org,read:user,repo,user:email,test:all",
+      },
+      { new: true, upsert: true }
+    );
+  } catch (e) {
+    console.log(e, `failed for step 3`);
+  }
+
+  const browser = await chromium.launch();
+  const context = await browser.newContext({ storageState: storagePath });
+  // 4.1. This cookie is what `NextAuth` will look after to validate if our user is authenticated
+  // Please note that the `value` of the cookie **must be the same** as the `sessionToken` we added in `step 2.`
+  await context.addCookies([
+    {
+      name: "next-auth.session-token",
+      value: sessionToken,
+      domain: "localhost",
+      path: "/",
+      httpOnly: true,
+      sameSite: "Lax",
+      expires: 1661406204,
+    },
+  ]);
+  await context.storageState({ path: storagePath });
+  await browser.close();
+};
+const logout = async () => {
+  const browser = await chromium.launch();
+  const context = await browser.newContext();
+  await context.clearCookies();
+};
+
+export { login, logout };
