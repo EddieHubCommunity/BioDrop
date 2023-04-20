d7f724d07681a85faa65edfb89384b1164c86e9f
Nanak <nanakbandyopadhyay@hotmail.com>
2023-04-20 22:19:15 +0530

    update heading component with predefined sizes

--- components/mdx/Heading.js
+++ components/mdx/Heading.js
@@ -1,6 +1,24 @@
-const Heading = ({ children, id, fontSize = "text-4xl" }) => {
+const Heading = ({ children, id, fontSize = "medium", customClasses }) => {
+  let fontSizeClass;
+  switch (fontSize) {
+    case "small":
+      fontSizeClass = "text-base md:text-lg";
+      break;
+    case "medium":
+      fontSizeClass = "text-xl md:text-2xl";
+      break;
+    case "large":
+      fontSizeClass = "text-xl md:text-3xl";
+      break;
+    case "extraLarge":
+      fontSizeClass = "text-3xl md:text-5xl";
+      break;
+    default:
+      fontSizeClass = "text-xl md:text-2xl";
+      break;
+  }
   return (
-    <h1 id={id} className={fontSize}>
+    <h1 id={id} className={`font-bold mb-4 ${fontSizeClass} ${customClasses}`}>
       {children}
     </h1>
   );
