90764934f66219f809c76c6c1255ea4a5d20e94b
itsosmx <o.h1277@yahoo.com>
2023-04-24 17:28:05 +0200

    Adding transitions to buttons (#6331)

--- components/Button.js
+++ components/Button.js
@@ -7,14 +7,14 @@ export default function Button({
   ...restProps
 }) {
   let className =
-    "inline-flex items-center justify-center rounded-md border border-transparent px-5 py-3 text-base font-medium first-letter:bg-white";
+    "inline-flex items-center justify-center rounded-md border border-transparent px-5 py-3 text-base font-medium first-letter:bg-white transition duration-300 ease-in-out";
   !disable
     ? (className += primary
-        ? " text-white bg-secondary-medium hover:bg-secondary-high"
-        : " text-secondary-high dark:text-secondary-high hover:text-white dark:hover:text-white dark:bg-secondary-low hover:bg-secondary-medium dark:hover:bg-secondary-medium")
+      ? " text-white bg-secondary-medium hover:bg-secondary-high"
+      : " text-secondary-high dark:text-secondary-high hover:text-white dark:hover:text-white dark:bg-secondary-low hover:bg-secondary-medium dark:hover:bg-secondary-medium")
     : (className += disable
-        ? " border-2 border-red border shadow-sm bg-primary-low text-primary-medium cursor-not-allowed "
-        : " cursor-pointer");
+      ? " border-2 border-red border shadow-sm bg-primary-low text-primary-medium cursor-not-allowed "
+      : " cursor-pointer");
 
   const link = (
     <Link className={className} {...restProps}>
