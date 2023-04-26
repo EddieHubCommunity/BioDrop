632f30323e2ccb440648e0e688792bdd0d2e37e8
Sambhav Gandhi <83087385+geeky-sambhav@users.noreply.github.com>
2023-04-26 19:08:41 +0530

    message

--- pages/index.js
+++ pages/index.js
@@ -104,7 +104,7 @@ export default function Home({ total, today }) {
       description:
         "Search Profiles not just by name but also by tags. This is a great way to connect with people and grow your network.",
       imageSrc:
-        "https://user-images.githubusercontent.com/55504616/216514019-abbd642a-150e-4ebf-acbf-41651bbddcc4.png",
+        "https://user-images.githubusercontent.com/83087385/234587034-baaf983f-1a91-4d2c-b28c-e9f4c9bb9509.png",
       imageAlt:
         "LinkFree screenshot of search page using tags for searching profiles.",
     },
