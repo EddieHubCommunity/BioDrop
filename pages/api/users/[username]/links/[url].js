9ce405983df18b79339cc582db6490621961f8fb
Danish Shaikh <110280126+Shaikh-Danish@users.noreply.github.com>
2023-05-02 09:36:35 +0530

    feat: single import for models (#5654) (#6147)
    * fix speaker details should be hidden in mobile #5962
    
    * Update components/event/EventCard.js
    
    * index imports in models
    
    * index imports in models
    
    ---------
    
    Co-authored-by: Eddie Jaoude <eddie@jaoudestudios.com>

--- pages/api/users/[username]/links/[url].js
+++ pages/api/users/[username]/links/[url].js
@@ -1,10 +1,6 @@
 import connectMongo from "@config/mongo";
 import logger from "@config/logger";
-
-import Link from "@models/Link";
-import Profile from "@models/Profile";
-import LinkStats from "@models/LinkStats";
-import Stats from "@models/Stats";
+import { Link, Profile, LinkStats, Stats } from "@models/index";
 import findOneByUsernameBasic from "@services/profiles/findOneByUsernameBasic";
 
 export default async function handler(req, res) {
