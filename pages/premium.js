31d8b1bd583eede6b1a0a1cfcbc36a94dabdaf24
Chinmay Mhatre <chinmaymhatre12@gmail.com>
2023-05-08 18:50:32 +0530

    Update pages/premium.js
    Co-authored-by: Eddie Jaoude <eddie@jaoudestudios.com>

--- pages/premium.js
+++ pages/premium.js
@@ -59,7 +59,7 @@ export default function Premium() {
           <div className="mx-auto max-w-7xl px-6 lg:px-8">
             <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-3">
               <div>
-                <h2 className="text-base font-semibold leading-7 text-secondary-medium dark:text-secondary-low   ">
+                <h2 className="text-base font-semibold leading-7 text-secondary-medium dark:text-secondary-low ">
                   Super charge your Profile from $5/month
                 </h2>
                 <p className="mt-2 text-2xl font-bold tracking-tight text-primary-high dark:text-white sm:text-3xl">
