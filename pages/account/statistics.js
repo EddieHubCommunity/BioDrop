e6dcbb29d47ac603726357ee2065d95c1da6c4c3
Eddie Jaoude <eddie@jaoudestudios.com>
2023-05-11 09:33:47 +0700

    feat: refactored toggle stats toggles

--- pages/account/statistics.js
+++ pages/account/statistics.js
@@ -139,22 +139,20 @@ export default function Statistics({ data, profile, progress }) {
     },
   ];
 
-  const updateProfile = async () => {
-    const res = await fetch(
-      `${process.env.NEXT_PUBLIC_BASE_URL}/api/account/manage/profile`,
-      {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({ customise: { hideFooter, hideNavbar } }),
-      }
-    );
-    profile = await res.json();
-  };
-
   useEffect(() => {
-    updateProfile();
+    (async () => {
+      const res = await fetch(
+        `${process.env.NEXT_PUBLIC_BASE_URL}/api/account/manage/profile`,
+        {
+          method: "PUT",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ customise: { hideFooter, hideNavbar } }),
+        }
+      );
+      profile = await res.json();
+    })();
   }, [hideNavbar, hideFooter]);
 
   return (
