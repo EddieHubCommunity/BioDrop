ee5a23ffa99ea90dca52edfa68a1e45aed689fcb
Segacnd <segabelka@yandex.ru>
2023-05-12 17:48:23 +0300

    create 404 page

---
+++ pages/404.js
@@ -0,0 +1,28 @@
+import Link from "@components/Link";
+
+export default function NotFoundPage() {
+  return (
+    <section className="flex items-center h-full p-16 dark:bg-gray-900 dark:text-gray-100">
+      <div className="container flex flex-col items-center justify-center px-5 mx-auto my-8">
+        <div className="max-w-md text-center">
+          <h2 className="mb-8 font-extrabold text-9xl dark:text-gray-600">
+            <span className="sr-only">Error</span>404
+          </h2>
+          <p className="text-2xl font-semibold md:text-3xl">
+            Sorry, we couldn’t find this page.
+          </p>
+          <p className="mt-4 mb-8 dark:text-gray-400">
+            But dont worry, you can find plenty of other things on our homepage.
+          </p>
+          <Link
+            rel="noopener noreferrer"
+            href="/"
+            className="px-8 py-3 font-semibold rounded bg-indigo-400"
+          >
+            Back to homepage
+          </Link>
+        </div>
+      </div>
+    </section>
+  );
+}
