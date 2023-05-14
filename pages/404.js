6d39d2f92028a8126e73664be58715c297096bfb
Eddie Jaoude <eddie@jaoudestudios.com>
2023-05-14 11:05:57 +0700

    fix: 404 page

--- pages/404.js
+++ pages/404.js
@@ -1,26 +1,27 @@
-import Link from "@components/Link";
+import Image from "next/image";
 
-export default function NotFoundPage() {
+import Button from "@components/Button";
+
+export default function Page404() {
   return (
     <div className="grid min-h-full place-items-center px-6 py-24 sm:py-32 lg:px-8">
       <div className="text-center">
-        <p className="text-base font-semibold text-indigo-600">404</p>
-        <h1 className="mt-4 text-3xl font-bold tracking-tight text-gray-900 sm:text-5xl">
+        <p className="text-base font-semibold text-secondary-high">404</p>
+        <h1 className="mt-4 text-3xl font-bold tracking-tight text-primary-high dark:text-primary-low sm:text-5xl">
           Page not found
         </h1>
-        <p className="mt-6 text-base leading-7 text-gray-600">
-          Sorry, we couldn’t find the page you’re looking for.
+        <p className="m-6 text-base leading-7 text-primary-medium dark:text-primary-medium-low">
+          Sorry, we could not find the page you are looking for.
         </p>
+        <Image
+          src="https://user-images.githubusercontent.com/624760/114314273-eaae0100-9af1-11eb-955a-4039657fe85a.png"
+          alt="EddieHub mascot scared pose"
+          width={300}
+          height={300}
+        />
         <div className="mt-10 flex items-center justify-center gap-x-6">
-          <Link
-            href="/"
-            className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-          >
-            Go back home
-          </Link>
-          <Link href="/docs" className="text-sm font-semibold ">
-            Contact support <span aria-hidden="true">&rarr;</span>
-          </Link>
+          <Button href="/" primary={true} text="Go back home" />
+          <Button href="/docs" text="Read the docs" />
         </div>
       </div>
     </div>
