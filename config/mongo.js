5daf3655e4377183ee711566a9bff75f768f653d
chalithya <chalithya@spadesapp.io>
2023-05-16 08:56:47 +0530

    minor changes for #6855

--- config/mongo.js
+++ config/mongo.js
@@ -7,20 +7,20 @@ let connection;
 const connectMongo = async () => {
   const { LINKFREE_MONGO_CONNECTION_STRING } = process.env;
 
+  if (connection) {
+    return connection;
+  }
+
   if (!LINKFREE_MONGO_CONNECTION_STRING) {
     throw new Error(
       "Please define the LINKFREE_MONGO_CONNECTION_STRING environment variable (if local add to .env file)"
     );
   }
 
-  if (connection) {
-    return connection;
-  }
-
   try {
     const options = { autoIndex: true, family: 4 };
-    connection = await mongoose.connect(LINKFREE_MONGO_CONNECTION_STRING,options);
-    logger.info("MongoDB connected:", connection.name);
+    connection = await mongoose.connect(LINKFREE_MONGO_CONNECTION_STRING, options);
+    logger.info("DB connection successful:", connection.name);
   } catch (error) {
     logger.error("DB connection failed:", error.message);
     throw error;
