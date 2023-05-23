a19f5c7f4fbfbe3edc5a9f0266d3dd456bf97ce9
Anik Dhabal Babu <81948346+anikdhabal@users.noreply.github.com>
2023-05-23 16:03:00 +0530

    fix: h1 on the homepage (#7111)
    Co-authored-by: Eddie Jaoude <eddie@jaoudestudios.com>

--- pages/index.js
+++ pages/index.js
@@ -253,10 +253,10 @@ export default function Home({ total, today }) {
           <div className="overflow-hidden rounded-lg bg-secondary-high shadow-xl lg:grid lg:grid-cols-2 lg:gap-4">
             <div className="px-6 pt-10 pb-12 sm:px-16 sm:pt-16 lg:py-16 lg:pr-0 xl:py-20 xl:px-20">
               <div className="lg:self-center">
-                <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
+                <h1 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
                   <span className="block">Connect to your audience</span>
                   <span className="block">with a single link</span>
-                </h2>
+                </h1>
                 <p className="mt-4 text-lg leading-6 text-primary-low">
                   Showcase the content you create and your projects in one
                   place. Make it easier for people to find, follow and
