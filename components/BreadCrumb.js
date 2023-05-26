49ec307ca4653d523774cbc93c7ff5bae978994e
Nitheesh Daram <daramnitheesh@gmail.com>
2023-05-26 19:46:38 +0530

    feat:code review Changes (#7045)

--- components/BreadCrumb.js
+++ components/BreadCrumb.js
@@ -1,25 +1,26 @@
 import Link from '@components/Link'
-import { ChevronRightIcon, HomeIcon } from '@heroicons/react/20/solid'
+import { FiChevronRight } from 'react-icons/fi'
+import {AiOutlineHome} from 'react-icons/ai'
 
 export default function BreadCrumb({pageData}) {
 
   return (
-    <nav className="flex sticky top-2 bg-slate-900 dark:bg-slate-800 p-3 rounded z-[100]" aria-label="Breadcrumb">
+    <nav className="flex  p-3 " aria-label="Breadcrumb">
       <ol role="list" className="flex items-center space-x-4">
         <li>
           <div>
-            <Link href="/docs" className="text-gray-400 hover:text-gray-500">
-              <HomeIcon className="h-5 w-5 flex-shrink-0" aria-hidden="true" />
+            <Link href="/docs" className="text-primary hover:text-primary-medium-low">
+              <AiOutlineHome className="h-5 w-5 flex-shrink-0" aria-hidden="true" /> 
               <span className="sr-only">Docs</span>
             </Link>
           </div>
         </li>
           <li key={pageData.section}>
             <div className="flex items-center">
-              <ChevronRightIcon className="h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
+              <FiChevronRight className="h-5 w-5 flex-shrink-0 text-primary" aria-hidden="true" />  
               <Link
                 href={"/docs/#"+pageData.section}
-                className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700"
+                className="ml-4 text-sm font-medium text-primary hover:text-primary-medium-low"
               >
                 {pageData.section}
               </Link>
@@ -27,10 +28,10 @@ export default function BreadCrumb({pageData}) {
           </li>
           <li key={pageData.name}>
             <div className="flex items-center">
-              <ChevronRightIcon className="h-5 w-5 flex-shrink-0 text-gray-400" aria-hidden="true" />
+              <FiChevronRight className="h-5 w-5 flex-shrink-0 text-primary" aria-hidden="true" />
               <Link
                 href="#"
-                className="ml-4 text-sm font-medium text-gray-500 hover:text-gray-700"
+                className="ml-4 text-sm font-medium text-primary hover:text-primary-medium-low"
               >
                 {pageData.name}
               </Link>
