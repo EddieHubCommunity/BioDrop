2cebceab8ad405383930c9b794906fcd6428ec45
Eddie Jaoude <eddie@jaoudestudios.com>
2023-05-28 07:36:04 +0700

    fix: removed links manage menu

--- pages/account/manage/links.js
+++ pages/account/manage/links.js
@@ -1,14 +1,13 @@
-import { Fragment } from "react";
-import { Menu, Transition } from "@headlessui/react";
 import { authOptions } from "../../api/auth/[...nextauth]";
 import { getServerSession } from "next-auth/next";
-import { HiEllipsisVertical } from "react-icons/hi2";
+import { FaEdit } from "react-icons/fa";
 
 import logger from "@config/logger";
 import PageHead from "@components/PageHead";
 import Page from "@components/Page";
 import Navigation from "@components/account/manage/navigation";
 import { getLinksApi } from "pages/api/account/manage/links";
+import Button from "@components/Button";
 
 export async function getServerSideProps(context) {
   const session = await getServerSession(context.req, context.res, authOptions);
@@ -37,9 +36,6 @@ export async function getServerSideProps(context) {
 }
 
 export default function Links({ links }) {
-  function classNames(...classes) {
-    return classes.filter(Boolean).join(" ");
-  }
   return (
     <>
       <PageHead
@@ -49,6 +45,8 @@ export default function Links({ links }) {
 
       <Page>
         <Navigation />
+
+        <Button href="/account/manage/link" text="Add Link" />
 
         <ul role="list" className="divide-y divide-gray-100">
           {links.map((link) => (
@@ -76,75 +74,11 @@ export default function Links({ links }) {
                 </div>
               </div>
               <div className="flex flex-none items-center gap-x-4">
-                <a
-                  href={link.url}
-                  className="hidden rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:block"
-                >
-                  View link<span className="sr-only">, {link.name}</span>
-                </a>
-                <Menu as="div" className="relative flex-none">
-                  <Menu.Button className="-m-2.5 block p-2.5 text-gray-500 hover:text-gray-900">
-                    <span className="sr-only">Open options</span>
-                    <HiEllipsisVertical
-                      className="h-5 w-5"
-                      aria-hidden="true"
-                    />
-                  </Menu.Button>
-                  <Transition
-                    as={Fragment}
-                    enter="transition ease-out duration-100"
-                    enterFrom="transform opacity-0 scale-95"
-                    enterTo="transform opacity-100 scale-100"
-                    leave="transition ease-in duration-75"
-                    leaveFrom="transform opacity-100 scale-100"
-                    leaveTo="transform opacity-0 scale-95"
-                  >
-                    <Menu.Items className="absolute right-0 z-10 mt-2 w-32 origin-top-right rounded-md bg-white py-2 shadow-lg ring-1 ring-gray-900/5 focus:outline-none">
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? "bg-gray-50" : "",
-                              "block px-3 py-1 text-sm leading-6 text-gray-900"
-                            )}
-                          >
-                            Edit
-                            <span className="sr-only">, {link.name}</span>
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? "bg-gray-50" : "",
-                              "block px-3 py-1 text-sm leading-6 text-gray-900"
-                            )}
-                          >
-                            Move
-                            <span className="sr-only">, {link.name}</span>
-                          </a>
-                        )}
-                      </Menu.Item>
-                      <Menu.Item>
-                        {({ active }) => (
-                          <a
-                            href="#"
-                            className={classNames(
-                              active ? "bg-gray-50" : "",
-                              "block px-3 py-1 text-sm leading-6 text-gray-900"
-                            )}
-                          >
-                            Delete
-                            <span className="sr-only">, {link.name}</span>
-                          </a>
-                        )}
-                      </Menu.Item>
-                    </Menu.Items>
-                  </Transition>
-                </Menu>
+                <Button
+                  icon={<FaEdit />}
+                  href={`/account/manage/link/${link._id}`}
+                  text="Edit"
+                />
               </div>
             </li>
           ))}
