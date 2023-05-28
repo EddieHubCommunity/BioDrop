f2844de97e5e42688cc0525c2901aee70a630048
Eddie Jaoude <eddie@jaoudestudios.com>
2023-05-28 20:36:42 +0700

    feat: alert to load json data

--- components/Alert.js
+++ components/Alert.js
@@ -16,7 +16,6 @@ export default function Alert({ type, message, url, action }) {
           {url && <Button href={url} text={action} primary={true} />}
         </div>
       );
-      break;
     case "error":
       return (
         <div
@@ -28,19 +27,19 @@ export default function Alert({ type, message, url, action }) {
           {url && <Button href={url} text={action} primary={true} />}
         </div>
       );
-      break;
     case "warning":
       return (
         <div
-          className={`alert-${type} bg-yellow-100 rounded-lg py-5 px-6 mb-3 text-base text-yellow-700 inline-flex items-center w-full animate-pulse`}
+          className={`alert-${type} bg-yellow-100 rounded-lg py-5 px-6 mb-3 text-base text-yellow-700 flex flex-col w-full animate-pulse sm:flex sm:flex-row`}
           role="alert"
         >
-          <MdOutlineWarningAmber className="mr-2" />
-          <span className="grow">{message}</span>
+          <div className="grow inline-flex items-center">
+            <MdOutlineWarningAmber className="mr-2" />
+            <span>{message}</span>
+          </div>
           {url && <Button href={url} text={action} primary={true} />}
         </div>
       );
-      break;
     case "info":
       return (
         <div
@@ -52,6 +51,5 @@ export default function Alert({ type, message, url, action }) {
           {url && <Button href={url} text={action} primary={true} />}
         </div>
       );
-      break;
   }
 }
--- components/layouts/MultiLayout.js
+++ components/layouts/MultiLayout.js
@@ -5,13 +5,15 @@ import Alert from "@components/Alert";
 export default function MultiLayout({ children }) {
   return (
     <div className="flex flex-col min-h-screen">
+      {process.env.NEXT_PUBLIC_VERCEL_ENV !== "production" && (
+        <Alert
+          type="warning"
+          message={`To (re)load json files and any changes, please click >>> `}
+          url="/api/system/reload?secret=development"
+          action="Load data"
+        />
+      )}
       <Navbar />
-      <Alert
-        type="warning"
-        message={`To (re)load json files and any changes, please click >>> `}
-        url="/api/system/reload?secret=development"
-        action="Load data"
-      />
       <main className="flex-1">{children}</main>
       <Footer />
     </div>
