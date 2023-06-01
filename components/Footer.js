f298633321929b86a2e3352ec73e7f5524decb5a
Dan <35927536+dan-mba@users.noreply.github.com>
2023-06-01 13:13:53 -0400

    Use dollar icon from heroicons

--- components/Footer.js
+++ components/Footer.js
@@ -4,8 +4,7 @@ import FaLinkedin from "@components/icons/FaLinkedin";
 import FaGithub from "@components/icons/FaGithub";
 import FaYoutube from "@components/icons/FaYoutube";
 
-import FaDollarSign from "@components/icons/FaDollarSign";
-
+import { CurrencyDollarIcon } from "@heroicons/react/24/outline";
 import { RocketLaunchIcon } from "@heroicons/react/20/solid";
 import Button from "./Button";
 import Link from "@components/Link";
@@ -185,11 +184,14 @@ export default function Footer() {
             </p>
           </div>
           <Button
-            text="Learn more about Premium"
             primary={true}
-            icon={<FaDollarSign />}
             href="/premium"
-          />
+          >
+            <span className="mr-2">
+              <CurrencyDollarIcon className="h-6 w-6" />
+            </span>
+            <span>Learn more about Premium</span>
+          </Button>
         </div>
         <div className="mt-8 border-t border-white/10 pt-8 flex flex-col md:flex-row   items-center justify-between">
           <div className="flex pb-4 md:pb-0 items-center justify-center space-x-6 md:order-2">
