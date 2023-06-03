58a142a891a2174485c45c65c74731649f358f11
Catalin Pit <catalinpit@gmail.com>
2023-06-03 12:00:46 +0300

    implemented the feedback

--- config/schemas/clientSchema.js
+++ config/schemas/clientSchema.js
@@ -1,7 +1,6 @@
 import * as z from "zod";
 
 const envSchema = z.object({
-  NEXT_PUBLIC_GA_MEASUREMENT_ID: z.string().optional(),
   NEXT_PUBLIC_BASE_URL: z.string().url().default("http://localhost:3000"),
 });
 
