99edb18dafeefbae528c13274f464a3d18ddc785
KushagraVarshney05 <varkushqwerty@gmail.com>
2023-06-07 06:46:12 +0530

    fix: font size of future description (#7425)

--- pages/index.js
+++ pages/index.js
@@ -311,7 +311,7 @@ export default function Home({ total, today }) {
             <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
               Features
             </h2>
-            <p className="mt-4 text-white">
+            <p className="mt-4 text-white text-xl">
               It is not just links... Take a look at the Features you can add to
               customize your LinkFree Profile.
             </p>
