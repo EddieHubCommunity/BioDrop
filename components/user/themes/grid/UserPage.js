21e4a3fa82de660e00d3cc740380a02f5c5dbbd9
Niranjan <niranjank.desktop@gmail.com>
2023-06-15 12:31:04 +0530

    Change the location of modified files

---
+++ components/user/themes/grid/UserPage.js
@@ -0,0 +1,123 @@
+import { useState } from "react";
+
+import UserProfile from "./UserProfile";
+import UserTabs from "./UserTabs";
+import UserLinks from "./UserLinks";
+import UserMilestones from "./UserMilestones";
+import UserTestimonials from "./UserTestimonials";
+import UserEvents from "./UserEvents";
+
+export default function UserPage({ data, BASE_URL }) {
+  const defaultTabs = [
+    { name: "My Links", href: "#", current: true }, 
+    { name: "Milestones", href: "#", current: false },
+    { name: "Testimonials", href: "#", current: false },
+    { name: "Events", href: "#", current: false },
+  ];
+  data.testimonials = data.testimonials.filter(
+    (testimonial) => testimonial.isPinned
+  );
+  let displayTabs = defaultTabs.flatMap((tab) => {
+    if (tab.name === "Milestones") {
+      if (data.milestones && data.milestones.length) {
+        return { ...tab, total: data.milestones.length };
+      }
+      return [];
+    }
+    if (tab.name === "Testimonials") {
+      if (data.testimonials && data.testimonials.length) {
+        return { ...tab, total: data.testimonials.length };
+      }
+      return [];
+    }
+    if (tab.name === "Events") {
+      if (data.events && data.events.length) {
+        return { ...tab, total: data.events.length };
+      }
+      return [];
+    }
+
+    return { ...tab, total: data.links?.length };
+  });
+  const [tabs, setTabs] = useState(displayTabs);
+
+  return (
+    <>
+      <UserProfile data={data} BASE_URL={BASE_URL} />
+
+      <div className="flex justify-between space-x-8">
+      {tabs.find((tab) => tab.name === "My Links") && (
+        <div className="hidden sm:block">
+          <div className="border-secondary-high dark:border-secondary-low text-secondary-high dark:text-secondary-low py-4 px-1 text-center border-b-2 font-medium text-sm">
+            My Links ({tabs.find((tab) => tab.name === "My Links").total})
+          </div>
+          <UserLinks data={data} BASE_URL={BASE_URL} />
+        </div>
+      )}
+
+      {tabs.find((tab) => tab.name === "Milestones") && (
+        <div className="hidden sm:block">
+          <div className="border-secondary-high dark:border-secondary-low text-secondary-high dark:text-secondary-low py-4 px-1 text-center border-b-2 font-medium text-sm">
+            Milestones ({tabs.find((tab) => tab.name === "Milestones").total})
+          </div>
+          <UserMilestones data={data} />
+        </div>
+      )}
+      </div>
+
+      <div className="flex justify-between space-x-8">
+      {tabs.find((tab) => tab.name === "Testimonials") && (
+        <div className="hidden sm:block">
+          <div className="border-secondary-high dark:border-secondary-low text-secondary-high dark:text-secondary-low py-4 px-1 text-center border-b-2 font-medium text-sm">
+            Testimonials ({tabs.find((tab) => tab.name === "Testimonials").total})
+          </div>
+          <UserTestimonials testimonials={data.testimonials} />
+        </div>
+      )}
+      {tabs.find((tab) => tab.name === "Events") && (
+        <div className="hidden sm:block">
+          <div className="border-secondary-high dark:border-secondary-low text-secondary-high dark:text-secondary-low py-4 px-1 text-center border-b-2 font-medium text-sm">
+            Events ({tabs.find((tab) => tab.name === "Events").total})
+          </div>
+          <UserEvents data={data} />
+        </div>
+      )}
+      </div>
+
+      {tabs.find((tab) => tab.name === "My Links") && (
+        <div className="sm:hidden">
+          <div className="border-secondary-high dark:border-secondary-low text-secondary-high dark:text-secondary-low py-4 px-1 text-center border-b-2 font-medium text-sm">
+            My Links ({tabs.find((tab) => tab.name === "My Links").total})
+          </div>
+          <UserLinks data={data} BASE_URL={BASE_URL} />        
+        </div>
+      )}
+
+      {tabs.find((tab) => tab.name === "Milestones") && (
+        <div className="sm:hidden">
+          <div className="border-secondary-high dark:border-secondary-low text-secondary-high dark:text-secondary-low py-4 px-1 text-center border-b-2 font-medium text-sm">
+            Milestones ({tabs.find((tab) => tab.name === "Milestones").total})
+          </div>          
+          <UserMilestones data={data} />
+        </div>
+      )}
+
+      {tabs.find((tab) => tab.name === "Events") && (
+        <div className="sm:hidden">
+          <div className="border-secondary-high dark:border-secondary-low text-secondary-high dark:text-secondary-low py-4 px-1 text-center border-b-2 font-medium text-sm">
+            Events ({tabs.find((tab) => tab.name === "Events").total})
+          </div>
+          <UserEvents data={data} />
+        </div>
+      )}
+      {tabs.find((tab) => tab.name === "Testimonials") && (
+        <div className="sm:hidden">
+          <div className="border-secondary-high dark:border-secondary-low text-secondary-high dark:text-secondary-low py-4 px-1 text-center border-b-2 font-medium text-sm">
+            Testimonials ({tabs.find((tab) => tab.name === "Testimonials").total})
+          </div>
+          <UserTestimonials testimonials={data.testimonials} />
+        </div>
+      )}
+    </>
+  );
+}
---
+++ components/user/themes/grid/UserTestimonials.js
@@ -0,0 +1,45 @@
+import { ReactMarkdown } from "react-markdown/lib/react-markdown";
+
+import Link from "@components/Link";
+import Alert from "@components/Alert";
+import FallbackImage from "@components/FallbackImage";
+
+export default function UserTestimonials({ testimonials }) {
+  return (
+    <>
+      {!testimonials && <Alert type="info" message="No testimonials found" />}
+      {testimonials &&
+        testimonials.map((testimonial, key) => (
+          <div
+            className="flex flex-col flex-row gap-2 items-center text-sm dark:text-primary-low dark:bg-primary-medium text-primary-medium-low dark:border-none border-2 my-4 px-5 p-6 rounded-xl shadow-xl"
+            key={key}>
+            <div className="flex items-center px-4">
+              <div className="w-20 h-20">
+                <FallbackImage
+                  width={100} // just to pass nextjs error
+                  height={100}
+                  src={`https://github.com/${testimonial.username}.png`}
+                  alt={testimonial.username}
+                  className="rounded-full bg-primary-low"
+                />
+              </div>
+              <div className="flex-1 p-6">
+                <h3 className="font-medium dark:text-primary-low text-primary-high">
+                  {testimonial.title}
+                </h3>
+                <Link href={testimonial.url} target="_blank">
+                  @{testimonial.username}
+                </Link>
+              </div>
+            </div>
+
+            <div className="flex flex-col">
+              <div className="prose prose-sm max-w-none w-fit text-primary-medium dark:text-primary-medium-low ">
+                <ReactMarkdown>{testimonial.description}</ReactMarkdown>
+              </div>
+            </div>
+          </div>
+        ))}
+    </>
+  );
+}
