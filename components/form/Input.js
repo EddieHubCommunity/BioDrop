fe9f62d6a162d925f02718e80d2afbaf9b080e83
Eddie Jaoude <eddie@jaoudestudios.com>
2023-06-18 18:34:35 +0700

    fix: vercel preview action

--- components/form/Input.js
+++ components/form/Input.js
@@ -16,7 +16,7 @@ export default function Input({
       <input
         type="text"
         placeholder={placeholder}
-        className={`border-2 transition-all duration-250 ease-linear rounded px-6 py-2 mb-4 block w-full ${
+        className={`border-2 transition-all duration-250 ease-linear rounded px-6 py-2 mb-2 block w-full ${
           disabled
             ? "bg-primary-low-medium dark:bg-primary-medium-low hover:border-primary-medium-low focus:ring-0 focus:border-primary-medium focus:outline-0 cursor-not-allowed"
             : "dark:bg-primary-high hover:border-tertiary-medium focus:ring-0 focus:border-tertiary-medium focus:outline-0"
