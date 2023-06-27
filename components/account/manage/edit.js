a8f0240dee169f3fdb540bcae79934a506240cb6
Eddie Jaoude <eddie@jaoudestudios.com>
2023-06-27 09:06:40 +0700

    fix: milestone date

--- components/account/manage/edit.js
+++ components/account/manage/edit.js
@@ -5,7 +5,7 @@ export default function Edit({ href, children }) {
     <div className="relative w-full">
       <a
         href={href}
-        className="rounded-full bg-secondary-medium p-2 text-white shadow-sm hover:bg-secondary-medium-low focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary-medium absolute top-0 right-0 pointer z-50"
+        className="rounded-full bg-secondary-medium p-2 text-white shadow-sm hover:bg-secondary-medium-low focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary-medium absolute top-0 left-0 pointer z-50"
       >
         <PencilIcon className="h-5 w-5" aria-hidden="true" />
       </a>
