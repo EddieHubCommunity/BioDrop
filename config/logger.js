927ce46950c6e1df32e7c66b287b3cbd9f88477e
Grut <akashsah946@gmail.com>
2023-06-27 15:59:58 +0000

    defined log levels and removed file appender functionality

--- config/logger.js
+++ config/logger.js
@@ -1,37 +1,29 @@
 import { serverEnv } from "@config/schemas/serverSchema";
 import * as PinoLogger from "pino";
-import { join } from "path";
 
-const logsPath = join(__dirname, "..", "logs", "app.log");
-
-const config = {
+const development = {
   transport: {
-    targets: [
-    {
-      target:"pino-pretty",
-      options: {
-        colorize: true,
-        translateTime: "SYS:dd-mm-yyyy HH:MM:ss",   //System time
-        ignore: "pid,hostname"            // Ignores process id and hostname in console that displays after log level message
-      }
+    target: "pino-pretty",
+    options: {
+      level:'debug',
+      colorize: true,
+      translateTime:"SYS:dd-mm-yyyy HH:MM:ss",
+      ignore:"pid,hostname"   //Ignores process id and hostname
     },
-    {
-    target: "pino/file",
-    options: {                     
-      destination: logsPath,
-      mkdir:true,               //creates log file if not present in specified directory
-      append: true,
-    },
-  }
-  ]
   },
-  level: serverEnv.NODE_ENV === "production" ? "info" : "debug",
-  streams: [
-    { stream: process.stdout }, // Output logs to console 
-    { stream: PinoLogger.destination(logsPath), level: "debug", enabled: true,formatter: "log", }, // Output logs to (app.log) file
-  ],
+};
+const production = {
+  transport: {
+    target: "pino-pretty",
+    options: {
+      level:'info',
+      colorize: true,
+    },
+  },
 };
 
-const logger = PinoLogger(config);
+let logger;
+
+logger = PinoLogger.pino(serverEnv.NODE_ENV === "development" ? development : production);
 
 export default logger;
