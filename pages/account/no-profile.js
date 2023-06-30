94ad32ca5474e2b746a9310beeb7656b558f0ddf
Eddie Jaoude <eddie@jaoudestudios.com>
2023-07-01 06:48:37 +0700

    fix: dark mode for no profile

--- pages/account/no-profile.js
+++ pages/account/no-profile.js
@@ -32,26 +32,26 @@ export default function NoProfile({ BASE_URL }) {
       />
 
       <Page>
-        <div className="bg-white">
+        <div className="bg-white dark:bg-primary-high">
           <div className="py-24 px-6 sm:px-6 sm:py-32 lg:px-8">
             <div className="mx-auto max-w-2xl text-center">
-              <h2 className="text-4xl font-bold tracking-tight text-primary-high">
+              <h2 className="text-4xl font-bold tracking-tight text-primary-high dark:text-primary-low">
                 Your LinkFree Profile does not exist yet
               </h2>
-              <p className="mx-auto mt-6 max-w-xl text-lg leading-8 text-primary-medium">
+              <p className="mx-auto mt-6 max-w-xl text-lg leading-8 text-primary-medium dark:text-primary-medium-low">
                 Please follow the QuickStart guide to create your Profile or
                 click on the Forms button to create your Profile
               </p>
               <div className="mt-10 flex items-center justify-center gap-x-6">
                 <Link
                   href={`${BASE_URL}/account/manage/profile`}
-                  className="rounded-md bg-secondary-high px-3.5 py-1.5 text-base font-semibold leading-7 text-white shadow-sm hover:bg-secondary-low focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary-high"
+                  className="rounded-md bg-secondary-high px-3.5 py-1.5 text-base font-semibold leading-7 text-primary-low dark:text-primary-low shadow-sm hover:bg-secondary-low focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary-high"
                 >
                   Go to Forms
                 </Link>
                 <Link
                   href="https://linkfree.eddiehub.io/docs/quickstart"
-                  className="text-base font-semibold leading-7 text-primary-high"
+                  className="text-base font-semibold leading-7 text-primary-high dark:text-primary-low"
                 >
                   QuickStart Guide <span aria-hidden="true">→</span>
                 </Link>
