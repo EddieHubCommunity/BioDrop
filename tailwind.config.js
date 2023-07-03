d5f42260fffc4ebc191a49ac20d7ac315592027e
Anik Dhabal Babu <adhabal2002@gmail.com>
2023-07-03 04:26:35 +0000

    event css issue fixed

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,12 @@ module.exports = {
   ],
   darkMode: "class",
   theme: {
+    borderWidth: {
+      DEFAULT : '1px',
+      '0': '0',
+      '2' : '2px',
+      '3' : '3px',
+    },
     extend: {
       colors: {
         primary: {
