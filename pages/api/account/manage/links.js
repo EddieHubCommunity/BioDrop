52358f0a32d22408b8e7b1888960e9a2fcad7e1d
Eddie Jaoude <eddie@jaoudestudios.com>
2023-07-07 02:32:20 +0700

    feat: manage reordering links and sorting (#8002)
    * fix: sort linsks by their order
    
    * fix: link sorting doesn't exist
    
    * feat: reorder links

--- pages/api/account/manage/links.js
+++ pages/api/account/manage/links.js
@@ -1,5 +1,6 @@
 import { authOptions } from "../../auth/[...nextauth]";
 import { getServerSession } from "next-auth/next";
+import { ObjectId } from "bson";
 
 import connectMongo from "@config/mongo";
 import logger from "@config/logger";
@@ -13,11 +14,20 @@ export default async function handler(req, res) {
     return;
   }
 
-  if (req.method !== "GET") {
+  if (!["GET", "PATCH"].includes(req.method)) {
     return res.status(400).json({ error: "Invalid request: GET required" });
   }
 
-  const data = await getLinksApi(session.username);
+  const username = session.username;
+
+  let data = [];
+  if (req.method === "GET") {
+    data = await getLinksApi(username);
+  }
+
+  if (req.method === "PATCH") {
+    data = await updateLinksOrderApi(username, req.body);
+  }
 
   return res.status(200).json(data);
 }
@@ -28,10 +38,33 @@ export async function getLinksApi(username) {
 
   let getLinks = [];
   try {
-    getLinks = await Link.find({ username }).sort({ isEnabled: -1, order: 1 });
+    getLinks = await Link.find({ username }).sort({ order: 1 });
   } catch (e) {
     log.error(e, `failed to get links for username: ${username}`);
   }
 
   return JSON.parse(JSON.stringify(getLinks));
+}
+
+export async function updateLinksOrderApi(username, data) {
+  await connectMongo();
+  const log = logger.child({ username });
+
+  data.map(async (link, idx) => {
+    try {
+      await Link.findOneAndUpdate(
+        {
+          username,
+          _id: new ObjectId(link._id),
+        },
+        {
+          order: idx,
+        }
+      );
+    } catch (e) {
+      log.error(e, `failed to update link order for username: ${username}`);
+    }
+  });
+
+  return JSON.parse(JSON.stringify(await getLinksApi(username)));
 }
