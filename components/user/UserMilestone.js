aa75212c056308473f679da9618444ab292b8694
Dan <35927536+dan-mba@users.noreply.github.com>
2023-07-09 11:43:41 -0400

    fix: basic tests for forms (#7987)
    * add test for manage/profile
    
    * fix color formatting
    
    * increase tests to 4 shards
    
    * add tests for manage/events
    
    * add tests for manage/links
    
    * add tests for manage/milestones
    
    * switch to 5 test shards
    
    * add tests for manage/testimonials
    
    * update toggle aria-label to work correctly

--- components/user/UserMilestone.js
+++ components/user/UserMilestone.js
@@ -10,49 +10,57 @@ export default function UserMilestone({ milestone, isGoal, manage }) {
   const [date, setDate] = useState(milestone.date);
 
   useEffect(() => {
-    const parse = Date.parse(date);
+    const parse = Date.parse(milestone.date);
     if (!isNaN(parse)) {
       setDate(new Date(parse).toLocaleDateString());
     }
   }, [milestone.date]);
 
   const DisplayIcon = getIcon(milestone.icon);
-  const item = (milestone) => (
-    <div className="flex space-x-3 grow">
-      {milestone.icon && <DisplayIcon className="h-8 w-8 rounded-full" />}
-      <div className="flex-1 space-y-1">
-        <div className="flex items-center justify-between">
-          <h3 className="text-sm font-medium flex gap-2 items-center">
-            <span>{milestone.title}</span>
-            {milestone.url && (
-              <Link href={milestone.url} target="_blank">
-                <FaExternalLinkAlt />
-              </Link>
-            )}
-          </h3>
-          <p className="text-sm text-primary-medium dark:text-primary-medium-low">
-            {date}
-          </p>
-        </div>
-        <ReactMarkdown className="text-sm text-primary-medium dark:text-primary-medium-low">
-          {milestone.description}
-        </ReactMarkdown>
-      </div>
-    </div>
-  );
+  const item = (milestone, isGoal) => {
+    const colors = isGoal ? "text-primary-medium/70 dark:text-primary-low-medium/[.83]" :
+      "text-primary-medium dark:text-primary-low-medium";
 
-  const edit = (milestone) => (
-    <Edit href={`/account/manage/milestone/${milestone._id}`}>
-      {item(milestone)}
+    return (
+      <div className="flex space-x-3 grow">
+        {milestone.icon && <DisplayIcon className="h-8 w-8 rounded-full" />}
+        <div className="flex-1 space-y-1">
+          <div className="flex items-center justify-between">
+            <h3 className={`text-sm font-medium flex gap-2 items-center ${isGoal ? "opacity-70" : ""}`}>
+              <span>{milestone.title}</span>
+              {milestone.url && (
+                <Link
+                  href={milestone.url}
+                  aria-label="Milestone Related Link"
+                  target="_blank"
+                >
+                  <FaExternalLinkAlt />
+                </Link>
+              )}
+            </h3>
+            <p className={`text-sm ${colors}`}>
+              {date}
+            </p>
+          </div>
+          <ReactMarkdown className={`text-sm ${colors}`}>
+            {milestone.description}
+          </ReactMarkdown>
+        </div>
+      </div>
+    );
+  };
+
+  const edit = (milestone, isGoal) => (
+    <Edit
+      href={`/account/manage/milestone/${milestone._id}`}
+      label={`${milestone.title} Milestone`}
+    >
+      {item(milestone, isGoal)}
     </Edit>
   );
   return (
-    <li
-      className={`flex flex-row gap-8 py-4 ${
-        isGoal ? "opacity-50" : ""
-      } border-primary-low-medium`}
-    >
-      {manage ? edit(milestone) : item(milestone)}
+    <li className={`flex flex-row gap-8 py-4 border-primary-low-medium`}>
+      {manage ? edit(milestone, isGoal) : item(milestone, isGoal)}
     </li>
   );
 }
