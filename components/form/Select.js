1f3d774c25ca85aabc7aff2cf7222ba378bdd0e0
Dhiraj <dhirajgilda12@gmail.com>
2023-07-10 16:28:28 +0530

    Updated dropdown options to improve visibility in dark mode

--- components/form/Select.js
+++ components/form/Select.js
@@ -20,7 +20,7 @@ export default function Select({
         {...restProps}
       >
         {options.map((v) => (
-          <option key={v}>{v}</option>
+          <option key={v} style={{color:"black"}}>{v}</option>
         ))}
       </select>
     </>
