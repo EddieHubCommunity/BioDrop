e9142242ce2fe141267234bb93ce00ca3866e718
Sahil Chalke <109215419+Sahilll15@users.noreply.github.com>
2023-07-11 22:42:27 +0530

    sort changed

--- pages/api/events.js
+++ pages/api/events.js
@@ -1,26 +1,23 @@
-import logger from "@config/logger";
-import Profile from "@models/Profile";
-
-export default async function handler(req, res) {
-  if (req.method != "GET") {
-    return res
-      .status(400)
-      .json({ error: "Invalid request: GET request required" });
-  }
-
-  const events = await getEvents();
-  return res.status(200).json(events);
-}
-
 export async function getEvents() {
   let events = [];
   try {
     events = await Profile.aggregate([
-      { $project: { username: 1, events: 1, isEnabled: 1 } },
-      { $match: { "events.date.start": { $gt: new Date() }, isEnabled: true } },
+      {
+        $project: { username: 1, events: 1, isEnabled: 1 },
+      },
+      {
+        $match: {
+          "events.date.start": { $gt: new Date() },
+          isEnabled: true,
+        },
+      },
       { $unwind: "$events" },
-      { $match: { "events.date.end": { $gt: new Date() } } },
-      { $sort: { "events.date.start": 1 } },
+      {
+        $match: { "events.date.end": { $gt: new Date() } },
+      },
+      {
+        $sort: { "events.date.start": 1 },
+      },
       {
         $group: {
           _id: "$events.url",
@@ -33,6 +30,9 @@ export async function getEvents() {
           description: { $first: "$events.description" },
           isEnabled: { $first: "$isEnabled" },
         },
+      },
+      {
+        $sort: { "date.start": 1 },
       },
     ]).exec();
 
