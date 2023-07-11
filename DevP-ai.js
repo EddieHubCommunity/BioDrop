c26befc80a10b703a0f779843d2d33e5fd5a1cef
Devajit Patar <107491760+DevP-ai@users.noreply.github.com>
2023-07-11 10:56:28 +0530

    Create DevP-ai.js

---
+++ DevP-ai.js
@@ -0,0 +1,15 @@
+{
+  "name": "Devajit Patar",
+  "bio": "Android | Open Source | ML",
+  "links": [
+    {
+      "name": "LinkedIn",
+      "url": "https://www.linkedin.com/in/devajit-patar-05980821a",
+      "icon": "FaLinkedin"
+    },{
+      "name":"Github",
+      "url":"https://github.com/DevP-ai",
+      "icon":"FaGithub"
+    }
+  ]
+}
