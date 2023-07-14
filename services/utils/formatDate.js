fe2018a69b8715d7ac3a4eef3d1b22a3e21608e1
Vishrut19 <vishrutagarwalla19@gmail.com>
2023-07-14 11:09:51 +0530

    Refactored formatDate with Original Configuration

--- services/utils/formatDate.js
+++ services/utils/formatDate.js
@@ -1,13 +1,7 @@
 export function formatDate(date, locale = "en-GB") {
-  let options = {
-    weekday: "long",
-    day: "numeric",
-    month: "long",
-    year: "numeric",
-    hour: "2-digit",
-    minute: "2-digit",
-    second: "2-digit",
-    timeZoneName: "short",
+  const options = {
+    dateStyle: "full",
+    timeStyle: "long",
   };
 
   return new Intl.DateTimeFormat(locale, options).format(date);
