001ffe06eb4b85bb6acc58eacbb6f9580d0cbf4e
Amanda <apandabr@gmail.com>
2023-07-18 19:38:02 +0000

    new tag component, user card modifications

---
+++ components/TagSimple.js
@@ -0,0 +1,11 @@
+
+export default function TagSimple({ name }) {
+  return (
+   
+    <div
+    className="text-xs px-1 mx-px mb-px font-mono border rounded-md line-clamp-1"
+  >
+     {name}
+  </div>
+  );
+}
--- components/user/UserCard.js
+++ components/user/UserCard.js
@@ -2,6 +2,7 @@ import ReactMarkdown from "react-markdown";
 
 import Link from "@components/Link";
 import FallbackImage from "@components/FallbackImage";
+import TagSimple from "@components/TagSimple";
 
 export default function UserCard({ profile }) {
   return (
@@ -13,21 +14,36 @@ export default function UserCard({ profile }) {
         <FallbackImage
           src={`https://github.com/${profile.username}.png`}
           alt={`Profile picture of ${profile.name}`}
-          width={80}
-          height={80}
+          width={75}
+          height={75}
           className="rounded-full"
           fallback={profile.name}
         />
       </div>
       <div>
-        <h3 className="text-xl justify-center text-center mb-2 text-tertiary-medium font-bold">
+        <h3 className="text-lg justify-center text-center mb-2 text-tertiary-medium font-bold">
           {profile.name}
         </h3>
         {/* Links inside a link is not allowed, remove them from bio in card */}
+        <div className="flex flex-wrap justify-center mb-px">
+        {profile.tags?.length > 0 &&
+            profile.tags.map((tag, index) => {
+              const trimmedTag = tag.trim();
+              if (!trimmedTag) {
+                return null;
+              }
+              return (
+                <TagSimple
+                  name={trimmedTag}
+                  key={index}
+                />
+              );
+            })}
+            </div>
         <ReactMarkdown
           disallowedElements={["a"]}
           unwrapDisallowed
-          className="text-center line-clamp-5"
+          className="text-center text-sm line-clamp-3"
         >
           {profile.bio}
         </ReactMarkdown>
