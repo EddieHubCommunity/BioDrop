482c6ab3b7cb0aced215cad8f60206b212b3f094
Eddie Jaoude <eddie@jaoudestudios.com>
2023-07-24 11:45:46 +0100

    fix: updated dependency + removed ip

--- pages/[username].js
+++ pages/[username].js
@@ -1,6 +1,5 @@
 import { IconContext } from "react-icons";
 import { FaRegComments } from "react-icons/fa";
-import requestIp from "request-ip";
 import { remark } from "remark";
 import strip from "strip-markdown";
 
@@ -16,14 +15,10 @@ import UserPage from "@components/user/UserPage";
 export async function getServerSideProps(context) {
   const { req, res } = context;
   const username = context.query.username;
-  const log = logger.child({
-    username: username,
-    ip: requestIp.getClientIp(req),
-  });
 
   const { status, profile } = await getUserApi(req, res, username);
   if (status !== 200) {
-    log.error(
+    logger.error(
       profile.error,
       `profile loading failed for username: ${username}`
     );
@@ -36,13 +31,13 @@ export async function getServerSideProps(context) {
     };
   }
 
-  log.info(`data loaded for username: ${username}`);
+  logger.info(`data loaded for username: ${username}`);
 
   try {
     const processedBio = await remark().use(strip).process(profile.bio);
     profile.cleanBio = processedBio.toString();
   } catch (e) {
-    log.error(e, `cannot strip markdown for: ${username}`);
+    logger.error(e, `cannot strip markdown for: ${username}`);
     profile.cleanBio = profile.bio;
   }
 
