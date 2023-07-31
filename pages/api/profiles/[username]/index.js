6b3fab4f78ea4bb7d658f043ffa0757e37b2efb1
Eddie Jaoude <eddie@jaoudestudios.com>
2023-07-31 03:59:01 +0100

    fix: profile updated datetime on stats

--- pages/api/profiles/[username]/index.js
+++ pages/api/profiles/[username]/index.js
@@ -165,7 +165,8 @@ export async function getUserApi(req, res, username) {
             },
             {
               $inc: { views: 1 },
-            }
+            },
+            { timestamps: false }
           );
           log.info(`stats incremented for username: ${username}`);
         } catch (e) {
