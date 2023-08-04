875ef4d019ca6ae06bfaf34d64e8ffc9e01de007
Eddie Jaoude <eddie@jaoudestudios.com>
2023-08-04 07:44:49 +0100

    Update components/account/manage/Delete.js
    Co-authored-by: Olabode Lawal-Shittabey <babblebey@gmail.com>

--- components/account/manage/Delete.js
+++ components/account/manage/Delete.js
@@ -7,6 +7,7 @@ export default function Delete({ action, id, label = "", children }) {
       <Button
         onClick={() => action(id)}
         aria-label={`Delete ${label}`}
+        title={`Delete ${label}`}
         className="rounded-full bg-red-500 p-2 text-white shadow-sm hover:bg-red-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-secondary-medium absolute top-0 right-0 pointer z-50"
       >
         <DocumentMinusIcon className="h-5 w-5" aria-hidden="true" />
