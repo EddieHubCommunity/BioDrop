a0fc4be76c15ad4d4cd676e00cb847375ad2c0a6
Chinmay Mhatre <chinmaymhatre12@gmail.com>
2023-08-04 16:32:03 +0530

    fix: dark/light mode for admin page (#8492)

--- components/user/UserRepos.js
+++ components/user/UserRepos.js
@@ -7,17 +7,17 @@ import dateFormat from "@services/utils/dateFormat";
 
 export default function UserRepos({ manage = false, handleDelete, repos }) {
   const item = (repo) => (
-    <div className="relative flex justify-between gap-x-6 px-4 py-5 hover:bg-gray-50 sm:px-6 lg:px-8">
+    <div className="relative flex justify-between gap-x-6 px-4 py-5 hover:bg-primary-low dark:hover:bg-primary-medium transition-all duration-100 sm:px-6 lg:px-8">
       <div className="flex gap-x-4">
         <FallbackImage
-          className="h-12 w-12 flex-none rounded-full bg-gray-50"
+          className="h-12 w-12 flex-none rounded-full bg-primary-low"
           src={`https://github.com/${repo.owner}.png`}
           alt={`${repo.owner} avatar`}
           width={100}
           height={100}
         />
         <div className="min-w-0 flex-auto">
-          <p className="text-sm font-semibold leading-6 text-gray-900">
+          <p className="text-sm font-semibold leading-6 text-primary-high dark:text-primary-low">
             <Link href={repo.url} target="_blank">
               <span className="absolute inset-x-0 -top-px bottom-0" />
               {repo.owner}/{repo.name}
@@ -26,7 +26,7 @@ export default function UserRepos({ manage = false, handleDelete, repos }) {
               ({dateFormat({ format: "long", date: repo.dates.pushedAt })})
             </span>
           </p>
-          <p className="mt-1 flex text-xs leading-5 text-gray-500">
+          <p className="mt-1 flex text-xs leading-5 text-primary-medium-low ">
             {repo.description}
           </p>
         </div>
@@ -35,14 +35,14 @@ export default function UserRepos({ manage = false, handleDelete, repos }) {
         <div className="hidden sm:flex sm:flex-col sm:items-end">
           <p className="inline-flex gap-2 p-3 items-center content-center rounded-md bg-yellow-50 px-2 py-1 text-xs font-medium text-yellow-800 ring-1 ring-inset ring-yellow-600/20">
             <StarIcon
-              className="h-4 w-4 flex-none text-gray-400"
+              className="h-4 w-4 flex-none text-primary-low-medium "
               aria-hidden="true"
             />
             {repo.stats.stars}
           </p>
         </div>
         <ChevronRightIcon
-          className="h-5 w-5 flex-none text-gray-400"
+          className="h-5 w-5 flex-none text-primary-low-medium"
           aria-hidden="true"
         />
       </div>
@@ -56,7 +56,7 @@ export default function UserRepos({ manage = false, handleDelete, repos }) {
   );
 
   return (
-    <ul role="list" className="divide-y divide-gray-100">
+    <ul role="list" className="divide-y divide-primary-low">
       {repos.map((repo) => (
         <li key={repo._id}>{manage ? manageDelete(repo) : item(repo)}</li>
       ))}
