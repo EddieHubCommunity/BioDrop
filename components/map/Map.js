11e267cdba9c9a10e8b2667242d8db2683c35e0d
shiraz <shirazyousuf1234@gmail.com>
2023-08-11 11:43:36 +0530

    added resize event

--- components/map/Map.js
+++ components/map/Map.js
@@ -1,16 +1,25 @@
 import { MapContainer, TileLayer } from "react-leaflet";
 import Clusters from "./Clusters";
 import "leaflet/dist/leaflet.css";
+import { useState, useEffect, useMemo} from "react";
 
 export default function Map({ users }) {
   const boundsMap = [
     [-90, -180], // Southwest coordinates
     [90, 180], // Northeast coordinates
   ];
+  const [width, setWidth] = useState(window.innerWidth);
+  useEffect(() => {
+    const handleResize = () => setWidth(window.innerWidth);
+    window.addEventListener("resize", handleResize);
+    handleResize();
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+  const height = useMemo(() => (width > 1000 ? "100vh" : "40vh"), [width]);
 
-  const windowWidth = window.innerWidth;
 
   return (
+    <div style={{height}}>
     <MapContainer
       center={[0, 0]}
       zoom={2}
@@ -19,13 +28,15 @@ export default function Map({ users }) {
       scrollWheelZoom={true}
       maxBounds={boundsMap}
       maxBoundsViscosity={0.7}
-      style={windowWidth > 1000 ? {height: "100vh"}:{height: "40vh"}}
+      style={{height:'100%'}}
     >
+    
       <TileLayer
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://b.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
       <Clusters users={users} />
     </MapContainer>
+    </div>
   );
 }
