5a1f1c9119587e8289336fa2e55ba8ca601e1e7a
Adarsh Malviya <adarshmalviya1199@gmail.com>
2023-08-13 11:19:07 +0530

    code formatting

--- components/tag/TagSimple.js
+++ components/tag/TagSimple.js
@@ -1,9 +1,13 @@
 import { classNames } from "@services/utils/classNames";
 
 export default function TagSimple({ name, isSelected }) {
- 
   return (
-    <div className={classNames("text-sm px-1 mx-px mb-2 font-mono border rounded-md line-clamp-1", isSelected && "border-tertiary-medium")}>
+    <div
+      className={classNames(
+        "text-sm px-1 mx-px mb-2 font-mono border rounded-md line-clamp-1",
+        isSelected && "border-tertiary-medium"
+      )}
+    >
       {name}
     </div>
   );
