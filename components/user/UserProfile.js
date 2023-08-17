8129f950ab9ecb23bf933518be151955670c21ab
Anik Dhabal Babu <adhabal2002@gmail.com>
2023-08-17 17:16:26 +0000

    fix

--- components/user/UserProfile.js
+++ components/user/UserProfile.js
@@ -52,7 +52,7 @@ function UserProfile({ BASE_URL, data }) {
         <div className="flex flex-col self-center gap-3">
           <h1 className="text-3xl font-bold">{data.name}</h1>
           <div className="flex md:w-full gap-2 mx-auto text-xl">
-            {data.socials?.map((social) => (
+            {data.links?.map((social) => (
               <UserSocial
                 social={social}
                 key={social._id}
