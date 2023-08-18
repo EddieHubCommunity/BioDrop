566b658b0c31dfc531f589c429d56bb4a6f4ca85
Dev Raj Singh <rehansing.4522@gmail.com>
2023-08-19 00:21:54 +0530

    show lable for search icon

--- components/IconSearch.js
+++ components/IconSearch.js
@@ -28,12 +28,13 @@ function IconSearch({selectedIcon, handleSelectedIcon}) {
   
   return (
     <Combobox value={selectedIcon} onChange={(handleSelectedIcon)}>
-      <Combobox.Input  onChange={(event) => setQuery(event.target.value)} className={`border-2 transition-all duration-250 ease-linear rounded px-6 py-2 mb-2 block w-full dark:bg-primary-high hover:border-tertiary-medium focus:ring-0 focus:border-tertiary-medium focus:outline-0`} />
+      <label htmlFor={"search-icon"} className="mt-4 mb-3">Icon</label>
+      <Combobox.Input name="search-icon"  onChange={(event) => setQuery(event.target.value)} className={`border-2 transition-all duration-250 ease-linear rounded px-6 py-2 mb-2 block w-full dark:bg-primary-high hover:border-tertiary-medium focus:ring-0 focus:border-tertiary-medium focus:outline-0`} />
       <Combobox.Options className={`border-2 rounded border-tertiary-medium dark:bg-primary-medium`}>
         {filteredIcon.map((icon) => {
           const Icon = getIcon(icon);
           return (
-            <Combobox.Option key={icon} value={icon} className={`px-6 py-2 flex items-center dark:hover:bg-tertiary-low/60 hover:bg-secondary-low/40`}>
+            <Combobox.Option key={icon} value={icon} className={`px-3 py-2 flex items-center dark:hover:bg-tertiary-medium/60 hover:bg-secondary-low/40`}>
                 <Icon className="h-5 w-5 mr-2 fill-grey-700 inline align-center justify-center" />{" "}
                 <span>{icon}</span>
             </Combobox.Option>
