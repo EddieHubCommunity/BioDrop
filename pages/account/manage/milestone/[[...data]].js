3d215f58f69cc56e55890da0213a9c61552a54c2
Dev Raj Singh <rehansing.4522@gmail.com>
2023-08-18 23:58:23 +0530

    update for icon search headless ui

--- pages/account/manage/milestone/[[...data]].js
+++ pages/account/manage/milestone/[[...data]].js
@@ -16,6 +16,7 @@ import Toggle from "@components/form/Toggle";
 import Notification from "@components/Notification";
 import Link from "@components/Link";
 import ConfirmDialog from "@components/ConfirmDialog";
+import IconSearch from "@components/IconSearch";
 
 export async function getServerSideProps(context) {
   const session = await getServerSession(context.req, context.res, authOptions);
@@ -223,16 +224,7 @@ export default function ManageMilestone({ BASE_URL, milestone }) {
                     </p>
                   </div>
                   <div className="mt-1 sm:col-span-2 sm:mt-0">
-                    <Input
-                      name="icon"
-                      label="Icon"
-                      onChange={(e) => setIcon(e.target.value)}
-                      value={icon}
-                      placeholder="FaGithub"
-                      required
-                      minLength="2"
-                      maxLength="32"
-                    />
+                    <IconSearch handleSelectedIcon={setIcon} selectedIcon={icon} />
                     <p className="text-sm text-primary-low-medium">
                       Search for available{" "}
                       <Link href="/icons" target="_blank">
