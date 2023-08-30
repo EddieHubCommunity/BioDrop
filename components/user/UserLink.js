4cb7c88053ed9b82d1894488db95ab6276201742
Akshit Gupta <96991785+akkshitgupta@users.noreply.github.com>
2023-08-31 02:17:09 +0530

    FIX : red/green dots added to the UI

--- components/user/UserLink.js
+++ components/user/UserLink.js
@@ -2,7 +2,7 @@ import colors from "@config/icons.json";
 import getIcon from "@components/Icon";
 import Link from "@components/Link";
 import Edit from "@components/account/manage/Edit";
-import styles from "../../styles/animation.module.css";
+import styles from '../../styles/animation.module.css';
 import config from "@config/app.json";
 import { classNames } from "@services/utils/classNames";
 const animations = config.animations;
@@ -35,65 +35,66 @@ export default function UserLink({
     aria = "Globe";
   }
 
-  const item = (link) => {
-    return (
-      <div>
-        {manage && link.isPinned && (
-          <span className="inline-flex items-center absolute top-[1.56rem] right-24 z-10 rounded-md px-2 py-1 text-xs font-medium bg-secondary-low text-secondary-high-high ring-1 ring-inset ring-secondary-high/10">
-            Pinned
+  const item = (link) => (
+    <Link
+      href={`${BASE_URL}/api/profiles/${username}/links/${link._id}`}
+      target="_blank"
+      rel="noopener noreferrer"
+      className={classNames(
+        animations[link.animation] === animations.iconGlow && "z-0",
+        animations[link.animation] !== animations.glow && "dark:hover:bg-secondary-low/40 hover:bg-secondary-low/40",
+        isEnabled && getLinkAnimation.get(animations[link.animation]),
+        "relative rounded-full border border-primary-medium-low dark:border-primary-medium-low dark:hover:border-[color:var(--hover-color)] hover:border-[color:var(--hover-color)] hover:shadow-xl p-4 my-2 w-full content-start flex flex-row gap-4 items-center dark:bg-primary-medium grow"
+      )}
+      style={{
+        "--hover-color": colors[link.icon],
+      }}
+    >
+      <span className="relative">
+        <span style={{ color: colors[link.icon] }} className={getIconAnimation.get(animations[link.animation])}>
+          <DisplayIcon aria-label={`${aria} icon`} />
+        </span>
+        {animations[link.animation] === animations.ping && 
+          <span style={{ color: colors[link.icon] }} className={`relative`}>
+            <DisplayIcon aria-label={`${aria} icon`} />
           </span>
-        )}
-        <Link
-          href={`${BASE_URL}/api/profiles/${username}/links/${link._id}`}
-          target="_blank"
-          rel="noopener noreferrer"
+        }
+      </span>
+      <span className="grow">{link.name}</span>
+      {manage && link.isPinned && (
+        <span className="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium bg-secondary-low text-secondary-high-high ring-1 ring-inset ring-secondary-high/10">
+          Pinned
+        </span>
+      )}
+      {manage && (
+        <span
           className={classNames(
-            animations[link.animation] === animations.iconGlow && "z-0",
-            animations[link.animation] !== animations.glow &&
-              "dark:hover:bg-secondary-low/40 hover:bg-secondary-low/40",
-            !isEnabled && "opacity-50",
-            isEnabled && getLinkAnimation.get(animations[link.animation]),
-            "relative rounded-full border border-primary-medium-low dark:border-primary-medium-low dark:hover:border-[color:var(--hover-color)] hover:border-[color:var(--hover-color)] hover:shadow-xl p-4 my-2 w-full content-start flex flex-row gap-4 items-center dark:bg-primary-medium grow"
+            link.isEnabled
+              ? "bg-tertiary-low text-tertiary-high"
+              : "bg-primary-low text-primary-high",
+            "inline-flex items-center rounded-md px-2 py-1 text-xs font-medium ring-1 ring-inset ring-primary-high/10"
           )}
-          style={{
-            "--hover-color": colors[link.icon],
-          }}
         >
-          <span className="relative">
-            <span
-              style={{ color: colors[link.icon] }}
-              className={getIconAnimation.get(animations[link.animation])}
-            >
-              <DisplayIcon aria-label={`${aria} icon`} />
-            </span>
-            {animations[link.animation] === animations.ping && (
-              <span style={{ color: colors[link.icon] }} className={`relative`}>
-                <DisplayIcon aria-label={`${aria} icon`} />
-              </span>
-            )}
-          </span>
-          <span className="grow">{link.name}</span>
-          {manage && (
-            <span
-              className={classNames(
-                link.isEnabled
-                  ? "bg-tertiary-low text-tertiary-high"
-                  : "bg-primary-low text-primary-high",
-                "inline-flex items-center rounded-md px-2 py-1 text-xs font-medium ring-1 ring-inset ring-primary-high/10"
-              )}
-            >
-              {link.isEnabled ? "Enabled" : "Disabled"}
-            </span>
-          )}
-          {manage && link.group && (
-            <span className="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium bg-tertiary-high text-tertiary-low ring-1 ring-inset ring-tertiary-low/10">
-              {link.group}
-            </span>
-          )}
-        </Link>
+          {link.isEnabled ? "Enabled" : "Disabled"}
+        </span>
+      )}
+      {manage && link.group && (
+        <span className="inline-flex items-center rounded-md px-2 py-1 text-xs font-medium bg-tertiary-high text-tertiary-low ring-1 ring-inset ring-tertiary-low/10">
+          {link.group}
+        </span>
+      )}
+      <div
+        className={classNames(
+          isEnabled
+            ? "text-green-600 bg-green-600/10 dark:text-green-400 dark:bg-green-400/10"
+            : "text-red-600 bg-red-600/10  dark:text-red-400 dark:bg-red-400/10",
+          "flex-none rounded-full p-1"
+        )}
+      >
+        <div className="h-1.5 w-1.5 rounded-full bg-current" />
       </div>
-    );
-  };
+    </Link>
+  );
 
   const edit = (link) => (
     <Edit href={`/account/manage/link/${link._id}`} label={`${link.name} Link`}>
