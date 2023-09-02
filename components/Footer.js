80e026e4c7b349d0a5abbefa0fdc4e9a09693760
Sara Jaoude <82668196+SaraJaoude@users.noreply.github.com>
2023-09-02 17:20:24 +0100

    feat: blog and newsletter (#8920)

--- components/Footer.js
+++ components/Footer.js
@@ -53,12 +53,14 @@ export default function Footer() {
         href: "/maintainers",
         external: false,
       },
-      { name: "Resources", href: "/docs/community-resources", external: false },
+
       {
         name: "Contributors",
         href: BASE_GITHUB_PROJECT_URL + "/graphs/contributors",
         external: true,
       },
+      { name: "Resources", href: "/docs/community-resources", external: false },
+      { name: "Blog & Newsletter", href: "https://biodrop.substack.com/", external: true },
       { name: `v${app.version}`, href: "/roadmap", external: false },
     ],
     legal: [
