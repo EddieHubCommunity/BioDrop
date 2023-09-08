f8bfca8b2ac6911650e7e921f9796520ecc2fab4
IMSRS <53367382+theunhackable@users.noreply.github.com>
2023-09-08 07:35:12 +0000

    fix: improved dark mode background (#8945)
    issue: 8847

--- components/layouts/MultiLayout.js
+++ components/layouts/MultiLayout.js
@@ -10,7 +10,7 @@ export default function MultiLayout({ settings, children }) {
         {(!settings ||
           settings.type === "free" ||
           (settings.type === "premium" && !settings.hideNavbar)) && <Navbar />}
-        <main id="main" className="flex-1 dark:bg-dark">
+        <main id="main" className="flex-1 dark:bg-dark-2">
           {children}
         </main>
         {(!settings ||
