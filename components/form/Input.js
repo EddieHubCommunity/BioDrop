09284b0ab37218ba60c2a81c0fd385c41e1c127b
Eddie Jaoude <eddie@jaoudestudios.com>
2023-09-14 07:26:47 +0100

    Update components/form/Input.js

--- components/form/Input.js
+++ components/form/Input.js
@@ -19,20 +19,22 @@ const Input = forwardRef(
         e.stopPropagation();
       }
     };
-let inputClassName = "";
+    let inputClassName = "";
     switch (type) {
       case "color":
         inputClassName = classNames(
           disabled
             ? "bg-primary-low-medium dark:bg-primary-medium-low hover:border-primary-medium-low focus:ring-0 focus:border-primary-medium focus:outline-0 cursor-not-allowed"
-            : "dark:bg-primary-high hover:border-tertiary-medium focus:ring-0 focus:border-tertiary-medium focus:outline-0", "border-2 transition-all duration-250 ease-linear rounded px-1 mb-2 block w-[100px]"
+            : "dark:bg-primary-high hover:border-tertiary-medium focus:ring-0 focus:border-tertiary-medium focus:outline-0",
+          "border-2 transition-all duration-250 ease-linear rounded px-1 mb-2 block w-[100px]"
         );
         break;
       default:
         inputClassName = classNames(
           disabled
             ? "bg-primary-low-medium dark:bg-primary-medium-low hover:border-primary-medium-low focus:ring-0 focus:border-primary-medium focus:outline-0 cursor-not-allowed"
-            : "dark:bg-primary-high hover:border-tertiary-medium focus:ring-0 focus:border-tertiary-medium focus:outline-0", "border-2 transition-all duration-250 ease-linear rounded px-6 py-2 mb-2 block w-full"
+            : "dark:bg-primary-high hover:border-tertiary-medium focus:ring-0 focus:border-tertiary-medium focus:outline-0",
+          "border-2 transition-all duration-250 ease-linear rounded px-6 py-2 mb-2 block w-full"
         );
     }
 
