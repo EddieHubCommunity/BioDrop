decaa0baec79d11e77c33a70a8eb75b551b33c2b
AbhishekBhure <abhiab7411@gmail.com>
2023-09-22 23:35:52 +0530

    committing my changes

--- components/form/Input.js
+++ components/form/Input.js
@@ -20,20 +20,24 @@ const Input = forwardRef(
       }
     };
 
-    const inputClassName =
-      type === "color"
-        ? classNames(
-            disabled
-              ? "bg-primary-low-medium dark:bg-primary-medium-low hover:border-primary-medium-low focus:ring-0 focus:border-primary-medium focus:outline-0 cursor-not-allowed"
-              : "dark:bg-primary-high hover:border-tertiary-medium focus:ring-0 focus:border-tertiary-medium focus:outline-0",
-            "border-2 transition-all duration-250 ease-linear rounded px-1 mb-2 block w-[100px]"
-          )
-        : classNames(
-            disabled
-              ? "bg-primary-low-medium dark:bg-primary-medium-low hover:border-primary-medium-low focus:ring-0 focus:border-primary-medium focus:outline-0 cursor-not-allowed"
-              : "dark:bg-primary-high hover:border-tertiary-medium focus:ring-0 focus:border-tertiary-medium focus:outline-0",
-            "border-2 transition-all duration-250 ease-linear rounded px-6 py-2 mb-2 block w-full"
-          );
+    let inputClassName = "";
+    switch (type) {
+      case "color":
+        inputClassName = classNames(
+          disabled
+            ? "bg-primary-low-medium dark:bg-primary-medium-low hover:border-primary-medium-low focus:ring-0 focus:border-primary-medium focus:outline-0 cursor-not-allowed"
+            : "dark:bg-primary-high hover:border-tertiary-medium focus:ring-0 focus:border-tertiary-medium focus:outline-0",
+          "border-2 transition-all duration-250 ease-linear rounded px-1 mb-2 block w-[100px]"
+        );
+        break;
+      default:
+        inputClassName = classNames(
+          disabled
+            ? "bg-primary-low-medium dark:bg-primary-medium-low hover:border-primary-medium-low focus:ring-0 focus:border-primary-medium focus:outline-0 cursor-not-allowed"
+            : "dark:bg-primary-high hover:border-tertiary-medium focus:ring-0 focus:border-tertiary-medium focus:outline-0",
+          "border-2 transition-all duration-250 ease-linear rounded px-6 py-2 mb-2 block w-full"
+        );
+    }
 
     return (
       <>
