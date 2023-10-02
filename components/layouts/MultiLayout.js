abfc4acf1ffdb98559bed5c5dc53afcf215d5273
Eddie Jaoude <eddie@jaoudestudios.com>
2023-10-02 08:41:31 +0100

    fix: hide alert if navbar hidden

--- components/layouts/MultiLayout.js
+++ components/layouts/MultiLayout.js
@@ -7,11 +7,15 @@ export default function MultiLayout({ settings, children }) {
   return (
     <>
       <SkipLink />
-      <Alert />
       <div className="flex flex-col min-h-screen">
         {(!settings ||
           settings.type === "free" ||
-          (settings.type === "premium" && !settings.hideNavbar)) && <Navbar />}
+          (settings.type === "premium" && !settings.hideNavbar)) && (
+          <>
+            <Alert />
+            <Navbar />
+          </>
+        )}
         <main id="main" className="flex-1 dark:bg-dark-2 dark:z-10">
           {children}
         </main>
