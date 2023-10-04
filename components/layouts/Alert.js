49d11fdf7dae0caf88b2b13c3f0167748b655183
Krushna Kanta Rout <129386740+krushnarout@users.noreply.github.com>
2023-10-04 13:44:56 +0530

    fix: mobile star banner styling (#9322)

--- components/layouts/Alert.js
+++ components/layouts/Alert.js
@@ -3,7 +3,7 @@ import { FaGithub } from "react-icons/fa6";
 export default function Alert() {
   return (
     <div className="flex items-center gap-x-6 bg-gray-900 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
-      <p className="text-sm leading-6 text-white">
+      <p className="text-sm leading-6 text-white whitespace-nowrap">
         <a
           href="https://github.com/EddieHubCommunity/BioDrop"
           target="_blank"
