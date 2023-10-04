a1a67ef7f23c6acd737f469807ef3a92ae787e74
Christine Belzie <shecoder30@gmail.com>
2023-10-04 17:20:59 -0400

    fix: revise suggestions

--- pages/docs/open-source-roadmap/what-next.js
+++ pages/docs/open-source-roadmap/what-next.js
@@ -17,7 +17,7 @@ export default function DocsNext() {
         <h1 className="text-4xl mb-4 font-bold">
           Open Source Roadmap: What next?
         </h1>
-        <p>It doesn't stop there! You can become a maintainer.</p>
+        <p>It doesn't stop there! You can become a maintainer. If you want to do this for the communities you're in, you can do things like identifying duplicate issues or spam pull requests, aiding contributors with their pull requests, giving contributors friendly reminders about the Contributing Guide for first-time contributors. You can even take what you have learned and start your own Open Source project. The possibilities are endless.</p>
       </Page>
     </>
   );
