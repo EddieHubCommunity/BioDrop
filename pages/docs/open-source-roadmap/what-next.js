c8598810112bbeef9fe0f1bbe456dbc218aced7c
Christine Belzie <105683440+CBID2@users.noreply.github.com>
2023-10-17 17:02:50 -0400

    docs: open source roadmap step 8 details (#9353)
    * feat: elaborating on step 8 in open source map
    
    * fix: revise paragraph
    
    * fix: revise suggestions
    
    * fix: made changes to line 20
    
    * fix: change sentence back
    
    * fix: revise single quote
    
    * fix: revise single quotes
    
    * fix: revise single quote part 3
    
    * Update pages/docs/open-source-roadmap/index.js
    
    * Update pages/docs/open-source-roadmap/what-next.js
    
    ---------
    
    Co-authored-by: Sara Jaoude <82668196+SaraJaoude@users.noreply.github.com>

--- pages/docs/open-source-roadmap/what-next.js
+++ pages/docs/open-source-roadmap/what-next.js
@@ -17,7 +17,23 @@ export default function DocsNext() {
         <h1 className="text-4xl mb-4 font-bold">
           Open Source Roadmap: What next?
         </h1>
-        <p>Coming soon</p>
+        <h2 className="mt-16 text-2xl font-bold tracking-tight text-primary-high dark:text-primary-low">
+          Become a Maintainer
+        </h2>
+        <p>
+          If you are looking for greater involvement in an Open Source project
+          then you can work towards becoming a maintainer. In this role, asides
+          from coding contributions you will be taking be one of the stewards of
+          the project.
+        </p>
+        <p>
+          Some example tasks of what maintainers do are: ensuring the project's
+          documentation is in order (for example, the README, the Contributing
+          Guide and the Code of Conduct), creating templates which guide other
+          contributors to submit good work, moderating the project,
+          communicating with contributors as to the direction of the project as
+          well as triaging and assigning issues and reviewing pull requests.
+        </p>
       </Page>
     </>
   );
