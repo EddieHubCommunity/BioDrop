9a8790e1b4b1135e33535f627a05c2ea4c595a77
Eddie Jaoude <eddie@jaoudestudios.com>
2023-10-26 07:59:36 +0100

    fix: url wrap in stats list (#9620)

--- pages/account/statistics/index.js
+++ pages/account/statistics/index.js
@@ -207,7 +207,7 @@ export default function Statistics({ data, profile, BASE_URL }) {
             {data.links &&
               data.links.individual.map((link) => (
                 <tr key={link.url}>
-                  <td className="md:whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-primary-high dark:text-primary-low sm:pl-6">
+                  <td className="md:whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-primary-high dark:text-primary-low sm:pl-6 text-ellipsis overflow-hidden">
                     {session && session.accountType === "premium" && (
                       <Link href={`/account/statistics/link/${link._id}`}>
                         {link.url}
