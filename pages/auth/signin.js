f3b4cc17739dcefb5c82a2bf7025edf846fe54fc
Eddie Jaoude <eddie@jaoudestudios.com>
2023-11-12 20:20:44 +0000

    fix: login page logo (#9777)
    * fix: login page logo
    
    * fix: logo for og tag

--- pages/auth/signin.js
+++ pages/auth/signin.js
@@ -1,4 +1,3 @@
-import Image from "next/image";
 import { getServerSession } from "next-auth/next";
 import { signIn } from "next-auth/react";
 
@@ -7,7 +6,7 @@ import { authOptions } from "pages/api/auth/[...nextauth]";
 import { BsGithub } from "react-icons/bs";
 import Button from "@components/Button";
 import Link from "@components/Link";
-import { PROJECT_NAME } from "@constants/index";
+import Logo from "@public/logos/Logo";
 
 export async function getServerSideProps(context) {
   const session = await getServerSession(context.req, context.res, authOptions);
@@ -26,13 +25,7 @@ export default function SignIn() {
     <div className="flex min-h-screen items-center justify-center px-4 py-12 sm:px-6 lg:px-8">
       <div className="w-full max-w-sm space-y-8 min-w-max">
         <Link href="/">
-          <Image
-            width={100}
-            height={100}
-            className="mx-auto h-64 w-auto"
-            src="/logo512.png"
-            alt={`${PROJECT_NAME} logo`}
-          />
+          <Logo className="w-1/2" />
         </Link>
         <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-primary-medium flex flex-col">
           <span>Connect to YOUR audience</span>
