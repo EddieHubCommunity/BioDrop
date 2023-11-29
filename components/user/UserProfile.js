203320c9593cf74a85b23af68ced4f917099b251
Eddie Jaoude <eddie@jaoudestudios.com>
2023-11-29 07:40:00 +0000

    fix: simplified external embed links

--- components/user/UserProfile.js
+++ components/user/UserProfile.js
@@ -17,7 +17,6 @@ import Markdown from "@components/Markdown";
 
 function UserProfile({ BASE_URL, data }) {
   const [qrShow, setQrShow] = useState(false);
-  const [embedFormat, setEmbedFormat] = useState("md");
   const [premiumShow, setPremiumShow] = useState(false);
   const router = useRouter();
   const fallbackImageSize = 120;
@@ -150,36 +149,28 @@ function UserProfile({ BASE_URL, data }) {
                   </Link>
                 ))}
               </div>
-              <div className=" flex items-center justify-center w-full overflow-hidden">
-                <ClipboardCopy>
-                  <p className="dark:text-gray-300 border p-3 rounded-md">
-                    {`${BASE_URL}/${data.username}`}
-                  </p>
-                </ClipboardCopy>
-              </div>
-              <div className="mt-5 space-y-4 items-center justify-center w-full overflow-hidden">
-                <h5 className="font-medium text-lg">Embed Profile</h5>
-                <div className="flex">
-                  <select 
-                    className="w-fit flex-none dark:text-gray-300 border-white pr-8 focus:border-white focus:ring-0 bg-transparent border p-3 rounded-l-md"
-                    onChange={e => setEmbedFormat(e.currentTarget.value)}
-                  >
-                    <option value="md">Markdown</option>
-                    <option value="html">HTML</option>
-                  </select>
-                  <div className="flex flex-auto">
-<ClipboardCopy>
-  <div className="dark:text-gray-300 border p-3 rounded-r-md h-full">
-    <p className="truncate absolute max-w-[80%]">
-      {embedFormat === "md" 
-        ? `[![${data.username} | BioDrop](${BASE_URL}/${data.username}?embed)](${BASE_URL}/${data.username})`
-        : `<a href="${BASE_URL}/${data.username}"><img src="${BASE_URL}/${data.username}?embed" alt="${data.username} | BioDrop" /></a>`
-      }
-    </p>
-  </div>
-</ClipboardCopy>
-                  </div>
-                </div> 
+              <div className="flex flex-col gap-4">
+                <div className="flex items-center justify-center w-full overflow-hidden">
+                  <ClipboardCopy>
+                    <p className="dark:text-gray-300 border p-3 rounded-md">
+                      {`${BASE_URL}/${data.username}`}
+                    </p>
+                  </ClipboardCopy>
+                </div>
+                <div className="flex items-center justify-center w-full overflow-hidden">
+                  <ClipboardCopy>
+                    <p className="dark:text-gray-300 border p-3 rounded-md">
+                      {`[![${data.username} | BioDrop](${BASE_URL}/${data.username}?embed)](${BASE_URL}/${data.username})`}
+                    </p>
+                  </ClipboardCopy>
+                </div>
+                <div className="flex items-center justify-center w-full overflow-hidden">
+                  <ClipboardCopy>
+                    <p className="dark:text-gray-300 border p-3 rounded-md">
+                      {`<a href="${BASE_URL}/${data.username}"><img src="${BASE_URL}/${data.username}?embed" alt="${data.username} | BioDrop" /></a>`}
+                    </p>
+                  </ClipboardCopy>
+                </div>
               </div>
             </>
           )}
