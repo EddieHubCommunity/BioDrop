a95715e2fdf3a93910219057efe757b7f31c26ca
GiulianoGosty <giuliano.gostinfini.93@gmail.com>
2023-12-14 16:57:57 +0100

    [ add ] reorder mechanic to UserRepos component

--- components/user/UserRepos.js
+++ components/user/UserRepos.js
@@ -6,10 +6,44 @@ import StarIcon from "@heroicons/react/20/solid/StarIcon";
 import PencilIcon from "@heroicons/react/20/solid/PencilIcon";
 import ExclamationCircleIcon from "@heroicons/react/20/solid/ExclamationCircleIcon";
 import dateFormat from "@services/utils/dateFormat";
+import { ReactSortable } from "react-sortablejs";
+import { useState } from "react";
+import { clientEnv } from "@config/schemas/clientSchema";
+import ArrowPathIcon from "@heroicons/react/24/outline/ArrowPathIcon";
+import Button from "@components/Button";
+
+
+
 
 export default function UserRepos({ manage = false, confirmDelete, repos }) {
+
+
+  const [reposList, setReposList] = useState(repos || []);
+  const [reorder, setReorder] = useState(false);
+  const [reposListPrevious, setReposListPrevious] = useState(repos || []);
+
+
+  const saveOrder = async () => {
+    const BASE_URL = clientEnv.NEXT_PUBLIC_BASE_URL;
+    const res = await fetch(`${BASE_URL}/api/account/manage/links`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(reposList),
+    });
+    const updatedRepos = await res.json();
+    setReposList(updatedRepos);
+    setReposListPrevious(updatedRepos);
+    setReorder(false);
+  };
+  
+
+
+
   const item = (repo) => (
     <div className="relative flex justify-between gap-x-6 px-4 py-5 hover:bg-primary-low dark:hover:bg-primary-medium transition-all duration-100 sm:px-6 lg:px-8">
+     
       <div className="flex gap-x-4">
         <FallbackImage
           className="h-12 w-12 flex-none rounded-full bg-primary-low"
@@ -81,10 +115,47 @@ export default function UserRepos({ manage = false, confirmDelete, repos }) {
   );
 
   return (
+    <>
+    <div className="flex gap-4">
+    {!reorder && (
+          <Button
+            onClick={() => setReorder(true)}
+            disabled={reposList.length < 2}
+          >
+            <ArrowPathIcon className="h-5 w-5 mr-2" />
+            REORDER
+          </Button>
+        )}
+        {reorder && (
+          <Button
+            onClick={() => {
+              setReorder(false);
+              setReposList(reposListPrevious);
+            }}
+          >
+            CANCEL
+          </Button>
+        )}
+        {reorder && (
+            <Button primary={true} onClick={() => saveOrder()}>
+              SAVE
+            </Button>
+          )}
+    </div>
     <ul role="list" className="divide-y divide-primary-low">
-      {repos.map((repo) => (
+      <ReactSortable
+          list={reposList}
+          setList={setReposList}
+          disabled={!reorder}
+          ghostClass="border-2"
+          chosenClass="border-dashed"
+          dragClass="border-red-500"
+        >
+      {reposList.map((repo) => (
         <li key={repo._id}>{manage ? manageDelete(repo) : item(repo)}</li>
       ))}
+      </ReactSortable>
     </ul>
+    </>
   );
 }
