365deabe8850ecc30c003a876bb5c6777279cad7
Sital Adhikari <sitaladhikari002@gmail.com>
2023-12-15 12:17:22 +0545

    fix: new group cannot be added

--- components/GroupLinkSearch.js
+++ components/GroupLinkSearch.js
@@ -5,9 +5,17 @@ import Label from "./form/Label";
 function GroupLinkSearch({ selectedGroup, handleGroupSelection, groups }) {
   const [query, setQuery] = useState("");
 
+  const handleGroupChange = (e) => {
+    setQuery(e.target.value);
+    const currentValue = e.target.value;
+    if (!groups.includes(currentValue)) {
+      handleGroupSelection(currentValue);
+    }
+  };
+
   const filteredGroup =
     query === ""
-      ? groups
+      ? groups.slice(0, 5)
       : groups
           .filter((group) => {
             return group.toLowerCase().includes(query.toLowerCase());
@@ -19,7 +27,7 @@ function GroupLinkSearch({ selectedGroup, handleGroupSelection, groups }) {
       <Label htmlFor="search-groups">Group</Label>
       <Combobox.Input
         name="search-group"
-        onChange={(event) => setQuery(event.target.value)}
+        onChange={handleGroupChange}
         className={`border-2 transition-all duration-250 ease-linear rounded px-6 py-2 mb-2 block w-full dark:bg-primary-high hover:border-tertiary-medium focus:ring-0 focus:border-tertiary-medium focus:outline-0`}
       />
       <Combobox.Options
