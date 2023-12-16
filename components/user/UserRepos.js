474942646d520608952379d4406dadde0b57801f
GiulianoGosty <giuliano.gostinfini.93@gmail.com>
2023-12-16 14:36:39 +0100

    [ remove ] role list since it does not affect reactSortable

--- components/user/UserRepos.js
+++ components/user/UserRepos.js
@@ -151,7 +151,6 @@ export default function UserRepos({ manage = false, confirmDelete, repos }) {
           chosenClass="border-dashed"
           dragClass="border-red-500"
           className="divide-y divide-primary-low"
-          role="list"
         >
       {reposList.map((repo) => (
         <li key={repo._id}>{manage ? manageDelete(repo) : item(repo)}</li>
