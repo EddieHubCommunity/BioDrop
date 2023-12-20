eba42f8ef773909b1e2abd07c89158177e741657
Kanhaiya Pandey <kanhaiyapandey2232@gmail.com>
2023-12-20 16:04:56 +0530

    Update Link.js
    manage links responsiveness fixed

--- components/Link.js
+++ components/Link.js
@@ -7,7 +7,7 @@ export default function Link({ children, className, rel, ...restProps }) {
       className={
         className
           ? className
-          : "text-primary-medium dark:text-primary-low underline decoration-dotted hover:underline hover:decoration-solid break-all"
+          : "text-primary-medium dark:text-primary-low underline decoration-dotted hover:underline hover:decoration-solid break-all sm:text-sm md:text-base lg:text-lg xl:text-xl"
       }
       prefetch={false}
       {...restProps}
