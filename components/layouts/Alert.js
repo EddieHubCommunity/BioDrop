f76c4da8814dff617fd1c94cb61d4160b07c4e44
Eddie Jaoude <eddie@jaoudestudios.com>
2023-12-27 10:57:00 +0000

    fix: cta from star to rate (#10035)

--- components/layouts/Alert.js
+++ components/layouts/Alert.js
@@ -1,17 +1,13 @@
-import { FaGithub } from "react-icons/fa6";
-
 export default function Alert() {
   return (
     <div className="flex items-center gap-x-6 bg-gray-900 px-6 py-2.5 sm:px-3.5 sm:before:flex-1">
       <p className="text-sm leading-6 text-white whitespace-nowrap">
         <a
-          href="https://github.com/EddieHubCommunity/BioDrop"
+          href="https://repo-rater.eddiehub.io"
           target="_blank"
           className="hover:text-primary-low text-tertiary-medium flex items-center gap-x-1"
         >
-          Support by Starring our
-          <FaGithub aria-label="GitHub" />
-          GitHub Repo!
+          Please rate us on the RepoRater platform
           <span aria-hidden="true">&rarr;</span>
         </a>
       </p>
