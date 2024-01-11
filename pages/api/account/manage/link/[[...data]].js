c44252c342e664ae7e82321d841d1dc0ca5c0fc7
sital002 <93700622+sital002@users.noreply.github.com>
2024-01-11 20:05:23 +0545

    fix:9878 group suggestion link

--- pages/api/account/manage/link/[[...data]].js
+++ pages/api/account/manage/link/[[...data]].js
@@ -56,7 +56,6 @@ export async function getLinkApi(username, id) {
 
 export async function getGroupLinkApi(username) {
   await connectMongo();
-  const log = logger.child({ username });
   let getGroupLink = [];
   getGroupLink = await Link.aggregate([
     { $match: { username } },
