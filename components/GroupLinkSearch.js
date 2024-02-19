9c39eadd7c4627e8834320845fe10dc8c5c1bdff
Tingwei Xie <tix980@mail.usask.ca>
2024-02-19 00:25:26 +0000

    Fixed the Issue The group drop-down suggestions are not visible properly

--- components/GroupLinkSearch.js
+++ components/GroupLinkSearch.js
@@ -45,7 +45,7 @@ function GroupLinkSearch({ selectedGroup, handleGroupSelection, groups }) {
                 key={group}
                 value={group}
                 className={
-                  "px-3 py-2 flex items-center dark:hover:bg-tertiary-medium/60 hover:bg-secondary-low/40"
+                  "px-3 py-2 flex items-c`px-3 py-2 flex items-center bg-white dark:bg-primary-medium dark:hover:bg-tertiary-medium/60 hover:bg-secondary-low/100`enter dark:hover:bg-tertiary-medium/60 hover:bg-secondary-low/40"
                 }
               >
                 {group !== "" && <span>{group}</span>}
